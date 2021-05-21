57189dc7501203c6029819f1e95152407cab0be0
adam-maj <mr.adam.maj@gmail.com>
2021-05-21 17:04:49 -0400

    Pack test passing

--- test/Pack.test.js
+++ test/Pack.test.js
@@ -1,15 +1,20 @@
-const chai = require("chai");
-const { ethers } = require("hardhat");
-const { solidity } = require("ethereum-waffle");
+const chai = require("chai")
+const { ethers } = require("hardhat")
+const { solidity } = require("ethereum-waffle")
 
-chai.use(solidity);
-const { expect } = chai;
+chai.use(solidity)
+const { expect } = chai
 
 describe("Pack", () => {
-  let pack;
+  let pack
 
   beforeEach(async () => {
-    const Pack = await ethers.getContractFactory("Pack");
-    pack = await Pack.deploy();
+    const Pack = await ethers.getContractFactory("Pack")
+    pack = await Pack.deploy()
+  })
+
+  it('Correct contract owner', async () => {
+    const [owner] = await ethers.getSigners()
+    expect(await pack.owner()).to.equal(owner.address)
   })
 })
