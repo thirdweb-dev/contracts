e7424e4b8ed0f6447011c5d5498fe66a398a96b5
adam-maj <mr.adam.maj@gmail.com>
2021-05-21 19:06:08 -0400

    createPack creates Token

--- test/Pack.test.js
+++ test/Pack.test.js
@@ -2,6 +2,7 @@ const chai = require("chai");
 const { ethers } = require("hardhat");
 const { solidity } = require("ethereum-waffle");
 
+// Use chai solidity plugin
 chai.use(solidity)
 const { expect } = chai;
 
@@ -9,6 +10,7 @@ describe("Pack", () => {
   let pack;
   let sender;
 
+  // Get message sender and pack interface before each test
   beforeEach(async () => {
     const [owner] = await ethers.getSigners();
     sender = owner;
@@ -17,10 +19,21 @@ describe("Pack", () => {
     pack = await Pack.deploy();
   })
 
-  it("createPack emits PackCreated", async () => {
-    expect(await pack.createPack("URI", 100))
-      .to
-      .emit(pack, "PackCreated")
-      .withArgs(sender.address, 0, "URI", 100);
+  // createPack(string memory tokenUri, uint256 maxSupply) external returns (uint256 tokenId)
+  describe("createPack", async () => {
+    it("createPack creates Token", async () => {
+      const tokenId = await pack.createPack("URI", 100);
+      expect(tokenId.value).to.equal(0);
+
+      const token = await pack.tokens(tokenId);
+      expect(token.uri).to.equal("URI");
+    })
+
+    it("createPack emits PackCreated", async () => {
+      expect(await pack.createPack("URI", 100))
+        .to
+        .emit(pack, "PackCreated")
+        .withArgs(sender.address, 0, "URI", 100);
+    })
   })
 })
