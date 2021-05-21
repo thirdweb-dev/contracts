455796f21db90ad2918d2562b17d4e907d58ccd0
adam-maj <mr.adam.maj@gmail.com>
2021-05-21 15:53:42 -0400

    Setup unit test format

---
+++ test/Pack.test.js
@@ -0,0 +1,12 @@
+const chai = require("chai");
+const { ethers } = require("hardhat");
+const { solidity } = require("ethereum-waffle");
+
+chai.use(solidity);
+const { expect } = chai;
+
+describe('Pack', () => {
+  
+})
+
+
