a1efcc2f9b537a07728a673c706b884a8b951816
adam-maj <mr.adam.maj@gmail.com>
2021-05-23 14:33:58 -0400

    PackMarket test stubs

--- test/PackMarket.test.js
+++ test/PackMarket.test.js
@@ -5,11 +5,53 @@ const { solidity } = require("ethereum-waffle")
 chai.use(solidity)
 const { expect } = chai
 
-describe("PackMarket", () => {
+describe("PackMarket", async () => {
   let packMarket
 
   beforeEach(async () => {
     const PackMarket = await ethers.getContractFactory("PackMarket")
     packMarket = await PackMarket.deploy()
+  })
+
+  describe("setPackToken", async () => {
+    it("setPackToken only owner can change Pack", async () => {
+
+    })
+
+    it("setPackToken emits PackTokenChange", async () => {
+
+    })
+  })
+
+  describe("sell", async () => {
+    it("sell requires token approval", async () => {
+      
+    })
+
+    it("sell requires at least one token", async () => {
+      
+    })
+
+    it("sell locks Pack", async () => {
+      
+    })
+
+    it("sell creates listing", async () => {
+
+    })
+
+    it("sell emits PackListed", async () => {
+
+    })
+  })
+
+  describe("buy", async () => {
+    it("buy successfully transfers Pack", async () => {
+      
+    })
+
+    it("buy emits PackSold", async () => {
+
+    })
   })
 })
