b6c8224ea7f4c625a088a26925f7303caac1ad84
nkrishang <kn1457@nyu.edu>
2021-06-02 14:27:10 -0400

    Deployed canonical contracts to Rinkeby

--- scripts/deploySimple.js
+++ scripts/deploySimple.js
@@ -1,3 +1,10 @@
+  
+// Chainlink info for Rinkeby
+
+const vrfCoordinator = '0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B'
+const linkTokenAddress = '0x01be23585060835e02b77ef475b0cc51aa1e0709'
+const keyHash = '0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311'
+
 async function main() {
 
   const [deployer] = await ethers.getSigners();
@@ -9,14 +16,13 @@ async function main() {
   
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-//   const PackToken_Factory = await ethers.getContractFactory("Pack");
-//   const packToken = await PackToken_Factory.deploy();
+  const PackToken_Factory = await ethers.getContractFactory("Pack");
+  const packToken = await PackToken_Factory.deploy(vrfCoordinator, linkTokenAddress, keyHash);
 
-//   console.log("Pack ERC1155 token address:", packToken.address);
+  console.log("Pack ERC1155 token address:", packToken.address);
 
   const PackMarket_Factory = await ethers.getContractFactory("PackMarket");
-//   const packMarket = await PackMarket_Factory.deploy(packToken.address);
-  const packMarket = await PackMarket_Factory.deploy("0xac063C80a70725e3c63FaaC04a10920596cd9255");
+  const packMarket = await PackMarket_Factory.deploy(packToken.address);
 
   console.log("Pack Market address:", packMarket.address);
 }
@@ -28,5 +34,5 @@ main()
     process.exit(1);
   });
 
-// Pack ERC1155 token address (Rinkeby) -- 0xac063C80a70725e3c63FaaC04a10920596cd9255
-// Pack Market address -- 0x09Ba216EE3006Bd86785b808B7BeDc6AA36201d7
+// Pack ERC1155 token address (Rinkeby) -- 0x0c56B393043CDA7c726c27FdD64Bd9262428515F
+// Pack Market address (Rinkeby) -- 0x24574D0C177ad9E5cD74d9dBF5a9A729924e72e2
---
+++ scripts/test-txs/helperValues.js
@@ -0,0 +1,33 @@
+const packURIs = [
+  'https://siasky.net/AADTWsStZLziMMaJChRYxAOxCZ7rscocx7GfwvLx309bag',
+  'https://siasky.net/_BE4UzjYVtOu36HVwKWumeXmQgUUvUKLWZPuDRuzeIpl0w',
+  'https://siasky.net/fALDtj5c1KqiUQvi6sawxEMkLjYYmOYrmLBlKUMqCY4wNg',
+  'https://siasky.net/fALS84HucF6A5bsF-I2GNa75oio4rtr3vAM7WCn7PlO4Zg',
+  'https://siasky.net/_AyTnOQNbgenuggWEXSDgahmlXDCqWTyLdFO54ME8Ogobw',
+  'https://siasky.net/PAA_gi468BhdLw9XpLlNXQ26kS3srsagR-hA01kE7kWk-Q',
+  'https://siasky.net/_B2027-GPaZI_nuSWgMTZthFfkO35zEKM-kUE_cPZ98-oQ',
+  'https://siasky.net/_AYMLkCbLOaGL1woCX3yuoMKEtYu4z8crVkGHpABGOnDOA',
+  'https://siasky.net/_BHcX3y5t3wlTafs_dwEkXtoa1vnVk6T1ZobX1g5IC3Aig',
+  'https://siasky.net/3AFRWgeS0AhWFgvWRyHvuW3Avj8Vm2R3xD_aM3B5YnYvpw',
+  'https://siasky.net/_BH5n9KrDUH2uhhpXDsSUjB-n2zE7cy18uL0MNoiBLfHoA',
+  'https://siasky.net/_AgIymYQ7HXuer-cFWrLhT5hfYmnsXeSzNshW1JNJOmRZw',
+]
+
+const rewardURIs = [
+  'https://siasky.net/IABmw8Ary3P-4zc9qFp6urgSW-DmolTva9s_3HIiVsDJhw',
+  'https://siasky.net/KACgVMqI34PP1xmVZSGmOEhWH8hGY2U2oN9xqIThh4_Y1A',
+  'https://siasky.net/KADzAKkdCqWJXVAjhVxHvRxabCVe3hlVASZVnSm732guuA',
+  'https://siasky.net/KADVgLkIe2QjRqwl_kM3hAzTlSG01_R4-9AfAoYLdCW-lA',
+  'https://siasky.net/IADtjJNzA5kBcieZPIWpu3nStFd3PU8iwYN62Ed2uQJ27Q'
+]
+
+const currencyAddresses = [
+  '0x49e7f00ee5652523fAdE13674100c8518d7DA8b6', // ERC 20 token (`PackCoin`)
+  '0x0000000000000000000000000000000000000000' // Zero address == ETH (on the subgraph)
+]
+
+module.exports = {
+  packURIs,
+  rewardURIs,
+  currencyAddresses
+}
