78f5b936dbff04ec466daa14254d14b6e8d04f6e
Furqan Rydhan <furqan.rydhan@gmail.com>
2021-06-06 09:07:00 +0000

    treasury address using env var

--- test/PackMarket.test.js
+++ test/PackMarket.test.js
@@ -26,7 +26,7 @@ describe("PackMarket", async () => {
   before(async () => {
     signers = await ethers.getSigners();
     [owner, buyer] = signers;
-  })
+  });
 
   beforeEach(async () => {
     const Pack = await ethers.getContractFactory("Pack", owner);
@@ -41,8 +41,8 @@ describe("PackMarket", async () => {
     currency = packCoin.address;
 
     const PackMarket = await ethers.getContractFactory("PackMarket", owner);
-    packMarket = await PackMarket.deploy(pack.address);
-  })
+    packMarket = await PackMarket.deploy(pack.address, process.env.TREASURY_ADDRESS);
+  });
 
   describe("setPackToken", async () => {
     let anotherPack;
@@ -54,8 +54,8 @@ describe("PackMarket", async () => {
         process.env.CHAINLINK_LINK_TOKEN,
         process.env.CHAINLINK_KEY_HASH,
       );
-    })
-    
+    });
+
     it("setPackToken only owner can change Pack", async () => {
       expect(await packMarket.packToken()).to.equal(pack.address);
 
@@ -68,16 +68,14 @@ describe("PackMarket", async () => {
 
       await packMarket.setPackToken(anotherPack.address);
       expect(await packMarket.packToken()).to.equal(anotherPack.address);
-
-    })
+    });
 
     it("setPackToken emits PackTokenChange", async () => {
       expect(await packMarket.setPackToken(anotherPack.address))
-        .to
-        .emit(packMarket, "PackTokenChanged")
+        .to.emit(packMarket, "PackTokenChanged")
         .withArgs(anotherPack.address);
-    })
-  })
+    });
+  });
 
   describe("sell", async () => {
     let tokenId;
@@ -85,7 +83,7 @@ describe("PackMarket", async () => {
     beforeEach(async () => {
       const packToken = await pack.createPack(uri, rewardUris, rewardSupplies);
       tokenId = packToken.value;
-    })
+    });
 
     it("sell requires token approval", async () => {
       try {
@@ -94,7 +92,7 @@ describe("PackMarket", async () => {
       } catch (err) {
         expect(err.message).to.contain("Must approve market contract to manage tokens");
       }
-    })
+    });
 
     it("sell must own enough tokens", async () => {
       await pack.setApprovalForAll(packMarket.address, true);
@@ -106,7 +104,7 @@ describe("PackMarket", async () => {
       } catch (err) {
         expect(err.message).to.contain("Must own the amount of tokens being listed");
       }
-    })
+    });
 
     it("sell must list at least one token", async () => {
       await pack.setApprovalForAll(packMarket.address, true);
@@ -117,7 +115,7 @@ describe("PackMarket", async () => {
       } catch (err) {
         expect(err.message).to.contain("Must list at least one token");
       }
-    })
+    });
 
     it("sell requires locked pack", async () => {
       await pack.setApprovalForAll(packMarket.address, true);
@@ -128,28 +126,27 @@ describe("PackMarket", async () => {
       } catch (err) {
         expect(err.message).to.contain("Cannot sell an unlocked pack or a token that has not been minted");
       }
-    })
+    });
 
     it("sell creates listing", async () => {
       await pack.setApprovalForAll(packMarket.address, true);
       await packMarket.sell(tokenId, currency, price, quantity);
-      
+
       const listing = await packMarket.listings(owner.address, tokenId);
       expect(listing.owner).to.equal(owner.address);
       expect(listing.tokenId).to.equal(tokenId);
       expect(listing.currency).to.equal(currency);
       expect(listing.price).to.equal(price);
-    })
+    });
 
     it("sell emits NewListing", async () => {
       await pack.setApprovalForAll(packMarket.address, true);
 
       expect(await packMarket.sell(tokenId, currency, price, quantity))
-        .to
-        .emit(packMarket, "NewListing")
+        .to.emit(packMarket, "NewListing")
         .withArgs(owner.address, tokenId, true, currency, price, quantity);
-    })
-  })
+    });
+  });
 
   describe("unlist", async () => {
     let tokenId;
@@ -160,7 +157,7 @@ describe("PackMarket", async () => {
 
       await pack.setApprovalForAll(packMarket.address, true);
       await packMarket.sell(tokenId, currency, price, quantity);
-    })
+    });
 
     it("unlist listing must exist", async () => {
       try {
@@ -169,7 +166,7 @@ describe("PackMarket", async () => {
       } catch (err) {
         expect(err.message).to.contain("Only the seller can modify the listing.");
       }
-    })
+    });
 
     it("unlist onlySeller can update", async () => {
       try {
@@ -178,22 +175,21 @@ describe("PackMarket", async () => {
       } catch (err) {
         expect(err.message).to.contain("Only the seller can modify the listing.");
       }
-    })
+    });
 
     it("unlist inactivates listing", async () => {
       await packMarket.unlist(tokenId);
 
-      const listing = await packMarket.listings(owner.address, tokenId)
+      const listing = await packMarket.listings(owner.address, tokenId);
       expect(listing.active).to.equal(false);
-    })
+    });
 
     it("unlist emits ListingUpdate", async () => {
       expect(await packMarket.unlist(tokenId))
-        .to
-        .emit(packMarket, "ListingUpdate")
+        .to.emit(packMarket, "ListingUpdate")
         .withArgs(owner.address, tokenId, false, currency, price, quantity);
-    })
-  })
+    });
+  });
 
   describe("buy", async () => {
     let tokenId;
@@ -201,7 +197,7 @@ describe("PackMarket", async () => {
     beforeEach(async () => {
       const packToken = await pack.createPack(uri, rewardUris, rewardSupplies);
       tokenId = packToken.value;
-    })
+    });
 
     it("buy must buy at least one token", async () => {
       await pack.setApprovalForAll(packMarket.address, true);
@@ -216,7 +212,7 @@ describe("PackMarket", async () => {
       } catch (err) {
         expect(err.message).to.contain("must buy at least one token");
       }
-    })
+    });
 
     it("buy cannot buy more than listed quantity", async () => {
       await pack.setApprovalForAll(packMarket.address, true);
@@ -231,7 +227,7 @@ describe("PackMarket", async () => {
       } catch (err) {
         expect(err.message).to.contain("attempting to buy more tokens than listed");
       }
-    })
+    });
 
     it("buy successfully transfers Pack", async () => {
       expect(await pack.balanceOf(buyer.address, tokenId)).to.equal(0);
@@ -244,7 +240,7 @@ describe("PackMarket", async () => {
       await packMarket.connect(buyer).buy(owner.address, tokenId, quantity);
 
       expect(await pack.balanceOf(buyer.address, tokenId)).to.equal(1);
-    })
+    });
 
     it("buy emits NewSale", async () => {
       await pack.setApprovalForAll(packMarket.address, true);
@@ -254,11 +250,10 @@ describe("PackMarket", async () => {
       await packCoin.connect(buyer).approve(packMarket.address, price * quantity);
 
       expect(await packMarket.connect(buyer).buy(owner.address, tokenId, quantity))
-        .to
-        .emit(packMarket, "NewSale")
+        .to.emit(packMarket, "NewSale")
         .withArgs(owner.address, buyer.address, tokenId, currency, price, quantity);
-    })
-  })
+    });
+  });
 
   describe("edit listing", async () => {
     let tokenId;
@@ -269,27 +264,26 @@ describe("PackMarket", async () => {
 
       await pack.setApprovalForAll(packMarket.address, true);
       await packMarket.sell(tokenId, currency, price, quantity);
-    })
+    });
 
     it("setListingPrice changes price", async () => {
-      const listing = await packMarket.listings(owner.address, tokenId)
+      const listing = await packMarket.listings(owner.address, tokenId);
       expect(listing.price).to.equal(price);
 
       await packMarket.setListingPrice(tokenId, price + 1);
 
-      const newListing = await packMarket.listings(owner.address, tokenId)
+      const newListing = await packMarket.listings(owner.address, tokenId);
       expect(newListing.price).to.equal(price + 1);
-    })
+    });
 
     it("setListingPrice emits ListingUpdate", async () => {
       expect(await packMarket.setListingPrice(tokenId, price + 1))
-        .to
-        .emit(packMarket, "ListingUpdate")
+        .to.emit(packMarket, "ListingUpdate")
         .withArgs(owner.address, tokenId, true, currency, price + 1, quantity);
-    })
+    });
 
     it("setListingCurrency changes currency", async () => {
-      const listing = await packMarket.listings(owner.address, tokenId)
+      const listing = await packMarket.listings(owner.address, tokenId);
       expect(listing.currency).to.equal(currency);
 
       const PackCoin = await ethers.getContractFactory("PackCoin", owner);
@@ -297,21 +291,19 @@ describe("PackMarket", async () => {
       const newCurrency = newPackCoin.address;
       await packMarket.setListingCurrency(tokenId, newCurrency);
 
-      const newListing = await packMarket.listings(owner.address, tokenId)
+      const newListing = await packMarket.listings(owner.address, tokenId);
       expect(newListing.currency).to.equal(newCurrency);
-    })
+    });
 
     it("setListingCurrency emits ListingUpdate", async () => {
       const PackCoin = await ethers.getContractFactory("PackCoin", owner);
       const newPackCoin = await PackCoin.deploy();
       const newCurrency = newPackCoin.address;
-      
+
       expect(await packMarket.setListingCurrency(tokenId, newCurrency))
-        .to
-        .emit(packMarket, "ListingUpdate")
-        .withArgs(owner.address, tokenId, true, newCurrency, price, quantity)
+        .to.emit(packMarket, "ListingUpdate")
+        .withArgs(owner.address, tokenId, true, newCurrency, price, quantity);
+    });
+  });
+});
 
-    })
-
-  })
-})
