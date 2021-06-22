6291773cd79bd4b65117788963f5d1ef649cbfc0
nkrishang <kn1457@nyu.edu>
2021-06-22 12:04:27 -0400

    Update PackMarket for new architecture

--- contracts/PackMarket.sol
+++ contracts/PackMarket.sol
@@ -3,97 +3,82 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "./Pack.sol";
 
-contract PackMarket is Ownable, ReentrancyGuard {
-  using SafeMath for uint256;
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
-  Pack public packToken;
-  address public protocolTreasury;
+import "./PackERC1155.sol";
+import "./PackControl.sol";
 
-  event PackTokenChanged(address newPackTokenAddress);
-  event TreasuryAddressChanged(address newTreasuryAddress);
+contract PackMarket is Ownable, ReentrancyGuard, IERC1155Receiver {
+
+  PackControl internal controlCenter;
 
   event NewListing(
     address indexed seller, 
-    uint256 indexed tokenId, 
-    bool active, 
+    uint indexed tokenId, 
     address currency, 
-    uint256 price, 
-    uint256 quantity
+    uint price, 
+    uint quantity
   );
   event NewSale(
     address indexed seller, 
     address indexed buyer, 
-    uint256 indexed tokenId, 
+    uint indexed tokenId, 
     address currency, 
-    uint256 price, 
-    uint256 quantity
+    uint price, 
+    uint quantity
   );
   event ListingUpdate(
     address indexed seller, 
-    uint256 indexed tokenId, 
-    bool active, 
+    uint indexed tokenId, 
     address currency, 
-    uint256 price, 
-    uint256 quantity
+    uint price, 
+    uint quantity
   );
+  event Unlisted(address indexed seller, uint indexed tokenId, uint quantity);
 
-  uint256 public constant MAX_BPS = 10000; // 100%
-  uint256 public protocolFeeBps = 500; // 5%
-  uint256 public creatorFeeBps = 500; // 5%
+  uint public constant MAX_BPS = 10000; // 100%
+  uint public protocolFeeBps = 500; // 5%
+  uint public creatorFeeBps = 500; // 5%
 
   struct Listing {
     address owner;
-    uint256 tokenId;
-    bool active;
+    uint tokenId;
 
+    uint quantity;
     address currency;
-    uint256 price;
-    uint256 quantity;
+    uint price;
   }
 
   // owner => tokenId => Listing
-  mapping(address => mapping(uint256 => Listing)) public listings;
+  mapping(address => mapping(uint => Listing)) public listings;
 
-  modifier eligibleToList(uint tokenId, uint _quantity, address _currency) {
-    require(packToken.isApprovedForAll(msg.sender, address(this)), "Must approve market contract to manage tokens.");
-    require(packToken.balanceOf(msg.sender, tokenId) >= _quantity, "Must own the amount of tokens being listed.");
+  modifier onlyControlCenter() {
+    require(msg.sender == address(controlCenter), "Only the protocol control center can call this function.");
+    _;
+  }
+
+  modifier eligibleToList(uint tokenId, uint _quantity) {
+    require(
+      PackERC1155(controlCenter.packERC1155()).isApprovedForAll(msg.sender, address(this)),
+      "Must approve market contract to manage tokens."
+    );
+    require(
+      PackERC1155(controlCenter.packERC1155()).balanceOf(msg.sender, tokenId) >= _quantity,
+      "Must own the amount of tokens being listed."
+    );
     require(_quantity > 0, "Must list at least one token");
     _;
   }
 
   modifier onlySeller(uint tokenId) {
     require(listings[msg.sender][tokenId].owner != address(0), "Only the seller can modify the listing.");
-
     _;
   }
 
-  constructor(address _packToken, address _treasuryAddress) {
-    packToken = Pack(_packToken);
-    protocolTreasury = _treasuryAddress;
-  }
-
-  /**
-   * @notice Sets the token address of the ERC1155 Pack token contract associated with the market.
-   *
-   * @param _packToken The address of an ERC1155 token contract.
-   */
-  function setPackToken(address _packToken) external onlyOwner {
-    packToken = Pack(_packToken);
-    emit PackTokenChanged(_packToken);
-  }
-
-  /**
-   * @notice Sets the protocol treasury address, in control of `protcolFees`
-   *
-   * @param _treasuryAddress The address of an ERC1155 token contract.
-   */
-  function setProtocolTreasury(address _treasuryAddress) external onlyOwner {
-    protocolTreasury = _treasuryAddress;
-    emit PackTokenChanged(_treasuryAddress);
+  constructor(address _controlCenter) {
+    controlCenter = PackControl(_controlCenter);
   }
 
   /**
@@ -104,97 +89,66 @@ contract PackMarket is Ownable, ReentrancyGuard {
    * @param price The price of each unit of token listed for sale.
    * @param quantity The number of ERC1155 tokens of id `tokenId` being listed for sale.
    */
-  function sell(
-    uint256 tokenId, 
+  function list(
+    uint tokenId, 
     address currency, 
-    uint256 price, 
-    uint256 quantity
-  ) external eligibleToList(tokenId, quantity, currency) {
+    uint price, 
+    uint quantity
+  ) external eligibleToList(tokenId, quantity) {
+
+    PackERC1155(controlCenter.packERC1155()).safeTransferFrom(
+      msg.sender,
+      address(this),
+      tokenId,
+      quantity,
+      ""
+    );
 
     listings[msg.sender][tokenId] = Listing({
       owner: msg.sender,
       tokenId: tokenId,
-      active: true,
       currency: currency,
       price: price,
       quantity: quantity
     });
 
-    emit NewListing(msg.sender, tokenId, true, currency, price, quantity);
+    emit NewListing(msg.sender, tokenId, currency, price, quantity);
   }
 
   /**
-   * @notice Lets a seller set an existing listing as active or inactive.
+   * @notice Lets a seller set unlist `quantity` amount of tokens.
    *
    * @param tokenId The ERC1155 tokenId of the token being unlisted.
-   * @param _active The new status of the listing -- either active or inactive.
+   * @param quantity The amount of tokens to unlist.
    */
-  function setListingStatus(uint256 tokenId, bool _active) external onlySeller(tokenId) {
-    listings[msg.sender][tokenId].active = _active;
+  function unlist(uint tokenId, uint quantity) external onlySeller(tokenId) {
+    require(listings[msg.sender][tokenId].quantity >= quantity, "Cannot unlist more tokens than are listed.");
 
-    emit ListingUpdate(
+    PackERC1155(controlCenter.packERC1155()).safeTransferFrom(
+      address(this),
       msg.sender,
       tokenId,
-      listings[msg.sender][tokenId].active, 
-      listings[msg.sender][tokenId].currency, 
-      listings[msg.sender][tokenId].price, 
-      listings[msg.sender][tokenId].quantity
+      quantity,
+      ""
     );
+
+    emit Unlisted(msg.sender, tokenId, quantity);
   }
 
   /**
-   * @notice Lets a seller change the price of a listing.
-   * 
-   * @param tokenId The ERC1155 tokenId associated with the listing.
-   * @param _newPrice The new price for the listing.
-   */
-  function setListingPrice(uint256 tokenId, uint256 _newPrice) external onlySeller(tokenId) {
-    listings[msg.sender][tokenId].price = _newPrice;
-    
-    emit ListingUpdate(
-      msg.sender,
-      tokenId,
-      listings[msg.sender][tokenId].active, 
-      listings[msg.sender][tokenId].currency, 
-      listings[msg.sender][tokenId].price, 
-      listings[msg.sender][tokenId].quantity
-    );
-  }
-
-  /**
-   * @notice Lets a seller change the currency they want to accept for the listing.
+   * @notice Lets a seller change the currency and price of a listing.
    * 
    * @param tokenId The ERC1155 tokenId associated with the listing.
    * @param _newCurrency The new currency for the listing. 
+   * @param _newPrice The new price for the listing.
    */
-  function setListingCurrency(uint256 tokenId, address _newCurrency) external onlySeller(tokenId) {
+  function setPriceStatus(uint tokenId, address _newCurrency, uint _newPrice) external onlySeller(tokenId) {
+    listings[msg.sender][tokenId].price = _newPrice;
     listings[msg.sender][tokenId].currency = _newCurrency;
 
     emit ListingUpdate(
       msg.sender,
       tokenId,
-      listings[msg.sender][tokenId].active, 
-      listings[msg.sender][tokenId].currency, 
-      listings[msg.sender][tokenId].price, 
-      listings[msg.sender][tokenId].quantity
-    );
-  }
-
-  /**
-   * @notice Lets a seller change the quantity of token to be listed for sale.
-   * 
-   * @param tokenId The ERC1155 tokenId associated with the listing.
-   * @param _newQuantity The new quantity of token to be listed. 
-   */
-  function setListingQuantity(uint256 tokenId, uint _newQuantity) external onlySeller(tokenId) {
-    require(packToken.balanceOf(msg.sender, tokenId) >= _newQuantity, "Must own the amount of tokens being listed.");
-
-    listings[msg.sender][tokenId].quantity = _newQuantity;
-
-    emit ListingUpdate(
-      msg.sender,
-      tokenId,
-      listings[msg.sender][tokenId].active, 
       listings[msg.sender][tokenId].currency, 
       listings[msg.sender][tokenId].price, 
       listings[msg.sender][tokenId].quantity
@@ -208,13 +162,12 @@ contract PackMarket is Ownable, ReentrancyGuard {
    * @param tokenId The ERC1155 tokenId associated with the listing.
    * @param quantity The quantity of tokens to buy from the relevant listing.
    */
-  function buy(address from, uint256 tokenId, uint256 quantity) external payable nonReentrant {
-    require(listings[from][tokenId].owner == from, "The listing does not exist.");
-    require(quantity > 0, "must buy at least one token");
+  function buy(address from, uint tokenId, uint quantity) external payable nonReentrant {
+    require(listings[from][tokenId].owner != address(0), "The listing does not exist.");
     require(quantity <= listings[from][tokenId].quantity, "attempting to buy more tokens than listed");
 
     Listing memory listing = listings[from][tokenId];
-    (address creator,,,,) = packToken.tokens(tokenId);
+    (address creator,,,) = PackERC1155(controlCenter.packERC1155()).tokens(tokenId);
     
     if(listing.currency == address(0)) {
       distributeEther(listing.owner, creator, listing.price, quantity);
@@ -222,7 +175,7 @@ contract PackMarket is Ownable, ReentrancyGuard {
       distributeERC20(listing.owner, creator, listing.currency, listing.price, quantity);
     }
 
-    packToken.safeTransferFrom(listing.owner, msg.sender, tokenId, quantity, "");
+    PackERC1155(controlCenter.packERC1155()).safeTransferFrom(listing.owner, msg.sender, tokenId, quantity, "");
     listings[from][tokenId].quantity -= quantity;
 
     emit NewSale(from, msg.sender, tokenId, listing.currency, listing.price, quantity);
@@ -238,10 +191,10 @@ contract PackMarket is Ownable, ReentrancyGuard {
    * @param quantity The quantity of ERC1155 tokens being purchased.  
    */
   function distributeERC20(address seller, address creator, address currency, uint price, uint quantity) internal {
-    uint256 totalPrice = price.mul(quantity);
-    uint256 protocolCut = totalPrice.mul(protocolFeeBps).div(MAX_BPS);
-    uint256 creatorCut = seller == creator ? 0 : totalPrice.mul(creatorFeeBps).div(MAX_BPS);
-    uint256 sellerCut = totalPrice - protocolCut - creatorCut;
+    uint totalPrice = price * quantity;
+    uint protocolCut = (totalPrice * protocolFeeBps) / MAX_BPS;
+    uint creatorCut = seller == creator ? 0 : (totalPrice * creatorFeeBps) / MAX_BPS;
+    uint sellerCut = totalPrice - protocolCut - creatorCut;
 
     IERC20 priceToken = IERC20(currency);
     priceToken.approve(address(this), sellerCut + creatorCut);
@@ -266,10 +219,10 @@ contract PackMarket is Ownable, ReentrancyGuard {
    * @param quantity The quantity of ERC1155 tokens being purchased.
    */
   function distributeEther(address seller, address creator, uint price, uint quantity) internal {
-    uint256 totalPrice = price.mul(quantity);
-    uint256 protocolCut = totalPrice.mul(protocolFeeBps).div(MAX_BPS);
-    uint256 creatorCut = seller == creator ? 0 : totalPrice.mul(creatorFeeBps).div(MAX_BPS);
-    uint256 sellerCut = totalPrice - protocolCut - creatorCut;
+    uint totalPrice = price * quantity;
+    uint protocolCut = (totalPrice * protocolFeeBps) / MAX_BPS;
+    uint creatorCut = seller == creator ? 0 : (totalPrice * creatorFeeBps) / MAX_BPS;
+    uint sellerCut = totalPrice - protocolCut - creatorCut;
 
     require(msg.value >= totalPrice, "Must sent enough eth to buy the given amount.");
 
@@ -282,7 +235,7 @@ contract PackMarket is Ownable, ReentrancyGuard {
   }
 
   function transferProtocolFees(address _to, address _currency, uint _amount) public {
-    require(msg.sender == protocolTreasury, "Only the treasury contract can transfer protocol fees.");
+    require(msg.sender == address(controlCenter), "Only the treasury contract can transfer protocol fees.");
 
     if(_currency == address(0)) {
       IERC20 feeToken = IERC20(_currency);
@@ -299,5 +252,32 @@ contract PackMarket is Ownable, ReentrancyGuard {
       (bool success,) = (_to).call{value: _amount}("");
       require(success, "ETH withdrawal of protocol fees failed.");
     }
+  }
+
+  /// @dev See `IERC1155Receiver.sol` and `IERC165.sol`
+  function supportsInterface(bytes4 interfaceID) external view override returns (bool) {
+      return  interfaceID == 0x01ffc9a7 || interfaceID == 0x4e2312e0;
+  }
+
+  /// @dev See `IERC1155Receiver.sol`
+  function onERC1155Received(
+    address operator,
+    address from,
+    uint256 id,
+    uint256 value,
+    bytes calldata data
+  ) external override returns (bytes4) {
+    return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+  }
+
+  /// @dev See `IERC1155Receiver.sol`
+  function onERC1155BatchReceived(
+    address operator,
+    address from,
+    uint256[] calldata ids,
+    uint256[] calldata values,
+    bytes calldata data
+  ) external override returns (bytes4) {
+    return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
   }
 }
