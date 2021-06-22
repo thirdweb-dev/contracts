e29525cb9a11ae78cff0bd16a1ad0915eb76be9b
nkrishang <kn1457@nyu.edu>
2021-06-22 13:03:58 -0400

    CRUD for pack protocol modules

--- contracts/PackControl.sol
+++ contracts/PackControl.sol
@@ -8,18 +8,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 
-import "./interfaces/IPackControl.sol";
-
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
-contract PackControl is AccessControl, IPackControl {
+contract PackControl is AccessControl {
 
   bytes32 public constant PROTOCOL_ADMIN = keccak256("PROTOCOL_ADMIN");
 
-  address public packERC1155;
-  address public packMarket;
-  address public packRNG;
+  mapping(bytes32 => address) public modules;
+  mapping(string => bytes32) public moduleId;
+
+  event ModuleAdded(string moduleName, bytes32 moduleId, address module);
+  event ModuleUpdated(string moduleName, bytes32 moduleId, address module);
 
   modifier onlyProtocolAdmin() {
     require(hasRole(PROTOCOL_ADMIN, msg.sender), "Only protocol admins can call this function.");
@@ -30,24 +30,49 @@ contract PackControl is AccessControl, IPackControl {
     grantRole(PROTOCOL_ADMIN, msg.sender);
   }
 
-  /// @notice Sets the ERC1155 part of the pack protocol.
-  function setPackERC1155(address _packERC1155) external override onlyProtocolAdmin {
-    packERC1155 = _packERC1155;
+  /// @dev Lets protocol admin add a module to the pack protocol.
+  function addModule(string calldata _moduleName, address _moduleAddress) external onlyProtocolAdmin {
+    bytes32 id = keccak256(bytes(_moduleName));
+
+    moduleId[_moduleName] = id;
+    modules[id] = _moduleAddress;
+
+    emit ModuleAdded(_moduleName, id, _moduleAddress);
   }
 
-  /// @notice Sets the address for the Market part of the pack protocol.
-  function setPackMarket(address _market) external override onlyProtocolAdmin {
-    packMarket = _market;
+  /// @dev Lets protocol admin change address of a module of the pack protocol.
+  function changeModuleAddress(string calldata _moduleName, address _newModuleAddress) external onlyProtocolAdmin {
+    
+    bytes32 id = keccak256(bytes(_moduleName));
+    require(modules[id] != address(0), "The given module does not exist.");
+
+    modules[id] = _newModuleAddress;
+
+    emit ModuleUpdated(_moduleName, id, _newModuleAddress);
   }
 
-  /// @notice Sets the RNG for pack protocol.
-  function setPackRNG(address _rng) external override onlyProtocolAdmin {
-    packRNG = _rng;
+  /// @dev Lets protocol admin delete a module of the pack protocol.
+  function deleteModule(string calldata _moduleName) external onlyProtocolAdmin {
+
+    bytes32 id = keccak256(bytes(_moduleName));
+    require(modules[id] != address(0), "The given module does not exist.");
+
+    delete modules[id];
+    delete moduleId[_moduleName];
+
+    emit ModuleUpdated(_moduleName, id, address(0));
+  }
+
+  /// @dev Returns a module of the pack protocol.
+  function getModule(string calldata _moduleName) public returns (address) {
+    return modules[moduleId[_moduleName]];
   }
 
   /// @notice Pauses all activity in the ERC1155 part of the protocol.
-  function pausePackToken() external onlyProtocolAdmin {
-    ERC1155PresetMinterPauser(packERC1155).pause();
+  function pausePackERC1155(string calldata _moduleName) external onlyProtocolAdmin {
+    ERC1155PresetMinterPauser(
+      getModule(_moduleName)
+    ).pause();
   }
 
   /// @notice Grants the `PROTOCOL_ADMIN` role to `_newAdmin`.
