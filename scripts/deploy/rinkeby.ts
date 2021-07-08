0e7427b5b070e8411dbc7d327f614638131adbe2
nkrishang <kn1457@nyu.edu>
2021-07-08 11:51:26 -0400

    Added deploy script

---
+++ scripts/deploy/rinkeby.ts
@@ -0,0 +1,81 @@
+import { run, ethers } from "hardhat";
+import { Signer, Contract, ContractFactory } from 'ethers';
+import { chainlinkVars, pairs } from "../../utils/utils";
+
+async function main() {
+  await run("compile");
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log(`Deploying contracts with account: ${await deployer.getAddress()}`)
+
+  // 1. Deploy ControlCenter
+  const ControlCenter_Factory: ContractFactory = await ethers.getContractFactory("ControlCenter");
+  const controlCenter: Contract = await ControlCenter_Factory.deploy(await deployer.getAddress());
+
+  console.log(`ControlCenter.sol address: ${controlCenter.address}`);
+
+  // 2. Deploy rest of the protocol modules.
+  const Pack_Factory: ContractFactory = await ethers.getContractFactory("Pack");
+  const pack: Contract = await Pack_Factory.deploy(controlCenter.address);
+
+  console.log(`Pack.sol address: ${pack.address}`);
+
+  const Handler_Factory: ContractFactory = await ethers.getContractFactory("Handler");
+  const handler: Contract = await Handler_Factory.deploy(controlCenter.address);
+
+  console.log(`Handler.sol address: ${handler.address}`);
+
+  const Market_Factory: ContractFactory = await ethers.getContractFactory("Market");
+  const market: Contract = await Market_Factory.deploy(controlCenter.address);
+
+  console.log(`Market.sol address: ${market.address}`);
+
+  const { vrfCoordinator, linkTokenAddress, keyHash } = chainlinkVars;
+  
+  const RNG_Factory: ContractFactory = await ethers.getContractFactory("RNG");
+  const rng: Contract = await RNG_Factory.deploy(
+    controlCenter.address,
+    vrfCoordinator,
+    linkTokenAddress,
+    keyHash
+  );
+
+  console.log(`RNG.sol address: ${rng.address}`);
+
+  const AssetSafe_Factory: ContractFactory = await ethers.getContractFactory("AssetSafe");
+  const assetSafe: Contract = await AssetSafe_Factory.deploy(controlCenter.address);
+
+  console.log(`AssetSafe.sol address: ${assetSafe.address}`);
+  
+  // Deploy Access Packs.
+  const AccessPacks_Factory: ContractFactory = await ethers.getContractFactory("AccessPacks");
+  const accessPacks: Contract = await AccessPacks_Factory.deploy();
+
+  console.log(`AccessPacks.sol address: ${accessPacks.address}`);
+
+  console.log("\n");
+
+  // Initialize $PACK Protocol in ControlCenter
+  await controlCenter.connect(deployer).initPackProtocol(
+    pack.address,
+    handler.address,
+    market.address,
+    rng.address,
+    assetSafe.address
+  );
+  console.log("Initialized $Pack Protocol.")
+
+  // Setup RNG
+  for(let pair of pairs) {
+    await rng.connect(deployer).addPair(pair.pair);
+  }
+  console.log("Initialized DEX RNG.")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
