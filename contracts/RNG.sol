2ed83e51b08a12525a973de99c1aaebd3e92d456
nkrishang <kn1457@nyu.edu>
2021-07-19 14:02:28 -0400

    Cleaned up RNG: inherits DexRNG and implements VRFConsumerBase

--- contracts/RNG.sol
+++ contracts/RNG.sol
@@ -1,22 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@chainlink/contracts/src/v0.8/dev/VRFConsumerBase.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./helpers/DexRNG.sol";
 
-interface IProtocolControl {
-  /// @dev Returns the address of pack protocol's module.
-  function getModule(string memory _moduleName) external view returns (address);
-}
+import { IProtocolControl, IRNGReceiver } from "./Interfaces.sol";
 
-/// @dev Basic interface for a random number receiver.
-interface IRNGReceiver {
-  function fulfillRandomness(uint requestId, uint randomness) external;
-}
-
-contract RNG is Ownable, VRFConsumerBase {
+contract RNG is DexRNG, VRFConsumerBase {
 
   /// @dev The pack protocol admin contract.
   IProtocolControl internal controlCenter;
@@ -28,38 +19,18 @@ contract RNG is Ownable, VRFConsumerBase {
   bool public isExternalService;
 
   /// @dev Chainlink VRF requirements.
-  bytes32 internal keyHash;
   uint internal fees;
+  bytes32 internal keyHash;
 
-  /// @dev Increments by one. Acts as a request ID for each external RNG request.
+  /// @dev Increments by one. Acts as a human readable request ID for each external RNG request.
   uint public currentRequestId;
 
-  /// @dev 
+  /// @dev bytes request ID => human readable integer request ID.
   mapping(bytes32 => uint) public requestIds;
 
+  /// @dev Events.
   event ExternalServiceRequest(address indexed requestor, uint requestId);
   event RandomNumberExternal(uint randomNumber);
-
-  /// @dev DEX RNG variables.
-  uint public currentPairIndex;
-  uint internal seed;
-
-  struct PairAddresses {
-    address tokenA;
-    address tokenB;
-    address pair;
-
-    uint lastUpdateTimeStamp;
-  }
-
-  mapping(uint => PairAddresses) public pairs;
-  mapping(address => uint) public pairIndex;
-  mapping(address => bool) public active;
-  mapping(uint => bool) public blockEntropy;
-  
-  event RandomNumber(address indexed requester, uint randomNumber);
-  event PairAdded(address pair, address tokenA, address tokenB);
-  event PairStatusUpdated(address pair, bool active);
 
   constructor(
     address _controlCenter,
@@ -68,6 +39,7 @@ contract RNG is Ownable, VRFConsumerBase {
     address _linkToken,
     bytes32 _keyHash,
     uint _fees
+
   ) VRFConsumerBase(_vrfCoordinator, _linkToken) {
     controlCenter = IProtocolControl(_controlCenter);
 
@@ -104,76 +76,23 @@ contract RNG is Ownable, VRFConsumerBase {
     emit RandomNumberExternal(randomness);
   }
 
+  /// @dev Returns the fee amount and token to pay fees in.
+  function getRequestFee() external view returns(address feeToken, uint feeAmount) {
+    return (address(LINK), fees);
+  }
+
+  /// @dev Changes the `fees` required by Chainlink VRF.
+  function setFees(uint _fees) external {
+    require(
+      controlCenter.hasRole(controlCenter.PROTOCOL_ADMIN(), msg.sender), 
+      "RNG: Only a pack protocol admin can call this function."
+    );
+    fees = _fees;
+  }
+
   /**
-   *  DEX RNG functions
+   *  View functions
   **/
-
-  /// @dev Add a UniswapV2/Sushiswap pair to draw randomness from.
-  function addPair(address pair) external onlyOwner {
-    require(IUniswapV2Pair(pair).MINIMUM_LIQUIDITY() == 1000, "Invalid pair address provided.");
-    require(pairIndex[pair] == 0, "This pair already exists as a randomness source.");
-    
-    // Update pair index.
-    currentPairIndex += 1;
-
-    // Store pair.
-    pairs[currentPairIndex] = PairAddresses({
-      tokenA: IUniswapV2Pair(pair).token0(),
-      tokenB: IUniswapV2Pair(pair).token1(),
-      pair: pair,
-      lastUpdateTimeStamp: 0
-    });
-
-    pairIndex[pair] = currentPairIndex;
-    active[pair] = true;
-
-    emit PairAdded(pair, pairs[currentPairIndex].tokenA, pairs[currentPairIndex].tokenB);
-  }
-
-  /// @dev Sets whether a UniswapV2 pair is actively used as a source of randomness.
-  function changePairStatus(address pair, bool activeStatus) external onlyOwner {
-    require(pairIndex[pair] != 0, "Cannot change the status of a pair that does not exist.");
-
-    active[pair] = activeStatus;
-    
-    emit PairStatusUpdated(pair, activeStatus);
-  }
-
-  /// @dev Returns a random number within the given range;
-  function getRandomNumber(uint range) external returns (uint randomNumber, bool acceptableEntropy) {
-    require(currentPairIndex > 0, "No Uniswap pairs available to draw randomness from.");
-
-    acceptableEntropy = blockEntropy[block.number];
-    
-    uint blockSignature = uint(keccak256(abi.encodePacked(msg.sender, seed, uint(blockhash(block.number - 1)))));
-
-    for(uint i = 1; i <= currentPairIndex; i++) {
-
-      if(!active[pairs[i].pair]) {
-        continue;
-      }
-
-      (uint reserveA, uint reserveB, uint lastUpdateTimeStamp) = getReserves(pairs[i].pair, pairs[i].tokenA, pairs[i].tokenB);
-      
-      uint randomMod = seed == 0 ? (reserveA + reserveB) % range : (reserveA + reserveB) % (seed % range);
-      blockSignature += randomMod;
-
-      if(lastUpdateTimeStamp > pairs[i].lastUpdateTimeStamp) {
-
-        if(!acceptableEntropy) {
-          acceptableEntropy = true;
-          blockEntropy[block.number] = true;
-        }
-
-        pairs[i].lastUpdateTimeStamp = lastUpdateTimeStamp;
-      }
-    }
-
-    randomNumber = blockSignature % range;
-    seed = uint(keccak256(abi.encodePacked(msg.sender, randomNumber)));
-    
-    emit RandomNumber(msg.sender, randomNumber);
-  }
 
   /// @dev Returns whether the RNG is using an external service for random number generation.
   function usingExternalService() external view returns (bool) {
@@ -183,23 +102,5 @@ contract RNG is Ownable, VRFConsumerBase {
   /// @dev Returns pack protocol's `Pack`
   function pack() internal view returns (IRNGReceiver) {
     return IRNGReceiver(controlCenter.getModule(PACK));
-  }
-  
-  /// @notice See `UniswapV2Library.sol`
-  function getReserves(
-    address pair, 
-    address tokenA, 
-    address tokenB
-  ) internal view returns (uint reserveA, uint reserveB, uint lastUpdateTimeStamp) {
-    (address token0,) = sortTokens(tokenA, tokenB);
-    (uint reserve0, uint reserve1, uint blockTimestampLast) = IUniswapV2Pair(pair).getReserves();
-    (reserveA, reserveB, lastUpdateTimeStamp) = tokenA == token0 ? (reserve0, reserve1, blockTimestampLast) : (reserve1, reserve0, blockTimestampLast);
-  }
-
-  /// @notice See `UniswapV2Library.sol`
-  function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
-    require(tokenA != tokenB, "UniswapV2Library: IDENTICAL_ADDRESSES");
-    (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-    require(token0 != address(0), "UniswapV2Library: ZERO_ADDRESS");
   }
 }
