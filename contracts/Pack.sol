9f877a1e84880950bc2f8d4a7bef73fc2d6cdc80
nkrishang <kn1457@nyu.edu>
2021-08-05 16:01:11 -0400

    Update Pack to be VRFConsumer

--- contracts/Pack.sol
+++ contracts/Pack.sol
@@ -6,21 +6,28 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { IProtocolControl, IRNG } from "./Interfaces.sol";
+import "@chainlink/contracts/src/v0.8/dev/VRFConsumerBase.sol";
 
-contract Pack is ERC1155, IERC1155Receiver {
+interface IProtocolControl {
+  /// @dev Returns whether the pack protocol is paused.
+  function systemPaused() external view returns (bool);
+}
 
-  /// @dev The pack protocol admin contract.
+contract Pack is ERC1155, IERC1155Receiver, VRFConsumerBase {
+
+  /// @dev The $PACK Protocol control center.
   IProtocolControl internal controlCenter;
 
-  /// @dev Pack protocol module names.
-  string public constant RNG = "RNG";
-  string public constant MARKET = "MARKET";
-
-  /// @dev The tokenId for packs to be minted.
+  /// @dev The tokenId for the next set of packs to be minted.
   uint public nextTokenId;
 
-  struct PackInfo {
+  /// @dev Chainlink VRF variables.
+  uint public vrfSeed;
+  uint public fees;
+  bytes32 public keyHash;
+  
+  /// @dev The state of packs with a unique tokenId.
+  struct PackState {
     uint packId;
     address creator;
     string uri;
@@ -30,6 +37,7 @@ contract Pack is ERC1155, IERC1155Receiver {
     uint openEnd;
   }
 
+  /// @dev The rewards in a given set of packs with a unique tokenId.
   struct Rewards {
     address source;
 
@@ -37,51 +45,51 @@ contract Pack is ERC1155, IERC1155Receiver {
     uint[] amountsPacked;
   }
 
-  struct Open {
-    uint start;
-    uint end;
-  }
-
+  /// @dev The state of a random number request made to Chainlink VRF on opening a pack.
   struct RandomnessRequest {
-    address opener;
     uint packId;
+    address opener;
   }
 
-  /// @dev tokenId => token creator.
-  mapping(uint => address) public creator;
+  /// @dev pack tokenId => The state of packs with id `tokenId`.
+  mapping(uint => PackState) public packs;
 
-  /// @dev tokenId => token URI.
-  mapping(uint => string) public tokenURI;
-
-  /// @dev tokenId => total supply of token.
-  mapping(uint => uint) public totalSupply;
-
-  /// @dev tokenId => rewards in pack.
+  /// @dev pack tokenId => rewards in pack with id `tokenId`.
   mapping(uint => Rewards) public rewards;
 
-  /// @dev tokenId => time limits on on when you can open a pack.
-  mapping(uint => Open) public openLimit;
+  /// @dev requestId => Chainlink VRF request state with id `requestId`.
+  mapping(bytes32 => RandomnessRequest) public randomnessRequests;
 
-  /// @dev requestId => Randomness request state.
-  mapping(uint => RandomnessRequest) public randomnessRequests;
+  /// @dev pack tokenId => pack opener => whether there is a pending Chainlink VRF random number request.
+  mapping(uint => mapping(address => bool)) public pendingRequests;
 
-  /// @dev packId => any pending randomness requests.
-  mapping(uint => mapping(address => bool)) public pendingRandomnessRequests;
+  /// @dev Emitted when a set of packs is created.
+  event PackCreated(address indexed rewardContract, address indexed creator, PackState packState, Rewards rewards);
+  /// @dev Emitted on a request to open a pack.
+  event PackOpenRequest(uint indexed packId, address indexed opener, bytes32 requestId);
+  /// @dev Emitted when a request to open a pack is fulfilled.
+  event PackOpenFulfilled(uint indexed packId, address indexed opener, bytes32 requestId, address indexed rewardContract, uint rewardId);
 
-  /// @dev Events.
-  event PackCreated(address indexed rewardContract, address indexed creator, uint packId, string packURI, uint packTotalSupply, uint openStart, uint openEnd);
-  event PackRewards(uint indexed packId, address indexed rewardContract, uint[] rewardIds, uint[] rewardAmounts);
-  event PackOpened(uint indexed packId, address indexed opener);
-  event RewardDistributed(address indexed rewardContract, address indexed receiver, uint indexed packId, uint rewardId);
-
-  /// @dev Checks whether Pack protocol is paused.
+  /// @dev Checks whether $PACK Protocol is paused.
   modifier onlyUnpausedProtocol() {
-    require(!controlCenter.systemPaused(), "Pack: The pack protocol is paused.");
+    require(!controlCenter.systemPaused(), "Pack: The protocol is paused.");
     _;
   }
 
-  constructor(address _controlCenter, string memory _uri) ERC1155(_uri) {
+  constructor(
+    address _controlCenter,
+    string memory _uri,
+
+    address _vrfCoordinator,
+    address _linkToken,
+    bytes32 _keyHash,
+    uint _fees
+
+  ) ERC1155(_uri) VRFConsumerBase(_vrfCoordinator, _linkToken) {
     controlCenter = IProtocolControl(_controlCenter);
+
+    keyHash = _keyHash;
+    fees = _fees;
   }
 
   /**
@@ -89,7 +97,7 @@ contract Pack is ERC1155, IERC1155Receiver {
   **/
 
   function uri(uint _id) public view override returns (string memory) {
-    return tokenURI[_id];
+    return packs[_id].uri;
   }
 
   function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual override returns (bytes4) {
@@ -122,99 +130,111 @@ contract Pack is ERC1155, IERC1155Receiver {
     require(IERC1155(_rewardContract).isApprovedForAll(msg.sender, address(this)), "Pack: not approved to transer the reward tokens.");
 
     // Get pack tokenId and total supply.
-    packId = _tokenId();
+    packId = _newPackId();
     packTotalSupply = _sumArr(_rewardAmounts);
 
     // Transfer ERC 1155 reward tokens Pack Protocol's asset manager. Will revert if `msg.sender` does not own the given amounts of tokens.
     IERC1155(_rewardContract).safeBatchTransferFrom(msg.sender, address(this), _rewardIds, _rewardAmounts, "");
 
     // Store pack state.
-    creator[packId] = msg.sender;
-    tokenURI[packId] = _packURI;
-    
-    rewards[packId] = Rewards({
+    PackState memory packState = PackState({
+      packId: packId,
+      creator: msg.sender,
+      uri: _packURI,
+      currentSupply: packTotalSupply,
+      openStart: block.timestamp + _secondsUntilOpenStart,
+      openEnd: _secondsUntilOpenEnd == 0 ? type(uint256).max : block.timestamp + _secondsUntilOpenEnd
+    });
+
+    Rewards memory rewardsInPack = Rewards({
       source: _rewardContract,
       tokenIds: _rewardIds,
       amountsPacked: _rewardAmounts
     });
 
-    openLimit[packId] = Open({
-      start: block.timestamp + _secondsUntilOpenStart,
-      end: _secondsUntilOpenEnd == 0 ? type(uint256).max : block.timestamp + _secondsUntilOpenEnd
-    });
-
+    packs[packId] = packState;
+    rewards[packId] = rewardsInPack;
+    
     // Mint packs to creator.
     _mint(msg.sender, packId, packTotalSupply, "");
 
-    emit PackCreated(_rewardContract, msg.sender, packId, _packURI, packTotalSupply, openLimit[packId].start, openLimit[packId].end);
-    emit PackRewards(packId, _rewardContract, _rewardIds, _rewardAmounts);
+    emit PackCreated(_rewardContract, msg.sender, packState, rewardsInPack);
   }
 
   /// @notice Lets a pack owner open a single pack.
   function openPack(uint _packId) external {
 
-    require(block.timestamp >= openLimit[_packId].start && block.timestamp <= openLimit[_packId].end, "Pack: the window to open packs has not started or closed.");
+    require(LINK.balanceOf(address(this)) >= fees, "Pack: Not enough LINK to fulfill randomness request.");
     require(balanceOf(msg.sender, _packId) > 0, "Pack: sender owns no packs of the given packId.");
-    require(!pendingRandomnessRequests[_packId][msg.sender], "Pack: must wait for the pending pack to be opened.");
+    require(!pendingRequests[_packId][msg.sender], "Pack: must wait for the pending pack to be opened.");
 
-    asyncOpenPack(msg.sender, _packId);
+    PackState memory packState = packs[_packId];
+
+    require(
+      block.timestamp >= packState.openStart && block.timestamp <= packState.openEnd, 
+      "Pack: the window to open packs has not started or closed."
+    );
+
+    // Send random number request.
+    vrfSeed += 1;
+    bytes32 requestId = requestRandomness(keyHash, fees, uint(keccak256(abi.encode(msg.sender, vrfSeed))));
+
+    // Update state to reflect the Chainlink VRF request.
+    randomnessRequests[requestId] = RandomnessRequest({
+      packId: _packId,
+      opener: msg.sender
+    });
+
+    pendingRequests[_packId][msg.sender] = true;
+
+    emit PackOpenRequest(_packId, msg.sender, requestId);
   }
 
-  /// @dev Called by protocol RNG when using an external random number provider. Completes a pack opening.
-  function fulfillRandomness(uint _requestId, uint _randomness) external {
-    require(msg.sender == address(rng()), "Pack: only the appointed RNG can fulfill random number requests.");
+  /// @dev Called by Chainlink VRF with a random number, completing the opening of a pack.
+  function fulfillRandomness(bytes32 _requestId, uint _randomness) internal override {
+    
+    RandomnessRequest memory request = randomnessRequests[_requestId];
 
     // Pending request completed
-    pendingRandomnessRequests[randomnessRequests[_requestId].packId][randomnessRequests[_requestId].opener] = false;
+    pendingRequests[request.packId][request.opener] = false;
 
     // Burn the pack being opened.
     _burn(msg.sender, randomnessRequests[_requestId].packId, 1);
 
-    emit PackOpened(randomnessRequests[_requestId].packId, msg.sender);
-
     // Get tokenId of the reward to distribute.
-    uint rewardId = getReward(randomnessRequests[_requestId].packId, _randomness);
-    address rewardContract = rewards[randomnessRequests[_requestId].packId].source;
+    Rewards memory rewardsInPack = rewards[request.packId];
+
+    uint rewardId = getReward(request.packId, _randomness, rewardsInPack);
 
     // Distribute the reward to the pack opener.
-    IERC1155(rewardContract).safeTransferFrom(address(this), randomnessRequests[_requestId].opener, rewardId, 1, "");
+    IERC1155(rewardsInPack.source).safeTransferFrom(address(this), request.opener, rewardId, 1, "");
 
-    emit RewardDistributed(rewardContract, randomnessRequests[_requestId].opener, randomnessRequests[_requestId].packId, rewardId);
+    emit PackOpenFulfilled(request.packId, request.opener, _requestId, rewardsInPack.source, rewardId);
   }
 
   /**
   *   Internal functions.
   **/
 
-  /// @dev Initiates a pack opening; sends a random number request to an external RNG service.
-  function asyncOpenPack(address _opener, uint _packId) internal {
-    // Request external service for a random number. Store the request ID.
-    uint requestId = rng().requestRandomNumber();
-
-    randomnessRequests[requestId] = RandomnessRequest({
-      opener: _opener,
-      packId: _packId
-    });
-
-    pendingRandomnessRequests[_packId][_opener] = true;
-  }
-
   /// @dev Returns a reward tokenId using `_randomness` provided by RNG.
-  function getReward(uint _packId, uint _randomness) internal returns (uint rewardTokenId) {
+  function getReward(uint _packId, uint _randomness, Rewards memory _rewardsInPack) internal returns (uint rewardTokenId) {
 
-    uint prob = _randomness % _sumArr(rewards[_packId].amountsPacked);
+    uint prob = _randomness % _sumArr(_rewardsInPack.amountsPacked);
     uint step = 0;
 
-    for(uint i = 0; i < rewards[_packId].tokenIds.length; i++) {
+    for(uint i = 0; i < _rewardsInPack.tokenIds.length; i += 1) {
 
-      if(prob < (rewards[_packId].amountsPacked[i] + step)) {
+      if(prob < (_rewardsInPack.amountsPacked[i] + step)) {
         
-        rewardTokenId = rewards[_packId].tokenIds[i];
+        // Return the reward's tokenId
+        rewardTokenId = _rewardsInPack.tokenIds[i];
+        
+        // Update amount of reward available in pack.
         rewards[_packId].amountsPacked[i] -= 1;
         break;
 
       } else {
-        step += rewards[_packId].amountsPacked[i];
+        step += _rewardsInPack.amountsPacked[i];
       }
     }
   }
@@ -222,7 +242,7 @@ contract Pack is ERC1155, IERC1155Receiver {
   /// @dev Updates a token's total supply.
   function _beforeTokenTransfer(
     address,
-    address from,
+    address,
     address to,
     uint256[] memory ids,
     uint256[] memory amounts,
@@ -231,36 +251,25 @@ contract Pack is ERC1155, IERC1155Receiver {
     internal
     override
   {
-    // Increase total supply if tokens are being minted.
-    if(from == address(0)) {
-      
-      for(uint i = 0; i < ids.length; i++) {
-        totalSupply[ids[i]] += amounts[i];
-      }
 
-      // Decrease total supply if tokens are being burned.
-    } else if (to == address(0)) {
+    // Decrease total supply if tokens are being burned.
+    if (to == address(0)) {
 
-      for(uint i = 0; i < ids.length; i++) {
-        totalSupply[ids[i]] -= amounts[i];
+      for(uint i = 0; i < ids.length; i += 1) {
+        packs[ids[i]].currentSupply -= amounts[i];
       }
     }
   }
 
   /// @dev Returns and then increments `currentTokenId`
-  function _tokenId() internal returns (uint tokenId) {
+  function _newPackId() internal returns (uint tokenId) {
     tokenId = nextTokenId;
     nextTokenId += 1;
   }
 
-  /// @dev Returns pack protocol's RNG.
-  function rng() internal view returns (IRNG) {
-    return IRNG(controlCenter.getModule(RNG));
-  }
-
   /// @dev Returns the sum of all elements in the array
   function _sumArr(uint[] memory arr) internal pure returns (uint sum) {
-    for(uint i = 0; i < arr.length; i++) {
+    for(uint i = 0; i < arr.length; i += 1) {
       sum += arr[i];
     }
   }
@@ -269,20 +278,30 @@ contract Pack is ERC1155, IERC1155Receiver {
   *   Getter functions.
   **/
 
+  /// @dev Returns the creator of a set of packs
+  function creator(uint _packId) external view returns (address) {
+    return packs[_packId].creator;
+  }
+
   /// @dev Returns a pack for the given pack tokenId
-  function getPackById(uint _packId) external view returns (PackInfo memory pack) {
-    pack = PackInfo({
-      packId: _packId,
-      creator: creator[_packId],
-      uri: tokenURI[_packId],
-      currentSupply: totalSupply[_packId],
-      openStart: openLimit[_packId].start,
-      openEnd: openLimit[_packId].end
-    });
+  function getPack(uint _packId) external view returns (PackState memory pack) {
+    pack = packs[_packId];
   }
 
   /// @dev Returns the the underlying rewards of a pack
-  function getRewards(uint _packId) external view returns (address source, uint[] memory tokenIds, uint[] memory amountsPacked) {
+  function getRewardsInPack(uint _packId) external view returns (address source, uint[] memory tokenIds, uint[] memory amountsPacked) {
+    source = rewards[_packId].source; 
+    tokenIds = rewards[_packId].tokenIds;
+    amountsPacked = rewards[_packId].amountsPacked;
+  }
+
+  /// @dev Returns a pack with its underlying rewards
+  function getPackWithRewards(uint _packId) 
+    external 
+    view 
+    returns (PackState memory pack, address source, uint[] memory tokenIds, uint[] memory amountsPacked) 
+  {
+    pack = packs[_packId];
     source = rewards[_packId].source; 
     tokenIds = rewards[_packId].tokenIds;
     amountsPacked = rewards[_packId].amountsPacked;
