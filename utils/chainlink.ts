84765fae5750034f306930b2e38955fad329d157
nkrishang <kn1457@nyu.edu>
2021-08-06 18:14:32 -0400

    format chainlink vars

--- utils/chainlink.ts
+++ utils/chainlink.ts
@@ -8,8 +8,8 @@ export const chainlinkVarsRinkeby = {
 }
 
 export const chainlinkVarsMainnet = {
-    vrfCoordinator: '0xf0d54349aDdcf704F77AE15b96510dEA15cb7952',
-    linkTokenAddress: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
-    keyHash: '0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445',
-    fees: ethers.utils.parseEther("2")
-  }
+  vrfCoordinator: '0xf0d54349aDdcf704F77AE15b96510dEA15cb7952',
+  linkTokenAddress: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+  keyHash: '0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445',
+  fees: ethers.utils.parseEther("2")
+}
