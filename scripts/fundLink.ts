683e69133dd2be84759d68cafe486f3864ac5ab6
nkrishang <kn1457@nyu.edu>
2021-08-09 13:02:26 -0400

    Adde script to fund Pack with LINK

---
+++ scripts/fundLink.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+import { BigNumber, Contract } from 'ethers';
+
+import { chainlinkVars } from "../utils/chainlink";
+import { addresses } from "../utils/contracts";
+
+import LinkTokenABI from "../abi/LinkTokenInterface.json";
+
+/// NOTE: set the right netowrk.
+
+async function main() {
+  const [funder] = await ethers.getSigners();
+
+  // Get LINK contract
+  const { linkTokenAddress } = chainlinkVars.rinkeby;
+  const { rinkeby: pack} = addresses;
+  const linkContract: Contract = await ethers.getContractAt(LinkTokenABI, linkTokenAddress);
+
+  // Fund pack contract.
+  const amountToFund: BigNumber = ethers.utils.parseEther("1");
+  const transferTx = await linkContract.connect(funder).transfer(pack, amountToFund);
+  console.log("Transferring link: ", transferTx.hash);
+
+  await transferTx.wait()
+}
