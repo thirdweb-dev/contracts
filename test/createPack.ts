287b946081c4407b996c5fedcc47ca43b2ff57d7
nkrishang <kn1457@nyu.edu>
2021-08-18 13:28:52 -0400

    final createPack test: passes

--- test/createPack.ts
+++ test/createPack.ts
@@ -13,8 +13,8 @@ describe("Testing createPack", function() {
   let creator: Signer;
 
   // Contracts
+  let pack: Contract;
   let rewards: Contract;
-  let pack: Contract
 
   // Reward parameterrs
   const rewardURIs: string[] = [
@@ -28,7 +28,12 @@ describe("Testing createPack", function() {
   const packURI = "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1";
   const openStartAndEnd: number = 0;
 
-  before(async () => {
+  // Expected results
+  const expectedPackId: number = 0;
+  const expectedPackSupply: number = rewardSupplies.reduce((a,b) => a + b);
+  const expectedRewardIds: number[] = [0, 1, 2]
+
+  beforeEach(async () => {
 
     // Fork rinkeby
     await forkFrom(9075707, "rinkeby");
@@ -62,13 +67,138 @@ describe("Testing createPack", function() {
     rewards = await Rewards_factory.connect(creator).deploy(pack.address);
   })
 
-  it("Should create a pack successfully", async () => {
+  describe("Revert cases", function() {
 
-    const gasEst = await rewards.estimateGas.createPack(rewardURIs, rewardSupplies, packURI, openStartAndEnd, openStartAndEnd)
+    it("Should revert if an unequal number of reward IDs and amounts are supplied", async () => {
+      await expect(rewards.connect(creator).createPack(
+        rewardURIs.slice(-2),
+        rewardSupplies,
+        packURI,
+        openStartAndEnd,
+        openStartAndEnd
+      ))
+      .to.be.revertedWith("Rewards: Must specify equal number of URIs and supplies.")
+    })
 
-    console.log("Gas to create pack: ", parseInt(gasEst.toString()))
+    it("Should revert if the creator has not approved the contract to transfer reward tokens", async () => {
+      await expect(rewards.connect(creator).createPack(
+        [],
+        [],
+        packURI,
+        openStartAndEnd,
+        openStartAndEnd
+      ))
+      .to.be.revertedWith("Rewards: Must create at least one reward.")
+    })
+  })
 
-    expect(await rewards.createPack(rewardURIs, rewardSupplies, packURI, openStartAndEnd, openStartAndEnd))
-      .to.emit(pack, "PackCreated")
+  describe("Events", function() {
+
+    it("Should emit RewardsCreated with the relevant reward info", async () => {
+      expect(await await rewards.connect(creator).createPack(
+        rewardURIs,
+        rewardSupplies,
+        packURI,
+        openStartAndEnd,
+        openStartAndEnd
+      ))
+      .to.emit(rewards, "RewardsCreated")
+      .withArgs(await creator.getAddress(), expectedRewardIds, rewardURIs, rewardSupplies)
+    })
+
+    it("Should emit PackCreated with pack related info", async () => {
+
+      const packCreatedPromise = new Promise((resolve, reject) => {
+        pack.on("PackCreated", async (_rewardContract, _creator, _packState, _rewards) => {
+        
+          expect(_rewardContract).to.equal(rewards.address)
+          expect(_creator).to.equal(await creator.getAddress())
+          expect(_packState.packId).to.equal(expectedPackId)
+          expect(_packState.uri).to.equal(packURI)
+          expect(_packState.currentSupply).to.equal(expectedPackSupply)
+
+          expect(_rewards.source).equal(rewards.address)
+
+          resolve(null);
+        })
+
+        setTimeout(() => {
+          reject(new Error("Event timeout: PackCreated"))
+        }, 5000)
+      })
+
+      await rewards.connect(creator).createPack(
+        rewardURIs,
+        rewardSupplies,
+        packURI,
+        openStartAndEnd,
+        openStartAndEnd
+      )
+
+      await packCreatedPromise;
+    })
+  })
+
+  describe("Balances", function() {
+    beforeEach(async () => {
+      // Create pack
+      await rewards.connect(creator).createPack(
+        rewardURIs,
+        rewardSupplies,
+        packURI,
+        openStartAndEnd,
+        openStartAndEnd
+      )
+    })
+
+    it("Should lock all reward tokens in the Pack contract", async () => {
+
+      for(let i = 0; i < expectedRewardIds.length; i++) {
+        expect(await rewards.balanceOf(await creator.getAddress(), expectedRewardIds[i])).to.equal(0);
+        expect(await rewards.balanceOf(pack.address, expectedRewardIds[i])).to.equal(rewardSupplies[i]);
+      }
+    })
+
+    it("Should mint the total supply of packs to the creator", async () => {
+      expect(await pack.balanceOf(await creator.getAddress(), 0)).to.equal(35)
+    })
+  })
+
+  describe("Contract state changes", function() {
+    beforeEach(async () => {      
+      // Create pack
+      await rewards.connect(creator).createPack(
+        rewardURIs,
+        rewardSupplies,
+        packURI,
+        openStartAndEnd,
+        openStartAndEnd
+      )
+    })
+
+    it("Should update the next token ID to be one more than the pack's tokenId", async () => {
+      expect(await pack.nextTokenId()).to.equal(1)
+    })
+
+    it("Should update the `creator` maping with the address of the creator of the pack", async () => {
+      expect(await pack.creator(0)).to.equal(await creator.getAddress())
+    })
+
+    it("Should update the `tokenURI` mapping with the URI of the pack", async () => {
+      expect(await pack.uri(0)).to.equal(packURI)
+    })
+
+    it("Should update the `rewards` mapping with the reward contract, IDs and amounts packed", async () => {
+      const rewardsInPack = await pack.getRewardsInPack(0);
+
+      expect(rewardsInPack.source).to.equal(rewards.address)
+      expect(rewardsInPack.tokenIds.length).to.equal(expectedRewardIds.length)
+      expect(rewardsInPack.amountsPacked.length).to.equal(rewardSupplies.length)
+
+      for(let i = 0; i < expectedRewardIds.length; i++) {
+        expect(rewardsInPack.tokenIds[i]).to.equal(expectedRewardIds[i])
+        expect(rewardsInPack.amountsPacked[i]).to.equal(rewardSupplies[i])
+      }
+    })
   })
 })
