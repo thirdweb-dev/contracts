be68548ce5dce54bf9c353d9295e424820d23420
nkrishang <kn1457@nyu.edu>
2021-08-24 15:50:56 -0400

    CreatePack event test script

--- scripts/txs/createPack.ts
+++ scripts/txs/createPack.ts
@@ -6,55 +6,58 @@ import { addresses } from '../../utils/contracts';
 /// NOTE: set the right network you want.
 
 // Transaction parameters.
-const rewardContract = addresses.mumbai.rewards;
 const packURI = "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1";
-const rewardIds: number[] = [0,1,2]
+const rewardURIs: string[] = [
+  "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1",
+  "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/2",
+  "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/3"
+]
 const rewardSupplies: number[] = [5, 10, 20];
 const openStartAndEnd: number = 0;
 
 async function createPack(
-  rewardContract: string,
   packURI: string,
-  rewardIds: number[],
+  rewardURIs: string[],
   rewardSupplies: number[],
 
   openStart: number,
   openEnd: number
 ) {
 
-  const manualGasPrice: BigNumber = ethers.utils.parseUnits("5", "gwei");
+  // const manualGasPrice: BigNumber = ethers.utils.parseUnits("5", "gwei");
 
   // Get signer.
   const [caller] = await ethers.getSigners();
 
   // Get contract instances connected to wallet.
-  const { mumbai: { pack, rewards } } = addresses;
+  const { rinkeby: { pack, rewards } } = addresses;
   const packContract: Contract = await ethers.getContractAt("Pack", pack)
   const rewardsContract: Contract = await ethers.getContractAt("Rewards", rewards)
 
-  // Approve Handler to transfer reward tokens.
-  const approveHandlerTx = await rewardsContract.connect(caller).setApprovalForAll(pack, true, { gasPrice: manualGasPrice});
-  console.log("Approving Pack for reward tokens: ", approveHandlerTx.hash);
-  await approveHandlerTx.wait()
-
   // Create packs with rewards and list packs for sale.
-  const createPackTx = await packContract.connect(caller).createPack(
-    packURI,
-    rewardContract,
-    rewardIds,
+  const createPackTx = await rewardsContract.connect(caller).createPackAtomic(
+    rewardURIs,
     rewardSupplies,
+    packURI,
     openStart,
-    openEnd,
-    { gasPrice: manualGasPrice}
-  );
+    openEnd
+  )
   console.log("Create pack: ", createPackTx.hash);
-  await createPackTx.wait();
+  const receipt = await createPackTx.wait();
+
+  const topic = packContract.interface.getEventTopic("PackCreated")
+  const log = receipt.logs.find((x: any) => x.topics.indexOf(topic) >= 0);
+  const packCreatedEvent: any = packContract.interface.parseLog(log);
+
+  console.log(packCreatedEvent)
+
+  const packId = packCreatedEvent.args.packState.packId
+  console.log(packId)
 }
 
 createPack(
-  rewardContract,
   packURI,
-  rewardIds,
+  rewardURIs,
   rewardSupplies,
   openStartAndEnd,
   openStartAndEnd
