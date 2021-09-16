4dc6c89f85999bd9aff3ce5dde7dc3e80f24a633
nkrishang <kn1457@nyu.edu>
2021-09-16 19:31:24 -0400

    Rename test helpers

--- contracts/test-helpers/VanillaCoin.sol
+++ contracts/test-helpers/VanillaCoin.sol
@@ -3,6 +3,6 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract Coin is ERC20PresetMinterPauser {
+contract VanillaCoin is ERC20PresetMinterPauser {
     constructor() ERC20PresetMinterPauser("ExampleCoin", "COIN") {}
 }
--- contracts/test-helpers/VanillaNFT.sol
+++ contracts/test-helpers/VanillaNFT.sol
@@ -3,6 +3,6 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 
-contract NFT is ERC721PresetMinterPauserAutoId {
+contract VanillaNFT is ERC721PresetMinterPauserAutoId {
     constructor() ERC721PresetMinterPauserAutoId("ExampleNFT", "EXNFT", "ipfs://dummyuri") {}
 }
