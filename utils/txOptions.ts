5c91e1a203c5078650153e27c3bae746ae84238f
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-09-20 22:32:41 +0000

    Polygon deploy

--- utils/txOptions.ts
+++ utils/txOptions.ts
@@ -1,12 +1,16 @@
 import { ethers } from "hardhat";
 
 export const txOptions = {
+  polygon: {
+    gasPrice: ethers.utils.parseUnits("10", "gwei"),
+  },
+
   matic: {
-    gasPrice: ethers.utils.parseUnits("5", "gwei"),
+    gasPrice: ethers.utils.parseUnits("10", "gwei"),
   },
 
   mumbai: {
-    gasPrice: ethers.utils.parseUnits("5", "gwei"),
+    gasPrice: ethers.utils.parseUnits("10", "gwei"),
   },
 
   rinkeby: {
