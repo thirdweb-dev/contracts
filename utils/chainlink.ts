f6cf2d2b3e2113c28fa1c8dac58e8153cf18b1d4
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-09-30 22:54:49 +0000

    chainlink / address update

--- utils/chainlink.ts
+++ utils/chainlink.ts
@@ -7,6 +7,12 @@ export const chainlinkVars = {
     keyHash: "0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da",
     fees: ethers.utils.parseEther("0.0001"),
   },
+  polygon: {
+    vrfCoordinator: "0x3d2341ADb2D31f1c5530cDC622016af293177AE0",
+    linkTokenAddress: "0xb0897686c545045aFc77CF20eC7A532E3120E0F1",
+    keyHash: "0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da",
+    fees: ethers.utils.parseEther("0.0001"),
+  },
   mumbai: {
     vrfCoordinator: "0x8C7382F9D8f56b33781fE506E897a4F1e2d17255",
     linkTokenAddress: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
