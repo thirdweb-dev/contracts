4a538a7610b1a4d06829fce57d7394abaf3c36f2
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2021-11-16 13:12:17 -0500

    createDirectListing test

--- test/v2/Market/createDirectListing.ts
+++ test/v2/Market/createDirectListing.ts
@@ -4,11 +4,11 @@ import { expect } from "chai";
 // Contract Types
 import { Forwarder } from "../../../typechain/Forwarder";
 import { AccessNFT } from "../../../typechain/AccessNFT";
-import { MarketWithAuction, ListingParametersStruct } from "../../../typechain/MarketWithAuction";
+import { MarketWithAuction, ListingParametersStruct, ListingStruct } from "../../../typechain/MarketWithAuction";
 import { Coin } from "../../../typechain/Coin";
 
 // Types
-import { BigNumber } from "ethers";
+import { BigNumber, BigNumberish } from "ethers";
 import { BytesLike } from "@ethersproject/bytes";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
@@ -117,6 +117,112 @@ describe("List token for sale: Direct Listing", function () {
       await expect(
         marketv2.connect(creator).createListing(listingParams)
       ).to.be.revertedWith("Market: must own and approve to transfer tokens.")
+    })
+  })
+
+  describe("Events", function() {
+
+    beforeEach(async () => {
+      // Approve Market to transfer tokens
+      await accessNft.connect(creator).setApprovalForAll(marketv2.address, true);
+    })
+
+    it("Should emit NewListing with all relevant info", async () => {
+
+      const expectedListingId: BigNumberish = await marketv2.totalListings();
+      
+      const eventPromise = new Promise((resolve, reject) => {
+        
+        marketv2.on("NewListing", (
+          _assetContract,
+          _seller,
+          _listingId,
+          _listing
+        ) => {
+
+          expect(_assetContract).to.equal(accessNft.address);
+          expect(_seller).to.equal(creator.address);
+          expect(_listingId).to.equal(expectedListingId);
+
+          expect(_listing.listingId).to.equal(expectedListingId);
+          expect(_listing.tokenOwner).to.equal(creator.address);
+          expect(_listing.assetContract).to.equal(accessNft.address);
+          expect(_listing.tokenId).to.equal(rewardId);
+          
+          expect(_listing.endTime).to.be.gt(_listing.startTime);
+
+          expect(_listing.quantity).to.equal(amountOfTokenToList)
+          expect(_listing.currency).to.equal(coin.address);
+          expect(_listing.reservePricePerToken).to.equal(0);
+          expect(_listing.buyoutPricePerToken).to.equal(price);
+          expect(_listing.tokensPerBuyer).to.equal(tokensPerBuyer);
+          expect(_listing.tokenType).to.equal(0) // 0 == ERC1155
+          expect(_listing.listingType).to.equal(ListingType.Direct);
+
+          resolve(null);
+        })
+
+        setTimeout(() => {
+          reject(new Error("Timeout: NewListing"))
+        }, 10000);
+      })
+
+      await marketv2.connect(creator).createListing(listingParams);
+      await eventPromise.catch(e => console.error(e));
+    })
+  })
+
+  describe("Token balances", function() {
+
+    beforeEach(async () => {
+      // Approve Market to transfer tokens
+      await accessNft.connect(creator).setApprovalForAll(marketv2.address, true);
+    })
+
+    it("Should not change the lister's token balance", async () => {
+      const balBefore: BigNumberish = await accessNft.balanceOf(creator.address, rewardId);
+      await marketv2.connect(creator).createListing(listingParams);
+      const balAfter: BigNumberish = await accessNft.balanceOf(creator.address, rewardId);
+      
+      expect(balBefore).to.equal(balAfter)
+    })
+  })
+
+  describe("Contract state", function () {
+    
+    beforeEach(async () => {
+      // Approve Market to transfer tokens
+      await accessNft.connect(creator).setApprovalForAll(marketv2.address, true);
+    })
+
+    it("Should increment the total listings on the contract", async () => {
+      const totalListingsBefore: BigNumberish = await marketv2.totalListings();
+      await marketv2.connect(creator).createListing(listingParams);
+      const totalListingAfter: BigNumberish = await marketv2.totalListings();
+
+      expect(totalListingAfter.sub(totalListingsBefore)).to.equal(1)
+    })
+
+    it("Should store the listing's state", async () => {
+      const listingId: BigNumberish = await marketv2.totalListings();
+      await marketv2.connect(creator).createListing(listingParams);
+
+      const _listing: ListingStruct = await marketv2.listings(listingId);
+
+      expect(_listing.listingId).to.equal(listingId);
+      expect(_listing.tokenOwner).to.equal(creator.address);
+      expect(_listing.assetContract).to.equal(accessNft.address);
+      expect(_listing.tokenId).to.equal(rewardId);
+          
+      expect(_listing.endTime).to.be.gt(_listing.startTime);
+
+      expect(_listing.quantity).to.equal(amountOfTokenToList)
+      expect(_listing.currency).to.equal(coin.address);
+      expect(_listing.reservePricePerToken).to.equal(0);
+      expect(_listing.buyoutPricePerToken).to.equal(price);
+      expect(_listing.tokensPerBuyer).to.equal(tokensPerBuyer);
+      expect(_listing.tokenType).to.equal(0) // 0 == ERC1155
+      expect(_listing.listingType).to.equal(ListingType.Direct);
     })
   })
 });
