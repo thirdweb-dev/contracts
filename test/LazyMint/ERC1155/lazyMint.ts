b18dd82067e9d46e624394095a246acafe51bc52
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2021-12-09 18:20:02 -0500

    fix test format

--- test/LazyMint/ERC1155/lazyMint.ts
+++ test/LazyMint/ERC1155/lazyMint.ts
@@ -37,52 +37,49 @@ describe("Test: lazy mint tokens", function() {
     lazyMintERC1155 = contracts.lazyMintERC1155;
   })
 
-  describe("Lazy mint with default base URI convention", function() {
+  describe("Revert cases", function() {      
+    it("Should revert if caller does not have minter role", async () => {
+      await expect(
+        lazyMintERC1155.connect(defaultSaleRecipient).lazyMint(amountToLazyMint, baseURI)
+      ).to.be.revertedWith("LazyMintERC1155: not minter.");
+    })
+  })
 
-    describe("Revert cases", function() {      
-      it("Should revert if caller does not have minter role", async () => {
-        await expect(
-          lazyMintERC1155.connect(defaultSaleRecipient).lazyMint(amountToLazyMint, baseURI)
-        ).to.be.revertedWith("LazyMintERC1155: not minter.");
-      })
+  describe("Events", function() {
+    it("Should emit LazyMintedTokens", async () => {
+      const expectedStartTokenId: BigNumber = await lazyMintERC1155.nextTokenIdToMint();
+      const expectedEndTokenId: BigNumber = expectedStartTokenId.add(amountToLazyMint).sub(1);
+
+      await expect(
+        lazyMintERC1155.connect(protocolAdmin).lazyMint(amountToLazyMint, baseURI)
+      ).to.emit(lazyMintERC1155, "LazyMintedTokens")
+      .withArgs(
+        ...Object.values({
+          startTokenId: expectedStartTokenId,
+          endTokenId: expectedEndTokenId,
+          baseURI: baseURI
+        })
+      )
+    })
+  })
+
+  describe("Contract state", function() {
+    
+    it("Should increment the 'nextTokenIdToMint' by the amount of tokens lazy minted", async () => {
+      const nextIdToMintBefore: BigNumber = await lazyMintERC1155.nextTokenIdToMint();
+      await lazyMintERC1155.connect(protocolAdmin).lazyMint(amountToLazyMint, baseURI);
+      const nextIdToMintAfter: BigNumber = await lazyMintERC1155.nextTokenIdToMint();
+
+      expect(nextIdToMintAfter).to.equal(nextIdToMintBefore.add(amountToLazyMint));
     })
 
-    describe("Events", function() {
-      it("Should emit LazyMintedTokens", async () => {
-        const expectedStartTokenId: BigNumber = await lazyMintERC1155.nextTokenIdToMint();
-        const expectedEndTokenId: BigNumber = expectedStartTokenId.add(amountToLazyMint).sub(1);
-
-        await expect(
-          lazyMintERC1155.connect(protocolAdmin).lazyMint(amountToLazyMint, baseURI)
-        ).to.emit(lazyMintERC1155, "LazyMintedTokens")
-        .withArgs(
-          ...Object.values({
-            startTokenId: expectedStartTokenId,
-            endTokenId: expectedEndTokenId,
-            baseURI: baseURI
-          })
-        )
-      })
-    })
-
-    describe("Contract state", function() {
+    it("Should return the URI for any token in the baseURI + tokenId convention", async () => {
+      const tokenIdToCheck: BigNumber = BigNumber.from(9999);
+      const expectedURI: string = baseURI + tokenIdToCheck.toString();
       
-      it("Should increment the 'nextTokenIdToMint' by the amount of tokens lazy minted", async () => {
-        const nextIdToMintBefore: BigNumber = await lazyMintERC1155.nextTokenIdToMint();
-        await lazyMintERC1155.connect(protocolAdmin).lazyMint(amountToLazyMint, baseURI);
-        const nextIdToMintAfter: BigNumber = await lazyMintERC1155.nextTokenIdToMint();
+      await lazyMintERC1155.connect(protocolAdmin).lazyMint(amountToLazyMint, baseURI);
 
-        expect(nextIdToMintAfter).to.equal(nextIdToMintBefore.add(amountToLazyMint));
-      })
-
-      it("Should return the URI for any token in the baseURI + tokenId convention", async () => {
-        const tokenIdToCheck: BigNumber = BigNumber.from(9999);
-        const expectedURI: string = baseURI + tokenIdToCheck.toString();
-        
-        await lazyMintERC1155.connect(protocolAdmin).lazyMint(amountToLazyMint, baseURI);
-
-        expect(await lazyMintERC1155.uri(tokenIdToCheck)).to.equal(expectedURI)
-      })
+      expect(await lazyMintERC1155.uri(tokenIdToCheck)).to.equal(expectedURI)
     })
   })
 })
