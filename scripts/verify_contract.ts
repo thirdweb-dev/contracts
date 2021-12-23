8853288d7d948c8d52802b73648c5df28241dc8d
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-12-23 22:01:27 +0000

    update scripts

--- scripts/verify_contract.ts
+++ scripts/verify_contract.ts
@@ -1,30 +1,55 @@
 import hre, { ethers } from "hardhat";
 
 async function verify() {
-  // txhash = "0x8f71c6b772a20340d33a9695dc5de1c72eedf36016daebcaaa72cfffe2c17fd0";
-  // type = "LazyNFT";
-  const txhash = "0x2bec0e703e343ba11e4f0de3ddb6d84a86aab784652c6751cb64ce2d19c975ef";
-  const type = "LazyMintERC1155";
+  const txhash = "";
+  const types = [
+    "Coin",
+    "LazyMintERC721",
+    "LazyMintERC1155",
+    "VotingGovernor",
+    "Marketplace",
+    "LazyNFT",
+    "NFT",
+    "NFTCollection",
+    "Royalty",
+    "Pack",
+    "DataStore",
+    "Market",
+  ];
 
   const tx = await ethers.provider.getTransaction(txhash);
   const txdata = tx.data;
   const address = (tx as any).creates;
-  const contract = await ethers.getContractFactory(type);
-  const { bytecode, deployedBytecode } = await hre.artifacts.readArtifact(type);
 
-  // make sure that deployed bytecode matches with contract bytecode
-  if (txdata.indexOf(bytecode) === -1) {
-    console.log("txdata", txdata, txdata.length);
-    console.log("bytecode", bytecode, bytecode.length);
-    throw new Error("invalid contract bytecode");
+  console.log("txhash", txhash, address, txdata.length);
+
+  for (const type of types) {
+    let contract;
+    try {
+      contract = await ethers.getContractFactory(type);
+    } catch (e) {
+      console.log("invalid artifacts", type);
+      continue;
+    }
+    const { bytecode, deployedBytecode } = await hre.artifacts.readArtifact(type);
+
+    // make sure that deployed bytecode matches with contract bytecode
+    if (txdata.indexOf(bytecode) === -1) {
+      //console.log("txdata", txdata, txdata.length);
+      //console.log("bytecode", bytecode, bytecode.length);
+      console.log("invalid contract bytecode", type);
+      continue;
+    }
+
+    const paramsData = `0x${txdata.substring(bytecode.length)}`;
+    const paramsDeployed = ethers.utils.defaultAbiCoder.decode(contract.interface.deploy.inputs, paramsData);
+    const paramsArguments = paramsDeployed.toString().split(",");
+    await hre.run("verify:verify", {
+      address,
+      constructorArguments: [...paramsArguments],
+    });
+    break;
   }
-  const paramsData = `0x${txdata.substring(bytecode.length)}`;
-  const paramsDeployed = ethers.utils.defaultAbiCoder.decode(contract.interface.deploy.inputs, paramsData);
-  const paramsArguments = paramsDeployed.toString().split(",");
-  await hre.run("verify:verify", {
-    address,
-    constructorArguments: [...paramsArguments],
-  });
 }
 
 verify()
