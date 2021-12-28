ec947d3b775f3eac4cf7b543680057ffb60c50f8
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2021-12-28 16:17:03 -0500

    add setClaimConditions

--- contracts/lazy_mint/ERC20/LazyMintERC20.sol
+++ contracts/lazy_mint/ERC20/LazyMintERC20.sol
@@ -30,6 +30,9 @@ contract LazyMintERC20 is ILazyMintERC20, Coin, ReentrancyGuard {
     /// @dev The % of primary sales collected by the contract as fees.
     uint128 public feeBps;
 
+    /// @dev The claim conditions at any given moment.
+    ClaimConditions public claimConditions;
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -56,7 +59,109 @@ contract LazyMintERC20 is ILazyMintERC20, Coin, ReentrancyGuard {
         feeBps = _feeBps;
     }
 
-        function claim(uint256 _quantity, bytes32[] calldata _proofs) external payable {}
+    //      =====   Public functions  =====
 
-        function setClaimConditions(ClaimCondition[] calldata _conditions) external {}
+    /// @dev At any given moment, returns the uid for the active claim condition.
+    function getIndexOfActiveCondition() public view returns (uint256) {
+        uint256 totalConditionCount = claimConditions.totalConditionCount;
+
+        require(totalConditionCount > 0, "no public mint condition.");
+
+        for (uint256 i = totalConditionCount; i > 0; i -= 1) {
+            if (block.timestamp >= claimConditions.claimConditionAtIndex[i - 1].startTimestamp) {
+                return i - 1;
+            }
+        }
+
+        revert("no active mint condition.");
+    }
+
+    //      =====   External functions  =====
+
+    function claim(uint256 _quantity, bytes32[] calldata _proofs) external payable {}
+
+    /// @dev Lets a module admin update mint conditions without resetting the restrictions.
+    function updateClaimConditions(ClaimCondition[] calldata _conditions) external onlyModuleAdmin {
+        resetClaimConditions(_conditions);
+
+        emit NewClaimConditions(_conditions);
+    }
+
+    /// @dev Lets a module admin set mint conditions.
+    function setClaimConditions(ClaimCondition[] calldata _conditions) external onlyModuleAdmin {
+        uint256 numOfConditionsSet = resetClaimConditions(_conditions);
+        resetTimestampRestriction(numOfConditionsSet);
+
+        emit NewClaimConditions(_conditions);
+    }
+
+    //      =====   Getter functions  =====
+
+    /// @dev Returns the current active mint condition for a given tokenId.
+    function getTimestampForNextValidClaim(uint256 _index, address _claimer)
+        public
+        view
+        returns (uint256 nextValidTimestampForClaim)
+    {
+        uint256 timestampIndex = _index + claimConditions.timstampLimitIndex;
+        uint256 timestampOfLastClaim = claimConditions.timestampOfLastClaim[_claimer][timestampIndex];
+
+        unchecked {
+            nextValidTimestampForClaim =
+                timestampOfLastClaim +
+                claimConditions.claimConditionAtIndex[_index].waitTimeInSecondsBetweenClaims;
+
+            if (nextValidTimestampForClaim < timestampOfLastClaim) {
+                nextValidTimestampForClaim = type(uint256).max;
+            }
+        }
+    }
+
+    /// @dev Returns the  mint condition for a given tokenId, at the given index.
+    function getClaimConditionAtIndex(uint256 _index) external view returns (ClaimCondition memory mintCondition) {
+        mintCondition = claimConditions.claimConditionAtIndex[_index];
+    }
+
+    //      =====   Internal functions  =====
+
+    /// @dev Overwrites the current claim conditions with new claim conditions
+    function resetClaimConditions(ClaimCondition[] calldata _conditions) internal returns (uint256 indexForCondition) {
+        // make sure the conditions are sorted in ascending order
+        uint256 lastConditionStartTimestamp;
+
+        for (uint256 i = 0; i < _conditions.length; i++) {
+            require(
+                lastConditionStartTimestamp == 0 || lastConditionStartTimestamp < _conditions[i].startTimestamp,
+                "startTimestamp must be in ascending order."
+            );
+            require(_conditions[i].maxClaimableSupply > 0, "max mint supply cannot be 0.");
+
+            claimConditions.claimConditionAtIndex[indexForCondition] = ClaimCondition({
+                startTimestamp: _conditions[i].startTimestamp,
+                maxClaimableSupply: _conditions[i].maxClaimableSupply,
+                supplyClaimed: 0,
+                waitTimeInSecondsBetweenClaims: _conditions[i].waitTimeInSecondsBetweenClaims,
+                pricePerToken: _conditions[i].pricePerToken,
+                currency: _conditions[i].currency,
+                merkleRoot: _conditions[i].merkleRoot
+            });
+
+            indexForCondition += 1;
+            lastConditionStartTimestamp = _conditions[i].startTimestamp;
+        }
+
+        uint256 totalConditionCount = claimConditions.totalConditionCount;
+        if (indexForCondition < totalConditionCount) {
+            for (uint256 j = indexForCondition; j < totalConditionCount; j += 1) {
+                delete claimConditions.claimConditionAtIndex[j];
+            }
+        }
+
+        claimConditions.totalConditionCount = indexForCondition;
+    }
+
+    /// @dev Updates the `timstampLimitIndex` to reset the time restriction between claims, for a claim condition.
+    function resetTimestampRestriction(uint256 _factor) internal {
+        claimConditions.timstampLimitIndex += _factor;
+    }
 }
