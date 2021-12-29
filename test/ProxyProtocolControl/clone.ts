9ed50948c37a24aa24f3a23c1c83d8c18e84160f
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2021-12-29 15:06:42 -0500

    test withdrawal of funds: erc20 and native token

--- test/ProxyProtocolControl/clone.ts
+++ test/ProxyProtocolControl/clone.ts
@@ -5,7 +5,9 @@ import { solidity } from "ethereum-waffle";
 // Types
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Log } from "@ethersproject/abstract-provider";
-import { ControlDeployer, Forwarder, ProtocolControl, Registry } from "typechain";
+import { Coin, ControlDeployer, Forwarder, ProtocolControl, Registry } from "typechain";
+import { getContracts } from "../../utils/tests/getContracts";
+import { BigNumber } from "ethers";
 
 use(solidity);
 
@@ -125,5 +127,74 @@ describe("Deploy proxies for ProtocolControl using ControlDeployer", function() 
       )
     ).to.be.revertedWith("Initializable: contract is already initialized")
 
+  })
+
+  describe("Test the withdrawal of funds from the clones", function() {
+    
+    // This works too: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+    const NATIVE_TOKEN_ADDRESS: string = ethers.constants.AddressZero;
+
+    // Additional signers
+    let erc20Signer: SignerWithAddress;
+    let fundsReceiver: SignerWithAddress;
+
+    // ERC20 token contract
+    let erc20Token: Coin;
+
+    // Test params
+    const amountToSendToClones: BigNumber = ethers.utils.parseEther("10");
+
+    // Sadly, just `before` refuses to work.
+    beforeEach(async () => {
+      // Get ERC20 signer
+      [erc20Signer, fundsReceiver] = (await ethers.getSigners()).slice(5);
+
+      // Get ERC20 token
+      const contracts = await getContracts(protocolProvider, erc20Signer);
+      erc20Token = contracts.coin;
+
+      // Mint ERC20 tokens to clones
+      await erc20Token.connect(erc20Signer).mint(clone_1.address, amountToSendToClones);
+
+      // Send ether to clones
+      await erc20Signer.sendTransaction({
+        to: clone_1.address,
+        value: amountToSendToClones
+      })
+    })
+
+    it("Should allow withdrawing native token from clones", async () => {
+      const fundsReceiverBalBefore: BigNumber = await ethers.provider.getBalance(fundsReceiver.address);
+      const registryTreasuryBalBefore: BigNumber = await ethers.provider.getBalance(await registry.treasury());
+
+      await clone_1.connect(protocolAdmin_1).withdrawFunds(fundsReceiver.address, NATIVE_TOKEN_ADDRESS);
+
+      const MAX_BPS: BigNumber = await clone_1.MAX_BPS();
+      const registryFeeBps: BigNumber = await registry.getFeeBps(clone_1.address);
+      const registryFee: BigNumber = (amountToSendToClones.mul(registryFeeBps)).div(MAX_BPS);
+
+      const fundsReceiverBalAfter: BigNumber = await ethers.provider.getBalance(fundsReceiver.address);
+      const registryTreasuryBalAfter: BigNumber = await ethers.provider.getBalance(await registry.treasury());
+
+      expect(fundsReceiverBalAfter).to.equal(fundsReceiverBalBefore.add(amountToSendToClones.sub(registryFee)));
+      expect(registryTreasuryBalAfter).to.equal(registryTreasuryBalBefore.add(registryFee));
+    })
+
+    it("Should allow withdrawing ERC20 token from clones", async () => {
+      const fundsReceiverBalBefore: BigNumber = await erc20Token.balanceOf(fundsReceiver.address);
+      const registryTreasuryBalBefore: BigNumber = await erc20Token.balanceOf(await registry.treasury());
+
+      await clone_1.connect(protocolAdmin_1).withdrawFunds(fundsReceiver.address, erc20Token.address);
+
+      const MAX_BPS: BigNumber = await clone_1.MAX_BPS();
+      const registryFeeBps: BigNumber = await registry.getFeeBps(clone_1.address);
+      const registryFee: BigNumber = (amountToSendToClones.mul(registryFeeBps)).div(MAX_BPS);
+
+      const fundsReceiverBalAfter: BigNumber = await erc20Token.balanceOf(fundsReceiver.address);
+      const registryTreasuryBalAfter: BigNumber = await erc20Token.balanceOf(await registry.treasury());
+
+      expect(fundsReceiverBalAfter).to.equal(fundsReceiverBalBefore.add(amountToSendToClones.sub(registryFee)));
+      expect(registryTreasuryBalAfter).to.equal(registryTreasuryBalBefore.add(registryFee));
+    })
   })
 })
