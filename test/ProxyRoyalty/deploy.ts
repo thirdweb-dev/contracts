dabd7654ff5421b60c3c79e40c5c3707d10217c5
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2021-12-31 16:37:57 -0500

    test proxy pattern for royalty

---
+++ test/ProxyRoyalty/deploy.ts
@@ -0,0 +1,86 @@
+import { ethers } from "hardhat";
+import { expect, use } from "chai";
+import { solidity } from "ethereum-waffle";
+
+// Types
+import { ProtocolControl, Registry, Royalty } from "typechain";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+// Helpers
+import { getContracts } from "../../utils/tests/getContracts";
+
+use(solidity);
+
+describe("Deploy proxies for Royalty module", function() {
+
+  // Signers
+  let protocolProvider: SignerWithAddress;
+  let protocolAdmin_dummy: SignerWithAddress;
+  let protocolAdmin_1: SignerWithAddress;
+  let protocolAdmin_2: SignerWithAddress;
+
+  // Contracts
+  let registry: Registry;
+  let controlCenter: ProtocolControl;
+  let royaltyContract: Royalty;
+  let proxyForRoyalty: Royalty;
+
+  // Initialization params
+  let trustedForwarderAddr: string;
+  let uri: string;
+  let payees: string[];
+  let shares: number[];
+
+  function scaleShares(_shares: number[]): number[] {
+    return _shares.map(val => val * 10_000);
+  }
+
+  function sharesPostFee(_shares: number[], _feeBps: number): number[] {
+    return _shares.map(val => val - (val * _feeBps) / 10_000);
+  }
+
+  before(async () => {
+    // Get signers
+    [protocolProvider, protocolAdmin_dummy, protocolAdmin_1, protocolAdmin_2] = await ethers.getSigners();
+
+    // Get initialize params
+    const contracts = await getContracts(protocolProvider, protocolAdmin_dummy);
+    registry = contracts.registry;
+    controlCenter = contracts.protocolControl;
+    trustedForwarderAddr = contracts.forwarder.address;
+    uri = "ipfs://"
+    payees = [protocolAdmin_dummy.address, protocolAdmin_1.address, protocolAdmin_2.address]
+    shares = [20, 40, 40];
+
+    // Deploy Royalty implementation
+    royaltyContract = await ethers.getContractFactory("Royalty").then(f => f.deploy());
+  })
+
+  beforeEach(async () => {
+    const thirdwebProxy = await ethers.getContractFactory("ThirdwebProxy")
+      .then(f => f.deploy(
+        royaltyContract.address,
+        royaltyContract.interface.encodeFunctionData("initialize", [controlCenter.address, trustedForwarderAddr, uri, payees, shares])
+      )
+    );
+
+    proxyForRoyalty = await ethers.getContractAt("Royalty", thirdwebProxy.address) as Royalty;
+  })
+
+  it("Should initialize the proxied royalty contract", async () => {
+    expect(await proxyForRoyalty.contractURI()).to.equal(uri);
+    expect(await proxyForRoyalty.totalShares()).to.equal(scaleShares(shares).reduce((a,b) => a+b));
+
+    const sharesAfterFee: number[] = sharesPostFee(scaleShares(shares), (await registry.getFeeBps(controlCenter.address)).toNumber()) 
+
+    for(let i = 0; i < payees.length; i += 1) {
+      expect(await proxyForRoyalty.shares(payees[i])).to.equal(sharesAfterFee[i]);
+    }
+  })
+
+  it("Should revert on trying to re-initialize contract via proxy", async () => {
+    await expect(
+      proxyForRoyalty.initialize(controlCenter.address, trustedForwarderAddr, uri, payees, shares)
+    ).to.be.revertedWith("Initializable: contract is already initialized");
+  })
+})
