73dd093286e9c4a57961fcc616051bc1912cb0a3
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2022-01-10 12:53:16 -0800

    Add deploy test for SigMint1155

--- test/SignatureMint/ERC1155/deploy.ts
+++ test/SignatureMint/ERC1155/deploy.ts
@@ -3,7 +3,7 @@ import { expect, use } from "chai";
 import { solidity } from "ethereum-waffle";
 
 // Contract Types
-import { SignatureMint721 } from "typechain/SignatureMint721";
+import { SignatureMint1155 } from "typechain/SignatureMint1155";
 
 // Types
 import { BigNumber, BytesLike } from "ethers";
@@ -14,18 +14,16 @@ import { getContracts, Contracts } from "../../../utils/tests/getContracts";
 
 use(solidity);
 
-describe("Initial state of SignatureMint721 on deployment", function () {
+describe("Initial state of SignatureMint1155 on deployment", function () {
   // Signers
   let protocolProvider: SignerWithAddress;
   let protocolAdmin: SignerWithAddress;
   let defaultSaleRecipient: SignerWithAddress;
 
   // Contracts
-  let sigMint721: SignatureMint721;
+  let sigMint1155: SignatureMint1155;
 
   // Deployment params
-  const name: string = "Name";
-  const symbol: string = "LAZY";
   const contractURI: string = "ipfs://contractURI/";
   let trustedForwarderAddr: string;
   let protocolControlAddr: string;
@@ -44,14 +42,12 @@ describe("Initial state of SignatureMint721 on deployment", function () {
     protocolControlAddr = contracts.protocolControl.address;
     nativeTokenWrapperAddr = contracts.weth.address;
 
-    sigMint721 = (await ethers
-      .getContractFactory("SignatureMint721")
+    sigMint1155 = (await ethers
+      .getContractFactory("SignatureMint1155")
       .then(f =>
         f
           .connect(protocolAdmin)
           .deploy(
-            name,
-            symbol,
             contractURI,
             protocolControlAddr,
             trustedForwarderAddr,
@@ -60,22 +56,22 @@ describe("Initial state of SignatureMint721 on deployment", function () {
             royaltyBps,
             feeBps,
           ),
-      )) as SignatureMint721;
+      )) as SignatureMint1155;
   });
 
   it("Should grant all relevant roles to contract deployer", async () => {
-    const DEFAULT_ADMIN_ROLE: BytesLike = await sigMint721.DEFAULT_ADMIN_ROLE();
-    const MINTER_ROLE: BytesLike = await sigMint721.MINTER_ROLE();
-    const TRANSFER_ROLE: BytesLike = await sigMint721.TRANSFER_ROLE();
+    const DEFAULT_ADMIN_ROLE: BytesLike = await sigMint1155.DEFAULT_ADMIN_ROLE();
+    const MINTER_ROLE: BytesLike = await sigMint1155.MINTER_ROLE();
+    const TRANSFER_ROLE: BytesLike = await sigMint1155.TRANSFER_ROLE();
 
-    expect(await sigMint721.hasRole(DEFAULT_ADMIN_ROLE, protocolAdmin.address)).to.be.true;
-    expect(await sigMint721.hasRole(MINTER_ROLE, protocolAdmin.address)).to.be.true;
-    expect(await sigMint721.hasRole(TRANSFER_ROLE, protocolAdmin.address)).to.be.true;
+    expect(await sigMint1155.hasRole(DEFAULT_ADMIN_ROLE, protocolAdmin.address)).to.be.true;
+    expect(await sigMint1155.hasRole(MINTER_ROLE, protocolAdmin.address)).to.be.true;
+    expect(await sigMint1155.hasRole(TRANSFER_ROLE, protocolAdmin.address)).to.be.true;
   });
 
   it("Should initialize relevant state variables in the constructor", async () => {
-    expect(await sigMint721.nativeTokenWrapper()).to.equal(nativeTokenWrapperAddr);
-    expect(await sigMint721.defaultSaleRecipient()).to.equal(defaultSaleRecipient.address);
-    expect(await sigMint721.contractURI()).to.equal(contractURI);
+    expect(await sigMint1155.nativeTokenWrapper()).to.equal(nativeTokenWrapperAddr);
+    expect(await sigMint1155.defaultSaleRecipient()).to.equal(defaultSaleRecipient.address);
+    expect(await sigMint1155.contractURI()).to.equal(contractURI);
   });
 });
