4b0c5971f48eb08672ef134b1fc1f70dfc6e0040
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2022-01-12 11:06:40 -0800

    Distinguish royalty and sales related info

--- contracts/ThirdwebFees.sol
+++ contracts/ThirdwebFees.sol
@@ -11,12 +11,11 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import { IThirdwebModule } from "./IThirdwebModule.sol";
 
 contract ThirdwebFees is Initializable, OwnableUpgradeable, UUPSUpgradeable {
-
     /// @dev Max bps in the thirdweb system
-    uint128 constant public MAX_BPS = 10_000;
+    uint128 public constant MAX_BPS = 10_000;
 
     /// @dev The threshold for thirdweb fees.
-    uint128 constant public maxFeeBps = 3000;
+    uint128 public constant maxFeeBps = 3000;
 
     /// @dev The default fee bps for thirdweb modules.
     uint256 public defaultFeeBps;
@@ -27,57 +26,95 @@ contract ThirdwebFees is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     /// @dev Mapping from particular module instance => whether thirdweb takes no fees
     mapping(address => bool) public takeNoFee;
 
-    /// @dev Mapping from module type => fee bps
-    mapping(bytes32 => uint256) public feeBpsByModuleType;
+    /// @dev Mapping from module type => royalty fee bps
+    mapping(bytes32 => mapping(FeeType => uint256)) public feeBpsByModuleType;
 
-    /// @dev Mapping from particular module instance => fee bps
-    mapping(address => uint256) public feeBpsByModuleInstance;
+    /// @dev Mapping from particular module instance => royalty fee bps
+    mapping(address => mapping(FeeType => uint256)) public feeBpsByModuleInstance;
 
-    /// @dev Mapping from module type => fee recipient
-    mapping(bytes32 => address) public feeRecipientByModuleType;
+    /// @dev Mapping from module type => royalty fee recipient
+    mapping(bytes32 => mapping(FeeType => address)) public feeRecipientByModuleType;
 
-    /// @dev Mapping from particular module instance => fee recipient
-    mapping(address => address) public feeRecipientByModuleInstance;
+    /// @dev Mapping from particular module instance => royalty fee recipient
+    mapping(address => mapping(FeeType => address)) public feeRecipientByModuleInstance;
 
     /// @dev Emitted when fee is set for a module type
-    event FeeForModuleType(uint256 feeBps, bytes32 moduleType);
+    event FeeForModuleType(uint256 feeBps, bytes32 moduleType, FeeType feeType);
 
     /// @dev Emitted when fee is set for a module instance
-    event FeeForModuleInstance(uint256 feeBps, address moduleInstance);
+    event FeeForModuleInstance(uint256 feeBps, address moduleInstance, FeeType feeType);
 
     /// @dev Emitted when fee recipient is set for a module type
-    event RecipientForModuleType(address recipient, bytes32 moduleType);
+    event RecipientForModuleType(address recipient, bytes32 moduleType, FeeType feeType);
 
     /// @dev Emitted when fee recipient is set for a module instance
-    event RecipientForModuleInstance(address recipient, address moduleInstance);
+    event RecipientForModuleInstance(address recipient, address moduleInstance, FeeType feeType);
+
+    enum FeeType { Sales, Royalty }
 
     modifier onlyValidFee(uint256 _feeBps) {
         require(_feeBps <= maxFeeBps, "fees too high");
         _;
     }
 
-    /// @dev Returns the fee bps for a module address
-    function getFeeBps(address _module) external view returns (uint256) {
-
+    /// @dev Returns the royalty bps for a module address
+    function getRoyaltyFeeBps(address _module) external view returns (uint256) {
         bytes32 moduleType = IThirdwebModule(_module).moduleType();
-        
-        if(takeNoFee[_module]) {
+
+        if (takeNoFee[_module]) {
             return 0;
-        
-        } else if (feeBpsByModuleInstance[_module] > 0) {
-            return feeBpsByModuleInstance[_module];
-        
-        } else if (feeBpsByModuleType[moduleType] > 0) {
-            return feeBpsByModuleType[moduleType];
-        
+        } else if (feeBpsByModuleInstance[_module][FeeType.Royalty] > 0) {
+            return feeBpsByModuleInstance[_module][FeeType.Royalty];
+        } else if (feeBpsByModuleType[moduleType][FeeType.Royalty] > 0) {
+            return feeBpsByModuleType[moduleType][FeeType.Royalty];
         } else {
             return defaultFeeBps;
         }
     }
 
+    /// @dev Returns the royalty fee recipient for a module address
+    function getRoyaltyFeeRecipient(address _module) external view returns (address) {
+        bytes32 moduleType = IThirdwebModule(_module).moduleType();
+
+        if (feeRecipientByModuleInstance[_module][FeeType.Royalty] != address(0)) {
+            return feeRecipientByModuleInstance[_module][FeeType.Royalty];
+        } else if (feeRecipientByModuleType[moduleType][FeeType.Royalty] != address(0)) {
+            return feeRecipientByModuleType[moduleType][FeeType.Royalty];
+        } else {
+            return defaultFeeRecipient;
+        }
+    }
+
+    /// @dev Returns the royalty bps for a module address
+    function getSalesFeeBps(address _module) external view returns (uint256) {
+        bytes32 moduleType = IThirdwebModule(_module).moduleType();
+
+        if (takeNoFee[_module]) {
+            return 0;
+        } else if (feeBpsByModuleInstance[_module][FeeType.Sales] > 0) {
+            return feeBpsByModuleInstance[_module][FeeType.Sales];
+        } else if (feeBpsByModuleType[moduleType][FeeType.Sales] > 0) {
+            return feeBpsByModuleType[moduleType][FeeType.Sales];
+        } else {
+            return defaultFeeBps;
+        }
+    }
+
+    /// @dev Returns the royalty fee recipient for a module address
+    function getSalesFeeRecipient(address _module) external view returns (address) {
+        bytes32 moduleType = IThirdwebModule(_module).moduleType();
+
+        if (feeRecipientByModuleInstance[_module][FeeType.Sales] != address(0)) {
+            return feeRecipientByModuleInstance[_module][FeeType.Sales];
+        } else if (feeRecipientByModuleType[moduleType][FeeType.Sales] != address(0)) {
+            return feeRecipientByModuleType[moduleType][FeeType.Sales];
+        } else {
+            return defaultFeeRecipient;
+        }
+    }
+
     /// @dev Initializes contract state.
     function initialize(uint256 _defaultFeeBps, address _defaultFeeRecipient) external initializer {
-        
         // Initialize inherited contracts.
         __Ownable_init();
 
@@ -85,28 +122,32 @@ contract ThirdwebFees is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         defaultFeeRecipient = _defaultFeeRecipient;
     }
 
-    /// @dev Lets the owner set fee bps for module type.
-    function setFeeForModuleType(bytes32 _moduleType, uint256 _feeBps) external onlyOwner onlyValidFee(_feeBps) {
-        feeBpsByModuleType[_moduleType] = _feeBps;
-        emit FeeForModuleType(_feeBps, _moduleType);
+    /// @dev Lets the owner set royalty fee bps for module type.
+    function setFeeForModuleType(bytes32 _moduleType, uint256 _feeBps, FeeType _feeType) external onlyOwner onlyValidFee(_feeBps) {
+        feeBpsByModuleType[_moduleType][_feeType] = _feeBps;
+        emit FeeForModuleType(_feeBps, _moduleType, FeeType.Royalty);
     }
 
-    /// @dev Lets the owner set fee bps for a particular module instance.
-    function setFeeForModuleInstance(address _moduleInstance, uint256 _feeBps) external onlyOwner onlyValidFee(_feeBps) {
-        feeBpsByModuleInstance[_moduleInstance] = _feeBps;
-        emit FeeForModuleInstance(_feeBps, _moduleInstance);
+    /// @dev Lets the owner set royalty fee bps for a particular module instance.
+    function setFeeForModuleInstance(address _moduleInstance, uint256 _feeBps, FeeType _feeType)
+        external
+        onlyOwner
+        onlyValidFee(_feeBps)
+    {
+        feeBpsByModuleInstance[_moduleInstance][_feeType] = _feeBps;
+        emit FeeForModuleInstance(_feeBps, _moduleInstance, FeeType.Royalty);
     }
 
-    /// @dev Lets the owner set fee recipient for module type.
-    function setRecipientForModuleType(bytes32 _moduleType, address _recipient) external onlyOwner {
-        feeRecipientByModuleType[_moduleType] = _recipient;
-        emit RecipientForModuleType(_recipient, _moduleType);
+    /// @dev Lets the owner set sales fee recipient for module type.
+    function setRecipientForModuleType(bytes32 _moduleType, address _recipient, FeeType _feeType) external onlyOwner {
+        feeRecipientByModuleType[_moduleType][_feeType] = _recipient;
+        emit RecipientForModuleType(_recipient, _moduleType, _feeType);
     }
 
-    /// @dev Lets the owner set fee recipient for a particular module instance.
-    function setRecipientForModuleInstance(address _moduleInstance, address _recipient) external onlyOwner {
-        feeRecipientByModuleInstance[_moduleInstance] = _recipient;
-        emit RecipientForModuleInstance(_recipient, _moduleInstance);
+    /// @dev Lets the owner set sales fee recipient for a particular module instance.
+    function setRecipientForModuleInstance(address _moduleInstance, address _recipient, FeeType _feeType) external onlyOwner {
+        feeRecipientByModuleInstance[_moduleInstance][_feeType] = _recipient;
+        emit RecipientForModuleInstance(_recipient, _moduleInstance, _feeType);
     }
 
     /// @dev Runs on every upgrade.
