08fe49bc8752d352db0441701a2ce7d0b6b20695
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2022-01-12 17:06:36 -0800

    create thirdweb registry and update factory

--- contracts/ThirdwebFactory.sol
+++ contracts/ThirdwebFactory.sol
@@ -1,49 +1,65 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-// Thirdweb Proxy
-import { ThirdwebProxy } from "./ThirdwebProxy.sol";
+// Thirdweb contracts
+import "./ThirdwebProxy.sol";
+import "./ThirdwebRegistry.sol"; 
 
 // Utils
 import "@openzeppelin/contracts/utils/Create2.sol";
 
 contract ThirdwebFactory {
 
+    address public thirdwebRegistry;
+
     /// @dev Emitted when a proxy is deployed.
     event ProxyDeployed(address indexed implementation, address indexed proxy, address indexed deployer);
 
-    constructor() {}
+    mapping(bytes32 => mapping(uint256 => address)) public modules;
+
+    constructor() {
+        bytes memory registryBytecode = abi.encodePacked(
+            type(ThirdwebRegistry).creationCode,
+            abi.encode(address(this))
+        );
+
+        bytes32 salt = keccak256(abi.encodePacked(msg.sender, block.number));
+
+        thirdwebRegistry = Create2.deploy(0, salt, registryBytecode);
+    }
 
     /// @dev Deploys a proxy that points to the given implementation.
-    function deployProxy(address _implementation,bytes memory _data) external {
+    function deployProxy(bytes32 _moduleType, uint256 _version, bytes memory _data) external {
 
-        // TODO: where to store: 'this deployer owns / has deployed this proxy?'
-        // TODO: should implement a check whether `_implementation` is a contract deployed via thirdweb?
+        address implementation = modules[_moduleType][_version];
 
         bytes memory proxyBytecode = abi.encodePacked(
             type(ThirdwebProxy).creationCode,
-            abi.encode(_implementation, _data)
+            abi.encode(implementation, _data)
         );
         bytes32 salt = keccak256(abi.encodePacked(msg.sender, block.number));
 
         address deployedProxy = Create2.deploy(0, salt, proxyBytecode);
 
-        emit ProxyDeployed(_implementation, deployedProxy, msg.sender);
+        ThirdwebRegistry(thirdwebRegistry).updateDeployments(_moduleType, deployedProxy, msg.sender);
+
+        emit ProxyDeployed(implementation, deployedProxy, msg.sender);
     }
 
     /// @dev Deploys a proxy at a deterministic address by taking in `salt` as a parameter.
-    function deployProxyDeterministic(address _implementation,bytes memory _data, bytes32 _salt) external {
+    function deployProxyDeterministic(bytes32 _moduleType, uint256 _version, bytes memory _data, bytes32 _salt) external {
 
-        // TODO: where to store: 'this deployer owns / has deployed this proxy?'
-        // TODO: should implement a check whether `_implementation` is a contract deployed via thirdweb?
+        address implementation = modules[_moduleType][_version];
 
         bytes memory proxyBytecode = abi.encodePacked(
             type(ThirdwebProxy).creationCode,
-            abi.encode(_implementation, _data)
+            abi.encode(implementation, _data)
         );
 
         address deployedProxy = Create2.deploy(0, _salt, proxyBytecode);
 
-        emit ProxyDeployed(_implementation, deployedProxy, msg.sender);
+        ThirdwebRegistry(thirdwebRegistry).updateDeployments(_moduleType, deployedProxy, msg.sender);
+
+        emit ProxyDeployed(implementation, deployedProxy, msg.sender);
     }
 }
---
+++ contracts/ThirdwebRegistry.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./thirdweb-presets/TWAccessControl.sol";
+
+contract ThirdwebRegistry is TWAccessControl {
+
+    bytes32 public constant FACTORY_ROLE = keccak256("FACTORY_ROLE");
+
+    struct Deployments {
+        uint256 totalDeployments;
+        
+        mapping(uint256 => address) moduleAddress;
+    }
+
+    mapping(bytes32 => mapping(address => Deployments)) public deployments;
+
+    event ModuleDeployed(bytes32 indexed moduleType, address indexed moduleAddress, address indexed deployer);
+
+    constructor(address _thirdwebFactory) {
+        _setupRole(FACTORY_ROLE, _thirdwebFactory);
+    }
+
+    function updateDeployments(
+        bytes32 _moduleType,
+        address _moduleAddress,
+        address _deployer
+    ) external {
+
+        require(hasRole(FACTORY_ROLE, msg.sender), "not factory");
+        
+        uint256 id = deployments[_moduleType][_deployer].totalDeployments;
+        deployments[_moduleType][_deployer].totalDeployments += 1;
+
+        deployments[_moduleType][_deployer].moduleAddress[id] = _moduleAddress;
+
+        emit ModuleDeployed(_moduleType, _moduleAddress, _deployer);
+    }
+
+    function getAllModulesOfType(bytes32 _moduleType, address _deployer) external view returns (address[] memory allModulesOfType) {
+        uint256 numOfModulesOfType = deployments[_moduleType][_deployer].totalDeployments;
+        allModulesOfType = new address[](numOfModulesOfType);
+
+        for(uint256 i = 0; i < numOfModulesOfType; i += 1) {
+            allModulesOfType[i] = deployments[_moduleType][_deployer].moduleAddress[i];
+        }
+    }
+}
