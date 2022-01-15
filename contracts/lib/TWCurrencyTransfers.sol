5471248a738f66dc06288e162a69c8522e1fff34
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2022-01-14 23:18:58 -0800

    Add lib

---
+++ contracts/lib/TWCurrencyTransfers.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+// Helper interfaces
+import { IWETH } from "../interfaces/IWETH.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+library TWCurrencyTransfers {
+    /// @dev The address interpreted as native token of the chain.
+    address public constant NATIVE_TOKEN = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    /// @dev Transfers a given amount of currency.
+    function transferCurrency(
+        address _currency,
+        address _from,
+        address _to,
+        uint256 _amount
+    ) public {
+        if (_amount == 0) {
+            return;
+        }
+
+        if (_currency == NATIVE_TOKEN) {
+            safeTransferNativeToken(_to, _amount);
+        } else {
+            safeTransferERC20(_currency, _from, _to, _amount);
+        }
+    }
+
+    /// @dev Transfer `amount` of ERC20 token from `from` to `to`.
+    function safeTransferERC20(
+        address _currency,
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        if (_from == _to) {
+            return;
+        }
+        uint256 balBefore = IERC20(_currency).balanceOf(_to);
+        bool success = _from == address(this)
+            ? IERC20(_currency).transfer(_to, _amount)
+            : IERC20(_currency).transferFrom(_from, _to, _amount);
+        uint256 balAfter = IERC20(_currency).balanceOf(_to);
+
+        require(success && balAfter == balBefore + _amount, "currency transfer failed.");
+    }
+
+    /// @dev Transfers `amount` of native token to `to`.
+    function safeTransferNativeToken(address to, uint256 value) internal {
+        (bool success, ) = to.call{ value: value }("");
+        require(success, "native token transfer failed");
+    }
+}
