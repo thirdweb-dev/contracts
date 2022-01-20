fc851e1422cb86b8638c4146a5db61dd94d38038
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-01-20 10:20:56 +0000

    empty fee test

---
+++ src/test/ThirdwebFee.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./utils/BaseTest.t.sol";
+import "contracts/ThirdwebFees.sol";
+
+contract ThirdwebFee is BaseTest {
+    ThirdwebFees fee;
+
+    function setUp() public {}
+}
