8bad7e4c687cde8a3cfdf2c67339fd8afc914a4b
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-01-28 09:19:38 +0000

    fix interface conflict from typechain

--- contracts/interfaces/IThirdwebRoyalty.sol
+++ contracts/interfaces/IThirdwebRoyalty.sol
@@ -5,8 +5,8 @@ import "./IThirdwebModule.sol";
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
 interface IThirdwebRoyalty is IERC2981, IThirdwebModule {
-    /// @dev Returns the platform fee bps and recipient.
-    function getRoyaltyInfo() external view returns (address royaltyRecipient, uint16 royaltyBps);
+    /// @dev Returns the royalty recipient and fee bps.
+    function getRoyaltyInfo() external view returns (address, uint16);
 
     /// @dev Lets a module admin update the royalty bps and recipient.
     function setRoyaltyInfo(address _royaltyRecipient, uint256 _royaltyBps) external;
