2688bf806ac15f894f31158fae4ce65c8ad0d3c4
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-02-01 15:13:36 -0500

    Fix TokenERC721

--- contracts/token/TokenERC721.sol
+++ contracts/token/TokenERC721.sol
@@ -1,181 +1,405 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-//Interface
-import { ITokenERC20 } from "../interfaces/token/ITokenERC20.sol";
+// Interface
+import { ITokenERC721 } from "../interfaces/token/ITokenERC721.sol";
 
 // Token
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 
-// Security
+// Signature utils
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+
+// Access Control + security
 import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
 // Meta transactions
 import "@openzeppelin/contracts-upgradeable/metatx/ERC2771ContextUpgradeable.sol";
 
 // Utils
+import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
 import "../openzeppelin-presets/utils/MulticallUpgradeable.sol";
+import "../lib/CurrencyTransferLib.sol";
 
-contract TokenERC20 is
-    ITokenERC20,
+// Helper interfaces
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
+
+// Thirdweb top-level
+import "../TWFee.sol";
+
+contract TokenERC721 is
     Initializable,
+    ITokenERC721,
+    ReentrancyGuardUpgradeable,
+    EIP712Upgradeable,
     ERC2771ContextUpgradeable,
     MulticallUpgradeable,
-    ERC20BurnableUpgradeable,
-    ERC20PausableUpgradeable,
-    ERC20VotesUpgradeable,
-    AccessControlEnumerableUpgradeable
+    AccessControlEnumerableUpgradeable,
+    ERC721EnumerableUpgradeable
 {
-    bytes32 private constant MODULE_TYPE = bytes32("TokenERC20");
+    using ECDSAUpgradeable for bytes32;
+    using StringsUpgradeable for uint256;
+
+    bytes32 private constant MODULE_TYPE = bytes32("TokenERC721");
     uint256 private constant VERSION = 1;
 
-    bytes32 internal constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 internal constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 internal constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
+    bytes32 private constant TYPEHASH =
+        keccak256(
+            "MintRequest(address to,string uri,uint256 price,address currency,uint128 validityStartTimestamp,uint128 validityEndTimestamp,bytes32 uid)"
+        );
+
+    /// @dev Only TRANSFER_ROLE holders can have tokens transferred from or to them, during restricted transfers.
+    bytes32 private constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
+    /// @dev Only MINTER_ROLE holders can sign off on `MintRequest`s.
+    bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /// @dev Max bps in the thirdweb system
+    uint256 private constant MAX_BPS = 10_000;
+
+    /// @dev The address interpreted as native token of the chain.
+    address private constant NATIVE_TOKEN = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    /// @dev The thirdweb contract with fee related information.
+    TWFee public immutable thirdwebFee;
+
+    /// @dev Owner of the contract (purpose: OpenSea compatibility, etc.)
+    address private _owner;
+
+    /// @dev The token ID of the next token to mint.
+    uint256 public nextTokenIdToMint;
+
+    /// @dev The adress that receives all primary sales value.
+    address public primarySaleRecipient;
+
+    /// @dev The adress that receives all primary sales value.
+    address public platformFeeRecipient;
+
+    /// @dev The recipient of who gets the royalty.
+    address public royaltyRecipient;
+
+    /// @dev The percentage of royalty how much royalty in basis points.
+    uint128 public royaltyBps;
+
+    /// @dev The % of primary sales collected by the contract as fees.
+    uint128 public platformFeeBps;
 
     /// @dev Whether transfers on tokens are restricted.
     bool public isTransferRestricted;
 
-    /// @dev Returns the URI for the storefront-level metadata of the contract.
+    /// @dev Contract level metadata.
     string public contractURI;
 
+    /// @dev Mapping from mint request UID => whether the mint request is processed.
+    mapping(bytes32 => bool) private minted;
+
+    mapping(uint256 => string) private uri;
+
+    /// @dev Checks whether the caller is a module admin.
     modifier onlyModuleAdmin() {
-        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "only module admin role");
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "not module admin.");
         _;
     }
 
-    /// @dev Emitted when restrictions on transfers is updated.
-    event RestrictedTransferUpdated(bool transferable);
+    /// @dev Checks whether the caller has MINTER_ROLE.
+    modifier onlyMinter() {
+        require(hasRole(MINTER_ROLE, _msgSender()), "not minter.");
+        _;
+    }
+
+    constructor(address _thirdwebFee) initializer {
+        thirdwebFee = TWFee(_thirdwebFee);
+    }
 
     /// @dev Initiliazes the contract, like a constructor.
-    function initialize(
+    function intialize(
         address _defaultAdmin,
         string memory _name,
         string memory _symbol,
         string memory _contractURI,
-        address _trustedForwarder
-    ) external initializer {
-        __TokenERC20_init(_defaultAdmin, _name, _symbol, _trustedForwarder, _contractURI);
-    }
-
-    function __TokenERC20_init(
-        address _defaultAdmin,
-        string memory _name,
-        string memory _symbol,
         address _trustedForwarder,
-        string memory _uri
-    ) internal onlyInitializing {
+        address _saleRecipient,
+        address _royaltyReceiver,
+        uint128 _royaltyBps,
+        uint128 _platformFeeBps,
+        address _platformFeeRecipient
+    ) external initializer {
         // Initialize inherited contracts, most base-like -> most derived.
-        __ERC2771Context_init_unchained(_trustedForwarder);
-        __ERC20Permit_init(_name);
-        __ERC20_init_unchained(_name, _symbol);
+        __ReentrancyGuard_init();
+        __EIP712_init("SignatureMint721", "1");
+        __ERC2771Context_init(_trustedForwarder);
+        __ERC721_init(_name, _symbol);
 
-        __TokenERC20_init_unchained(_defaultAdmin, _uri);
-    }
-
-    function __TokenERC20_init_unchained(address _defaultAdmin, string memory _uri) internal onlyInitializing {
         // Initialize this contract's state.
-        contractURI = _uri;
+        royaltyRecipient = _royaltyReceiver;
+        royaltyBps = _royaltyBps;
+        platformFeeRecipient = _platformFeeRecipient;
+        primarySaleRecipient = _saleRecipient;
+        contractURI = _contractURI;
+        platformFeeBps = _platformFeeBps;
 
+        _owner = _defaultAdmin;
         _setupRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
-        _setupRole(TRANSFER_ROLE, _defaultAdmin);
         _setupRole(MINTER_ROLE, _defaultAdmin);
-        _setupRole(PAUSER_ROLE, _defaultAdmin);
+        _setupRole(TRANSFER_ROLE, _defaultAdmin);
     }
+
+    ///     =====   Public functions  =====
 
     /// @dev Returns the module type of the contract.
-    function moduleType() external pure virtual returns (bytes32) {
+    function moduleType() external pure returns (bytes32) {
         return MODULE_TYPE;
     }
 
     /// @dev Returns the version of the contract.
-    function version() external pure virtual returns (uint8) {
+    function version() external pure returns (uint8) {
         return uint8(VERSION);
     }
 
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-        super._afterTokenTransfer(from, to, amount);
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return hasRole(DEFAULT_ADMIN_ROLE, _owner) ? _owner : address(0);
     }
 
-    /// @dev Runs on every transfer.
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20Upgradeable, ERC20PausableUpgradeable) {
-        super._beforeTokenTransfer(from, to, amount);
+    /// @dev Verifies that a mint request is signed by an account holding MINTER_ROLE (at the time of the function call).
+    function verify(MintRequest calldata _req, bytes calldata _signature) public view returns (bool, address) {
+        address signer = recoverAddress(_req, _signature);
+        return (!minted[_req.uid] && hasRole(MINTER_ROLE, signer), signer);
+    }
 
-        if (isTransferRestricted && from != address(0) && to != address(0)) {
-            require(hasRole(TRANSFER_ROLE, from) || hasRole(TRANSFER_ROLE, to), "transfers restricted.");
+    /// @dev Returns the URI for a tokenId
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        return uri[_tokenId];
+    }
+
+    /// @dev Lets an account with MINTER_ROLE mint an NFT.
+    function mintTo(address _to, string calldata _uri) external onlyMinter returns (uint256) {
+        // `_mintTo` is re-used. `mintTo` just adds a minter role check.
+        return _mintTo(_to, _uri);
+    }
+
+    ///     =====   External functions  =====
+
+    /// @dev Distributes accrued royalty and thirdweb fees to the relevant stakeholders.
+    function withdrawFunds(address _currency) external {
+        address recipient = royaltyRecipient;
+        (address twFeeRecipient, uint256 twFeeBps) = thirdwebFee.getFeeInfo(address(this), TWFee.FeeType.Royalty);
+
+        uint256 totalTransferAmount = _currency == NATIVE_TOKEN
+            ? address(this).balance
+            : IERC20(_currency).balanceOf(_currency);
+        uint256 fees = (totalTransferAmount * twFeeBps) / MAX_BPS;
+
+        CurrencyTransferLib.transferCurrency(_currency, address(this), recipient, totalTransferAmount - fees);
+        CurrencyTransferLib.transferCurrency(_currency, address(this), twFeeRecipient, fees);
+
+        emit FundsWithdrawn(recipient, twFeeRecipient, totalTransferAmount, fees);
+    }
+
+    /// @dev Lets the contract accept ether.
+    receive() external payable {
+        emit EtherReceived(msg.sender, msg.value);
+    }
+
+    /// @dev See EIP-2981
+    function royaltyInfo(uint256, uint256 salePrice)
+        external
+        view
+        virtual
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        receiver = address(this);
+        (, uint256 royaltyFeeBps) = thirdwebFee.getFeeInfo(address(this), TWFee.FeeType.Transaction);
+        if (royaltyBps > 0) {
+            royaltyAmount = (salePrice * (royaltyBps + royaltyFeeBps)) / MAX_BPS;
         }
     }
 
-    function _mint(address account, uint256 amount) internal virtual override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-        super._mint(account, amount);
+    /// @dev Mints an NFT according to the provided mint request.
+    function mintWithSignature(MintRequest calldata _req, bytes calldata _signature)
+        external
+        payable
+        nonReentrant
+        returns (uint256 tokenIdMinted)
+    {
+        address signer = verifyRequest(_req, _signature);
+        address receiver = _req.to == address(0) ? _msgSender() : _req.to;
+
+        tokenIdMinted = _mintTo(receiver, _req.uri);
+
+        collectPrice(_req);
+
+        emit MintWithSignature(signer, receiver, tokenIdMinted, _req);
     }
 
-    function _burn(address account, uint256 amount) internal virtual override(ERC20Upgradeable, ERC20VotesUpgradeable) {
-        super._burn(account, amount);
+    //      =====   Setter functions  =====
+
+    /// @dev Lets a module admin set the default recipient of all primary sales.
+    function setPrimarySaleRecipient(address _saleRecipient) external onlyModuleAdmin {
+        primarySaleRecipient = _saleRecipient;
+        emit NewPrimarySaleRecipient(_saleRecipient);
     }
 
-    /**
-     * @dev Creates `amount` new tokens for `to`.
-     *
-     * See {ERC20-_mint}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `MINTER_ROLE`.
-     */
-    function mint(address to, uint256 amount) public virtual {
-        require(hasRole(MINTER_ROLE, _msgSender()), "not minter.");
-        _mint(to, amount);
+    /// @dev Lets a module admin update the royalty bps and recipient.
+    function setRoyaltyInfo(address _royaltyRecipient, uint256 _royaltyBps) external onlyModuleAdmin {
+        require(_royaltyBps <= MAX_BPS, "exceed royalty bps");
+
+        royaltyRecipient = _royaltyRecipient;
+        royaltyBps = uint128(_royaltyBps);
+
+        emit RoyaltyUpdated(_royaltyRecipient, _royaltyBps);
     }
 
-    /**
-     * @dev Pauses all token transfers.
-     *
-     * See {ERC20Pausable} and {Pausable-_pause}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "not pauser.");
-        _pause();
+    /// @dev Lets a module admin update the fees on primary sales.
+    function setPlatformFeeInfo(address _platformFeeRecipient, uint256 _platformFeeBps) external onlyModuleAdmin {
+        require(_platformFeeBps <= MAX_BPS, "bps <= 10000.");
+
+        platformFeeBps = uint64(_platformFeeBps);
+        platformFeeRecipient = _platformFeeRecipient;
+
+        emit PlatformFeeUpdates(_platformFeeRecipient, _platformFeeBps);
     }
 
-    /**
-     * @dev Unpauses all token transfers.
-     *
-     * See {ERC20Pausable} and {Pausable-_unpause}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "not pauser.");
-        _unpause();
-    }
-
-    /// @dev Lets a protocol admin restrict token transfers.
+    /// @dev Lets a module admin restrict token transfers.
     function setRestrictedTransfer(bool _restrictedTransfer) external onlyModuleAdmin {
         isTransferRestricted = _restrictedTransfer;
 
-        emit RestrictedTransferUpdated(_restrictedTransfer);
+        emit TransfersRestricted(_restrictedTransfer);
     }
 
-    /// @dev Sets contract URI for the storefront-level metadata of the contract.
+    /// @dev Lets a module admin set a new owner for the contract. The new owner must be a module admin.
+    function setOwner(address _newOwner) external onlyModuleAdmin {
+        require(hasRole(DEFAULT_ADMIN_ROLE, _newOwner), "new owner not module admin.");
+        address _prevOwner = _owner;
+        _owner = _newOwner;
+
+        emit NewOwner(_prevOwner, _newOwner);
+    }
+
+    /// @dev Lets a module admin set the URI for contract-level metadata.
     function setContractURI(string calldata _uri) external onlyModuleAdmin {
         contractURI = _uri;
+    }
+
+    ///     =====   Getter functions    =====
+
+    /// @dev Returns the platform fee bps and recipient.
+    function getPlatformFeeInfo() external view returns (address, uint16) {
+        return (platformFeeRecipient, uint16(platformFeeBps));
+    }
+
+    /// @dev Returns the platform fee bps and recipient.
+    function getRoyaltyInfo() external view returns (address, uint16) {
+        return (royaltyRecipient, uint16(royaltyBps));
+    }
+
+    ///     =====   Internal functions  =====
+
+    /// @dev Mints an NFT to `to`
+    function _mintTo(address _to, string calldata _uri) internal returns (uint256 tokenIdToMint) {
+        tokenIdToMint = nextTokenIdToMint;
+        nextTokenIdToMint += 1;
+
+        uri[tokenIdToMint] = _uri;
+
+        _mint(_to, tokenIdToMint);
+
+        emit TokenMinted(_to, tokenIdToMint, _uri);
+    }
+
+    /// @dev Returns the address of the signer of the mint request.
+    function recoverAddress(MintRequest calldata _req, bytes calldata _signature) internal view returns (address) {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        TYPEHASH,
+                        _req.to,
+                        keccak256(bytes(_req.uri)),
+                        _req.price,
+                        _req.currency,
+                        _req.validityStartTimestamp,
+                        _req.validityEndTimestamp,
+                        _req.uid
+                    )
+                )
+            ).recover(_signature);
+    }
+
+    /// @dev Verifies that a mint request is valid.
+    function verifyRequest(MintRequest calldata _req, bytes calldata _signature) internal returns (address) {
+        (bool success, address signer) = verify(_req, _signature);
+        require(success, "invalid signature");
+
+        require(
+            _req.validityStartTimestamp <= block.timestamp && _req.validityEndTimestamp >= block.timestamp,
+            "request expired"
+        );
+
+        minted[_req.uid] = true;
+
+        return signer;
+    }
+
+    /// @dev Collects and distributes the primary sale value of tokens being claimed.
+    function collectPrice(MintRequest memory _req) internal {
+        if (_req.price == 0) {
+            return;
+        }
+
+        uint256 totalPrice = _req.price;
+        uint256 platformFees = (totalPrice * platformFeeBps) / MAX_BPS;
+        (address twFeeRecipient, uint256 twFeeBps) = thirdwebFee.getFeeInfo(address(this), TWFee.FeeType.Transaction);
+        uint256 twFee = (totalPrice * twFeeBps) / MAX_BPS;
+
+        if (_req.currency == NATIVE_TOKEN) {
+            require(msg.value == _req.price, "must send total price.");
+        }
+        CurrencyTransferLib.transferCurrency(_req.currency, _msgSender(), platformFeeRecipient, platformFees);
+        CurrencyTransferLib.transferCurrency(_req.currency, _msgSender(), twFeeRecipient, twFee);
+        CurrencyTransferLib.transferCurrency(
+            _req.currency,
+            _msgSender(),
+            primarySaleRecipient,
+            totalPrice - platformFees - twFee
+        );
+    }
+
+    ///     =====   Low-level overrides  =====
+
+    /// @dev Burns `tokenId`. See {ERC721-_burn}.
+    function burn(uint256 tokenId) public virtual {
+        //solhint-disable-next-line max-line-length
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721Burnable: caller is not owner nor approved");
+        _burn(tokenId);
+    }
+
+    /// @dev See {ERC721-_beforeTokenTransfer}.
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721EnumerableUpgradeable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+
+        // if transfer is restricted on the contract, we still want to allow burning and minting
+        if (isTransferRestricted && from != address(0) && to != address(0)) {
+            require(hasRole(TRANSFER_ROLE, from) || hasRole(TRANSFER_ROLE, to), "restricted to TRANSFER_ROLE holders");
+        }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlEnumerableUpgradeable, ERC721EnumerableUpgradeable, IERC165)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId) || interfaceId == type(IERC2981).interfaceId;
     }
 
     function _msgSender()
