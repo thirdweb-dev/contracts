febc12c55ceac86a87fc88145b2b0bd277449c90
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-02-09 08:18:30 +0000

    echina: addresses

--- echidna/Address.sol
+++ echidna/Address.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.11;
 
 contract EchidnaAddress {
+    address public DEPLOYER = address(0x40000);
+    address public SENDER_0 = address(0x50000);
+    address public SENDER_1 = address(0x50001);
+    address public SENDER_2 = address(0x50002);
+
     address public TRUSTED_FORWARDER = address(0x90000);
     address public FEE_0 = address(0x90001);
     address public FEE_1 = address(0x90002);
