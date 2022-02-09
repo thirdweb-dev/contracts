ceec4a467451d9700dbb4d1be9a142f2af2774b2
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-02-09 13:59:56 -0500

    add module-level generic stuff

--- contracts/Multiwrap.sol
+++ contracts/Multiwrap.sol
@@ -2,25 +2,80 @@
 pragma solidity ^0.8.0;
 
 // Base
-import "./openzeppelin-presets/ERC1155PresetUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 
 // Token interfaces
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+// Access Control + security
+import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+// Utils
+import "./openzeppelin-presets/utils/MulticallUpgradeable.sol";
+
+// Meta transactions
+import "@openzeppelin/contracts-upgradeable/metatx/ERC2771ContextUpgradeable.sol";
+
 // Helpers
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 import "./lib/CurrencyTransferLib.sol";
 
 /**
  *      - Wrap multiple ERC721 and ERC20 tokens into 'n' shares (i.e. variable supply of 1 ERC 1155 token)
  */
 
-contract Multiwrap is ERC1155PresetUpgradeable {
+contract Multiwrap is
+    ReentrancyGuardUpgradeable,
+    ERC2771ContextUpgradeable,
+    MulticallUpgradeable,
+    AccessControlEnumerableUpgradeable,
+    ERC1155Upgradeable 
+{
+    bytes32 private constant MODULE_TYPE = bytes32("Multiwrap");
+    uint256 private constant VERSION = 1;
 
+    /// @dev Only TRANSFER_ROLE holders can have tokens transferred from or to them, during restricted transfers.
+    bytes32 private constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
+    /// @dev Only MINTER_ROLE holders can sign off on `MintRequest`s.
+    bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    // Token name
+    string public name;
+
+    // Token symbol
+    string public symbol;
+
+    /// @dev Owner of the contract (purpose: OpenSea compatibility, etc.)
+    address private _owner;
+
+    /// @dev The next token ID of the NFT to mint.
     uint256 public nextTokenIdToMint;
 
+    /// @dev The recipient of who gets the royalty.
+    address public royaltyRecipient;
+
+    /// @dev The percentage of royalty how much royalty in basis points.
+    uint128 public royaltyBps;
+
+    /// @dev Max bps in the thirdweb system
+    uint128 private constant MAX_BPS = 10_000;
+
+    /// @dev Whether transfers on tokens are restricted.
+    bool public isTransferRestricted;
+
+    /// @dev Contract level metadata.
+    string public contractURI;
+
+    /// @dev Token ID => total circulating supply of tokens with that ID.
+    mapping(uint256 => uint256) public totalSupply;
+
+    /// @dev Mapping from tokenId => uri for tokenId
     mapping(uint256 => string) private uriForShares;
+
+    /// @dev Mapping from tokenId => wrapped contents of the token.
     mapping(uint256 => WrappedContents) private wrappedContents;
 
     struct WrappedContents {
@@ -33,12 +88,57 @@ contract Multiwrap is ERC1155PresetUpgradeable {
         uint256[] erc20AmountsToWrap;
     }
 
+    /// @dev Initiliazes the contract, like a constructor.
+    function initialize(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol,
+        string memory _contractURI,
+        address _trustedForwarder,
+        address _royaltyRecipient,
+        uint256 _royaltyBps
+    ) external initializer {
+        // Initialize inherited contracts, most base-like -> most derived.
+        __ReentrancyGuard_init();
+        __ERC2771Context_init(_trustedForwarder);
+        __ERC1155_init("");
+
+        // Initialize this contract's state.
+        name = _name;
+        symbol = _symbol;
+        royaltyRecipient = _royaltyRecipient;
+        royaltyBps = uint128(_royaltyBps);
+        contractURI = _contractURI;
+
+        _owner = _defaultAdmin;
+        _setupRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
+        _setupRole(MINTER_ROLE, _defaultAdmin);
+        _setupRole(TRANSFER_ROLE, _defaultAdmin);
+    }
+
+    ///     =====   Public functions  =====
+
+    /// @dev Returns the module type of the contract.
+    function moduleType() external pure returns (bytes32) {
+        return MODULE_TYPE;
+    }
+
+    /// @dev Returns the version of the contract.
+    function version() external pure returns (uint8) {
+        return uint8(VERSION);
+    }
+
+    /// @dev See ERC1155 - returns the metadata for a token.
     function uri(uint256 _tokenId) public view override returns (string memory) {
         return uriForShares[_tokenId];
     }
 
-    // TODO: initialize function
-    // TODO: TW regulars i.e. `contractURI` etc.
+    /// @dev Alternative method to get the metadata for a token.
+    function tokenURI(uint256 _tokenId) external view returns (string memory) {
+        return uriForShares[_tokenId];
+    }
+
+    ///     =====   External functions  =====
 
     // TODO: require checks.
     function wrap(
@@ -65,7 +165,7 @@ contract Multiwrap is ERC1155PresetUpgradeable {
 
         delete wrappedContents[_tokenId];
 
-        burn(msg.sender, _tokenId, totalSupply(_tokenId));
+        burn(msg.sender, _tokenId, totalSupply[_tokenId]);
 
         transferWrappedAssets(address(this), msg.sender, _wrappedContents);
     }
@@ -107,5 +207,99 @@ contract Multiwrap is ERC1155PresetUpgradeable {
             );
         }
 
+    }
+
+    ///     =====   Low-level overrides  =====
+
+    /// @dev Lets a token owner burn the tokens they own (i.e. destroy for good)
+    function burn(
+        address account,
+        uint256 id,
+        uint256 value
+    ) public virtual {
+        require(
+            account == _msgSender() || isApprovedForAll(account, _msgSender()),
+            "ERC1155: caller is not owner nor approved."
+        );
+
+        _burn(account, id, value);
+    }
+
+    /// @dev Lets a token owner burn multiple tokens they own at once (i.e. destroy for good)
+    function burnBatch(
+        address account,
+        uint256[] memory ids,
+        uint256[] memory values
+    ) public virtual {
+        require(
+            account == _msgSender() || isApprovedForAll(account, _msgSender()),
+            "ERC1155: caller is not owner nor approved."
+        );
+
+        _burnBatch(account, ids, values);
+    }
+
+    /**
+     * @dev See {ERC1155-_beforeTokenTransfer}.
+     */
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+
+        // if transfer is restricted on the contract, we still want to allow burning and minting
+        if (isTransferRestricted && from != address(0) && to != address(0)) {
+            require(hasRole(TRANSFER_ROLE, from) || hasRole(TRANSFER_ROLE, to), "restricted to TRANSFER_ROLE holders.");
+        }
+
+        if (from == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                totalSupply[ids[i]] += amounts[i];
+            }
+        }
+
+        if (to == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                totalSupply[ids[i]] -= amounts[i];
+            }
+        }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlEnumerableUpgradeable, ERC1155Upgradeable)
+        returns (bool)
+    {
+        return
+            super.supportsInterface(interfaceId) ||
+            interfaceId == type(IERC1155Upgradeable).interfaceId ||
+            interfaceId == type(IERC2981).interfaceId;
+    }
+
+    function _msgSender()
+        internal
+        view
+        virtual
+        override(ContextUpgradeable, ERC2771ContextUpgradeable)
+        returns (address sender)
+    {
+        return ERC2771ContextUpgradeable._msgSender();
+    }
+
+    function _msgData()
+        internal
+        view
+        virtual
+        override(ContextUpgradeable, ERC2771ContextUpgradeable)
+        returns (bytes calldata)
+    {
+        return ERC2771ContextUpgradeable._msgData();
     }
 }
