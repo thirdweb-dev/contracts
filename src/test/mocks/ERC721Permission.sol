1d84d1e994678f2e630fd1291c405116bca330ad
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-02-09 08:12:59 +0000

    mock ercs

---
+++ src/test/mocks/ERC721Permission.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+
+contract ERC721Permission is ERC721PresetMinterPauserAutoId {
+    constructor() ERC721PresetMinterPauserAutoId("Mock", "MOCK", "ipfs://BaseURI") {}
+}
