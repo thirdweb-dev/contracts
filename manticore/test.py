ef2e225e7e05c04aaded068658df1c4870cdc6a2
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-02-11 06:05:16 +0000

    manticore: simplify example script

--- manticore/test.py
+++ manticore/test.py
@@ -18,43 +18,33 @@ ETHER = 10 ** 18
 deployer = m.create_account(balance=100 * ETHER, name="deployer")
 user = m.create_account(balance=100 * ETHER, name="user")
 attacker = m.create_account(balance=100 * ETHER, name="attacker")
-
-print(f'[+] Create user wallet. deployer: {hex(deployer.address)}, user: {hex(user.address)}, attacker: {hex(attacker.address)}')
+print(f'[+] Created user wallet. deployer: {hex(deployer.address)}, user: {hex(user.address)}, attacker: {hex(attacker.address)}')
 
 contract = m.solidity_create_contract('./Test.sol', contract_name='Test', owner=user)
-print(f'[+] Deploy contract. address: {hex(contract.address)}')
+print(f'[+] Deployed contract. address: {hex(contract.address)}')
 
+print(f'[+] Declaring symbolic variables.')
 x = m.make_symbolic_value()
 y = m.make_symbolic_value()
 z = m.make_symbolic_value()
 
-print(f'[+] Call contract functions.')
+print(f'[+] Calling contract functions.')
+
 # outline the transactions
-
 contract.set(x, y, z, caller=attacker, value=1*ETHER)
-
-m.get_balance(contract.address)
-
 contract.take(caller=attacker)
 
-m.get_balance(contract.address)
-
 # Let seth know we are not sending more transactions
+print(f'[+] Finalizing transactions.')
 m.finalize(only_alive_states=True)
 
-state_found = False
-for state in m.ready_states:
-    balance_before = state.platform.transactions[2].return_data
-    balance_before = ABI.deserialize("uint", balance_before)
+print(f'[+] Generating transactions traces.')
+num_state_found = 0
+for state in m.all_states:
+    num_state_found += 1
+    balance = state.platform.get_balance(int(user_account))
+    print(state.solve_one(balance))
 
-    balance_after = state.platform.transactions[-1].return_data
-    balance_after = ABI.deserialize("uint", balance_after)
-
-    # Check if it is possible to have balance_after > balance_before
-    condition = Operators.UGT(balance_before, balance_after)
-    if m.generate_testcase(state, name="State", only_if=condition):
-        print("State found. see {}".format(m.workspace))
-        state_found = True
-
-if not state_found:
-    print("[-] No state found")
+print(f"[+] {num_state_found} constraints test cases generated.")
+print(f"[+] Global coverage: {contract.address:x} - {m.global_coverage(contract)}")
+print(f'[+] Results in workspace: {m.workspace}')
