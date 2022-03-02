9910c27761cef388a2070bf875c73721b6ae8509
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-03-02 01:58:56 +0000

    forwarder: minor cleanup

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
 /*
  * @dev Minimal forwarder for GSNv2
  */
 contract Forwarder is MinimalForwarder {
+    // solhint-disable-next-line no-empty-blocks
     constructor() MinimalForwarder() {}
 }
