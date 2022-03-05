5019ff14da810ab20fb16345e82862208e0167b2
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-03-04 22:28:48 -0500

    Make ERC721 burnable

--- src/test/mocks/MockERC721.sol
+++ src/test/mocks/MockERC721.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract MockERC721 is ERC721 {
+contract MockERC721 is ERC721Burnable {
     uint256 nextTokenIdToMint;
 
     constructor() ERC721("MockERC721", "MOCK") {}
