1f6ea718250f71f28db42237ae472d85861ee5b3
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-03-04 21:47:21 -0500

    cleanup mocks

--- src/test/mocks/MockERC721.sol
+++ src/test/mocks/MockERC721.sol
@@ -8,12 +8,12 @@ contract MockERC721 is ERC721 {
 
     constructor() ERC721("MockERC721", "MOCK") {}
 
-    function mint(uint256 _amount) external {
+    function mint(address _receiver, uint256 _amount) external {
         uint256 tokenId = nextTokenIdToMint;
         nextTokenIdToMint += _amount;
 
         for (uint256 i = 0; i < _amount; i += 1) {
-            _mint(msg.sender, tokenId);
+            _mint(_receiver, tokenId);
             tokenId += 1;
         }
     }
