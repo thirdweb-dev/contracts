2e07f4264eeb1b331881d719db1f51bc172aaef6
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-03-30 13:23:11 -0400

    Update multiwrap contract

--- contracts/Multiwrap.sol
+++ contracts/Multiwrap.sol
@@ -1,33 +1,26 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.11;
 
-// Base
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+//  ==========  External imports    ==========
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
-// Interfaces
-import "./interfaces/IMultiwrap.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 
-// Access Control + security
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+
+import "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-// Utils
-import "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
+//  ==========  Internal imports    ==========
 
-// Meta transactions
-import "./openzeppelin-presets/metatx/ERC2771ContextUpgradeable.sol";
-
-// Helpers
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
+import "./interfaces/IMultiwrap.sol";
 import "./lib/CurrencyTransferLib.sol";
-import "./lib/MultiTokenTransferLib.sol";
-
-/**
- *      - Wrap multiple ERC721 and ERC20 tokens into 'n' shares (i.e. variable supply of 1 ERC 1155 token)
- */
+import "./openzeppelin-presets/metatx/ERC2771ContextUpgradeable.sol";
 
 contract Multiwrap is
     IMultiwrap,
@@ -37,54 +30,61 @@ contract Multiwrap is
     AccessControlEnumerableUpgradeable,
     ERC1155HolderUpgradeable,
     ERC721HolderUpgradeable,
-    ERC1155Upgradeable
+    ERC721Upgradeable
 {
+    /*///////////////////////////////////////////////////////////////
+                            State variables
+    //////////////////////////////////////////////////////////////*/
+
     bytes32 private constant MODULE_TYPE = bytes32("Multiwrap");
     uint256 private constant VERSION = 1;
 
-    /// @dev Only TRANSFER_ROLE holders can have tokens transferred from or to them, during restricted transfers.
+    /// @dev Only transfers to or from TRANSFER_ROLE holders are valid, when transfers are restricted.
     bytes32 private constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
-    /// @dev Only MINTER_ROLE holders can sign off on `MintRequest`s.
+    /// @dev Only MINTER_ROLE holders can wrap tokens, when wrapping is restricted.
     bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    // Token name
-    string public name;
-
-    // Token symbol
-    string public symbol;
-
-    /// @dev Owner of the contract (purpose: OpenSea compatibility, etc.)
+    /// @dev Owner of the contract (purpose: OpenSea compatibility)
     address private _owner;
+
+    /// @dev The address of the native token wrapper contract.
+    address private immutable nativeTokenWrapper;
 
     /// @dev The next token ID of the NFT to mint.
     uint256 public nextTokenIdToMint;
 
-    /// @dev The recipient of who gets the royalty.
+    /// @dev The (default) address that receives all royalty value.
     address private royaltyRecipient;
 
-    /// @dev The percentage of royalty how much royalty in basis points.
+    /// @dev The (default) % of a sale to take as royalty (in basis points).
     uint128 private royaltyBps;
 
-    /// @dev Max bps in the thirdweb system
+    /// @dev Max bps in the thirdweb system.
     uint128 private constant MAX_BPS = 10_000;
 
     /// @dev Contract level metadata.
     string public contractURI;
 
-    /// @dev Token ID => royalty recipient and bps for token
+    /*///////////////////////////////////////////////////////////////
+                                Mappings
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Mapping from tokenId of wrapped NFT => royalty recipient and bps for token.
     mapping(uint256 => RoyaltyInfo) private royaltyInfoForToken;
 
-    /// @dev Token ID => total circulating supply of tokens with that ID.
-    mapping(uint256 => uint256) public totalSupply;
+    /// @dev Mapping from tokenId of wrapped NFT => uri for the token.
+    mapping(uint256 => string) private uri;
 
-    /// @dev Token ID => total circulating supply of tokens with that ID.
-    mapping(uint256 => uint256) public totalShares;
+    /// @dev Mapping from tokenId of wrapped NFT => wrapped contents of the token.
+    mapping(uint256 => WrappedContents) private wrappedContents;
 
-    /// @dev Mapping from tokenId => uri for tokenId
-    mapping(uint256 => string) private uriForShares;
+    /*///////////////////////////////////////////////////////////////
+                    Constructor + initializer logic
+    //////////////////////////////////////////////////////////////*/
 
-    /// @dev Mapping from tokenId => wrapped contents of the token.
-    mapping(uint256 => MultiTokenTransferLib.MultiToken) private wrappedContents;
+    constructor(address _nativeTokenWrapper) initializer {
+        nativeTokenWrapper = _nativeTokenWrapper;
+    }
 
     /// @dev Initiliazes the contract, like a constructor.
     function initialize(
@@ -99,24 +99,38 @@ contract Multiwrap is
         // Initialize inherited contracts, most base-like -> most derived.
         __ReentrancyGuard_init();
         __ERC2771Context_init(_trustedForwarders);
-        __ERC1155_init("");
+        __ERC721_init(_name, _symbol);
 
         // Initialize this contract's state.
-        name = _name;
-        symbol = _symbol;
         royaltyRecipient = _royaltyRecipient;
         royaltyBps = uint128(_royaltyBps);
         contractURI = _contractURI;
-
         _owner = _defaultAdmin;
+
         _setupRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
         _setupRole(MINTER_ROLE, _defaultAdmin);
         _setupRole(TRANSFER_ROLE, _defaultAdmin);
+        _setupRole(TRANSFER_ROLE, address(0));
     }
 
-    ///     =====   Public functions  =====
+    /*///////////////////////////////////////////////////////////////
+                            Modifiers
+    //////////////////////////////////////////////////////////////*/
 
-    /// @dev Returns the module type of the contract.
+    modifier onlyMinter() {
+        // if transfer is restricted on the contract, we still want to allow burning and minting
+        if (!hasRole(MINTER_ROLE, address(0))) {
+            require(hasRole(MINTER_ROLE, _msgSender()) , "restricted to MINTER_ROLE holders.");
+        }
+
+        _;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        Generic contract logic
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Returns the type of the contract.
     function contractType() external pure returns (bytes32) {
         return MODULE_TYPE;
     }
@@ -126,11 +140,6 @@ contract Multiwrap is
         return uint8(VERSION);
     }
 
-    /// @dev See ERC1155 - returns the metadata for a token.
-    function uri(uint256 _tokenId) public view override returns (string memory) {
-        return uriForShares[_tokenId];
-    }
-
     /**
      * @dev Returns the address of the current owner.
      */
@@ -138,56 +147,118 @@ contract Multiwrap is
         return hasRole(DEFAULT_ADMIN_ROLE, _owner) ? _owner : address(0);
     }
 
-    ///     =====   External functions  =====
+    /*///////////////////////////////////////////////////////////////
+                        ERC 165 / 721 / 2981 logic
+    //////////////////////////////////////////////////////////////*/
 
-    /// @dev Wrap multiple ERC1155, ERC721, ERC20 tokens into 'n' shares (i.e. variable supply of 1 ERC 1155 token)
+    /// @dev Returns the URI for a given tokenId.
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        return uri[_tokenId];
+    }
+
+    /// @dev See ERC 165
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlEnumerableUpgradeable, ERC1155ReceiverUpgradeable, ERC721Upgradeable, IERC165Upgradeable)
+        returns (bool)
+    {
+        return
+            super.supportsInterface(interfaceId) ||
+            interfaceId == type(IERC721Upgradeable).interfaceId ||
+            interfaceId == type(IERC2981Upgradeable).interfaceId;
+    }
+
+    /// @dev Returns the royalty recipient and amount, given a tokenId and sale price.
+    function royaltyInfo(uint256 tokenId, uint256 salePrice)
+        external
+        view
+        virtual
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        (address recipient, uint256 bps) = getRoyaltyInfoForToken(tokenId);
+        receiver = recipient;
+        royaltyAmount = (salePrice * bps) / MAX_BPS;
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                    Wrapping / Unwrapping logic
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Wrap multiple ERC1155, ERC721, ERC20 tokens into a single wrapped NFT.
     function wrap(
-        MultiTokenTransferLib.MultiToken calldata _wrappedContents,
-        uint256 _shares,
-        string calldata _uriForShares
-    ) external payable nonReentrant returns (uint256 tokenId) {
+        Token[] calldata _wrappedContents,
+        string calldata _uriForWrappedToken,
+        address _recipient
+    ) external payable nonReentrant onlyMinter returns (uint256 tokenId) {
         tokenId = nextTokenIdToMint;
         nextTokenIdToMint += 1;
 
-        uriForShares[tokenId] = _uriForShares;
-        wrappedContents[tokenId] = _wrappedContents;
+        for(uint256 i = 0; i < _wrappedContents.length; i += 1) {
+            wrappedContents[tokenId].token[i] = _wrappedContents[i];
+        }
+        wrappedContents[tokenId].count = _wrappedContents.length;
 
-        _mint(_msgSender(), tokenId, _shares, "");
-        totalShares[tokenId] = _shares;
+        uri[tokenId] = _uriForWrappedToken;
 
-        MultiTokenTransferLib.transferAll(_msgSender(), address(this), _wrappedContents);
+        _safeMint(_recipient, tokenId);
 
-        emit TokensWrapped(_msgSender(), tokenId, _wrappedContents);
+        transferTokenBatch(_msgSender(), address(this), _wrappedContents);
+
+        emit TokensWrapped(_msgSender(), _recipient, tokenId, _wrappedContents);
     }
 
-    /// @dev Unwrap shares to retrieve underlying ERC1155, ERC721, ERC20 tokens.
+    /// @dev Unwrap a wrapped NFT to retrieve underlying ERC1155, ERC721, ERC20 tokens.
     function unwrap(
         uint256 _tokenId,
-        uint256 _amountToRedeem,
-        address _sendTo
+        address _recipient
     ) external nonReentrant {
         require(_tokenId < nextTokenIdToMint, "invalid tokenId");
-        require(balanceOf(_msgSender(), _tokenId) >= _amountToRedeem, "unwrapping more than owned");
+        require(_isApprovedOrOwner(_msgSender(), _tokenId), "unapproved called");
 
-        uint256 totalSharesOfToken = totalShares[_tokenId];
-        bool isTotalRedemption = _amountToRedeem == totalSharesOfToken;
+        _burn(_tokenId);
 
-        MultiTokenTransferLib.MultiToken memory wrappedContents_ = wrappedContents[_tokenId];
+        uint256 count = wrappedContents[_tokenId].count;
+        Token[] memory tokensUnwrapped = new Token[](count);
 
-        burn(_msgSender(), _tokenId, _amountToRedeem);
-
-        if (totalSupply[_tokenId] == 0) {
-            delete wrappedContents[_tokenId];
+        for(uint256 i = 0; i < count; i += 1) {
+            tokensUnwrapped[i] = wrappedContents[_tokenId].token[i];
+            transferToken(address(this), _recipient, tokensUnwrapped[i]);
         }
 
-        if (isTotalRedemption) {
-            MultiTokenTransferLib.transferAll(address(this), _sendTo, wrappedContents_);
-        } else {
-            transfer20ByShares(address(this), _sendTo, wrappedContents_, _amountToRedeem, totalSharesOfToken);
-        }
+        delete wrappedContents[_tokenId];
 
-        emit TokensUnwrapped(_msgSender(), _sendTo, _tokenId, _amountToRedeem, wrappedContents_);
+        emit TokensUnwrapped(_msgSender(), _recipient, _tokenId, tokensUnwrapped);
     }
+
+    /// @dev Transfers an arbitrary ERC20 / ERC721 / ERC1155 token.
+    function transferToken(address _from, address _to, Token memory _token) internal {
+        if(_token.tokenType == TokenType.ERC20) {
+            CurrencyTransferLib.transferCurrencyWithWrapperAndBalanceCheck(
+                _token.assetContract,
+                _from,
+                _to,
+                _token.amount,
+                nativeTokenWrapper
+            );
+        } else if(_token.tokenType == TokenType.ERC721) {
+            IERC721Upgradeable(_token.assetContract).safeTransferFrom(_from, _to, _token.tokenId);
+        } else if(_token.tokenType == TokenType.ERC1155) {
+            IERC1155Upgradeable(_token.assetContract).safeTransferFrom(_from, _to, _token.tokenId, _token.amount, "");
+        }
+    }
+
+    /// @dev Transfers multiple arbitrary ERC20 / ERC721 / ERC1155 tokens.
+    function transferTokenBatch(address _from, address _to, Token[] memory _tokens) internal {
+        for(uint256 i = 0; i < _tokens.length; i += 1) {
+            transferToken(_from, _to, _tokens[i]);
+        }
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        Getter functions
+    //////////////////////////////////////////////////////////////*/
 
     /// @dev Returns the platform fee bps and recipient.
     function getDefaultRoyaltyInfo() external view returns (address, uint16) {
@@ -203,6 +274,10 @@ contract Multiwrap is
                 ? (royaltyRecipient, uint16(royaltyBps))
                 : (royaltyForToken.recipient, uint16(royaltyForToken.bps));
     }
+
+    /*///////////////////////////////////////////////////////////////
+                        Setter functions
+    //////////////////////////////////////////////////////////////*/
 
     /// @dev Lets a module admin update the royalty bps and recipient.
     function setDefaultRoyaltyInfo(address _royaltyRecipient, uint256 _royaltyBps)
@@ -242,125 +317,24 @@ contract Multiwrap is
         contractURI = _uri;
     }
 
-    ///     =====   Internal functions  =====
-
-    function transfer20ByShares(
-        address _from,
-        address _to,
-        MultiTokenTransferLib.MultiToken memory _wrappedContents,
-        uint256 _sharesToAccount,
-        uint256 _totalShares
-    ) internal {
-        require(
-            _wrappedContents.erc1155AssetContracts.length == 0 && _wrappedContents.erc721AssetContracts.length == 0,
-            "cannot unwrap by shares"
-        );
-
-        bool isValidData = _wrappedContents.erc20AssetContracts.length == _wrappedContents.erc20AmountsToWrap.length;
-        if (isValidData) {
-            for (uint256 i = 0; i < _wrappedContents.erc20AssetContracts.length; i += 1) {
-                isValidData = _wrappedContents.erc20AmountsToWrap[i] % _totalShares == 0;
-
-                if (!isValidData) {
-                    break;
-                }
-                uint256 tokensToIssue = (_wrappedContents.erc20AmountsToWrap[i] * _sharesToAccount) / _totalShares;
-
-                CurrencyTransferLib.transferCurrency(
-                    _wrappedContents.erc20AssetContracts[i],
-                    _from,
-                    _to,
-                    tokensToIssue
-                );
-            }
-        }
-        require(isValidData, "invalid erc20 wrap");
-    }
-
-    /// @dev See EIP-2981
-    function royaltyInfo(uint256 tokenId, uint256 salePrice)
-        external
-        view
-        virtual
-        returns (address receiver, uint256 royaltyAmount)
-    {
-        (address recipient, uint256 bps) = getRoyaltyInfoForToken(tokenId);
-        receiver = recipient;
-        royaltyAmount = (salePrice * bps) / MAX_BPS;
-    }
-
-    ///     =====   Low-level overrides  =====
-
-    /// @dev Lets a token owner burn the tokens they own (i.e. destroy for good)
-    function burn(
-        address account,
-        uint256 id,
-        uint256 value
-    ) public virtual {
-        require(
-            account == _msgSender() || isApprovedForAll(account, _msgSender()),
-            "ERC1155: caller is not owner nor approved."
-        );
-
-        _burn(account, id, value);
-    }
-
-    /// @dev Lets a token owner burn multiple tokens they own at once (i.e. destroy for good)
-    function burnBatch(
-        address account,
-        uint256[] memory ids,
-        uint256[] memory values
-    ) public virtual {
-        require(
-            account == _msgSender() || isApprovedForAll(account, _msgSender()),
-            "ERC1155: caller is not owner nor approved."
-        );
-
-        _burnBatch(account, ids, values);
-    }
+    /*///////////////////////////////////////////////////////////////
+                        Miscellaneous
+    //////////////////////////////////////////////////////////////*/
 
     /**
-     * @dev See {ERC1155-_beforeTokenTransfer}.
+     * @dev See {ERC721-_beforeTokenTransfer}.
      */
     function _beforeTokenTransfer(
-        address operator,
         address from,
         address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
+        uint256 tokenId
     ) internal virtual override {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+        super._beforeTokenTransfer(from, to, tokenId);
 
         // if transfer is restricted on the contract, we still want to allow burning and minting
         if (!hasRole(TRANSFER_ROLE, address(0)) && from != address(0) && to != address(0)) {
             require(hasRole(TRANSFER_ROLE, from) || hasRole(TRANSFER_ROLE, to), "restricted to TRANSFER_ROLE holders.");
         }
-
-        if (from == address(0)) {
-            for (uint256 i = 0; i < ids.length; ++i) {
-                totalSupply[ids[i]] += amounts[i];
-            }
-        }
-
-        if (to == address(0)) {
-            for (uint256 i = 0; i < ids.length; ++i) {
-                totalSupply[ids[i]] -= amounts[i];
-            }
-        }
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(AccessControlEnumerableUpgradeable, ERC1155Upgradeable, IERC165Upgradeable, ERC1155ReceiverUpgradeable)
-        returns (bool)
-    {
-        return
-            super.supportsInterface(interfaceId) ||
-            interfaceId == type(IERC1155Upgradeable).interfaceId ||
-            interfaceId == type(IERC2981Upgradeable).interfaceId;
     }
 
     function _msgSender()
