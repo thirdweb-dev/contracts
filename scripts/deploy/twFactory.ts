259e8836838ae38b848d1daae5c2c5bc866d5c67
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-04-17 20:01:58 -0400

    add contract verification to factoru deploy script

--- scripts/deploy/twFactory.ts
+++ scripts/deploy/twFactory.ts
@@ -1,8 +1,8 @@
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
 import { TWFactory } from "typechain/TWFactory";
 
 /**
- *  Note: this script deploys a new instance of TWFactory.
+ *  Note: this script deploys a new instance of TWFactory + verifies it on block explorer.
  */
 
 async function main() {
@@ -17,7 +17,21 @@ async function main() {
     console.log("Deploying TWFactory \ntransaction: ", twFactory.deployTransaction.hash, "\naddress: ", twFactory.address);
 
     console.log("\n")
+
+    console.log("Verifying contract.")
+    await verify(twFactory.address, [forwarderAddress, registryAddress]);
 }
+
+async function verify(address: string, args: any[]) {
+    try {
+      return await hre.run("verify:verify", {
+        address: address,
+        constructorArguments: args,
+      });
+    } catch (e) {
+      console.log(address, args, e);
+    }
+  }
 
 main()
     .then(() => process.exit(0))
