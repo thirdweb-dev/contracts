57232131d32e876e129d2f0fe8316e207669084a
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-04-22 16:18:16 -0400

    update delayed reveal interface

--- contracts/interfaces/drop/IDelayedReveal.sol
+++ contracts/interfaces/drop/IDelayedReveal.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.11;
 
 interface IDelayedReveal {
-    function reveal(uint256 index, bytes calldata _key) external returns (string memory revealedURI);
-
+    function reveal(uint256 identifier, bytes calldata key) external returns (string memory revealedURI);
     function encryptDecrypt(bytes memory data, bytes calldata key) external pure returns (bytes memory result);
 }
