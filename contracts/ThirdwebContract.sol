0f61e7adc459f498caea7dcf9cac7b8b01500fc7
Krishang Nadgauda <nkrishang@Krishangs-MBP.localdomain>
2022-05-03 16:26:48 -0700

    ThirdwebContract with setContractURI

--- contracts/ThirdwebContract.sol
+++ contracts/ThirdwebContract.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-contract ThirdwebContract {
-    /// @dev The publish metadata of the contract of which this contract is an instance.
-    string private publishMetadataUri;
-    /// @dev The metadata for this contract.
-    string public contractURI;
+import "./feature/Ownable.sol";
+import "./feature/Context.sol";
+import "./feature/ContractMetadata.sol";
+
+contract ThirdwebContract is Context, Ownable, ContractMetadata {
 
     struct ThirdwebInfo {
         string publishMetadataUri;
         string contractURI;
+        address owner;
     }
+    
+    /// @dev The publish metadata of the contract of which this contract is an instance.
+    string private publishMetadataUri;
 
     /// @dev Returns the publish metadata for this contract.
     function getPublishMetadataUri() external view returns (string memory) {
@@ -23,5 +27,16 @@ contract ThirdwebContract {
 
         publishMetadataUri = _thirdwebInfo.publishMetadataUri;
         contractURI = _thirdwebInfo.contractURI;
+        owner = _thirdwebInfo.owner;
+    }
+
+    /// @dev Returns whether owner can be set in the given execution context.
+    function _canSetOwner() internal virtual override returns (bool) {
+        return _msgSender() == owner;
+    }
+
+    /// @dev Returns whether contract metadata can be set in the given execution context.
+    function _canSetContractURI() internal virtual override returns (bool) {
+        return _msgSender() == owner;
     }
 }
