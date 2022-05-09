8208b7f272e945d38330f75415db65703fafb1cd
yash <kumaryashcse@gmail.com>
2022-05-09 22:27:00 +0530

    token bundle documentation

--- contracts/feature/TokenBundle.sol
+++ contracts/feature/TokenBundle.sol
@@ -4,22 +4,25 @@ pragma solidity ^0.8.0;
 import "./interface/ITokenBundle.sol";
 
 abstract contract TokenBundle is ITokenBundle {
+    /// @dev UID => asset count, bundle uri, and tokens contained in the bundle
     mapping(uint256 => BundleInfo) private bundle;
 
-    // function _getNextBundleId() internal virtual returns (uint256);
-
+    /// @dev Returns the count of assets in a bundle, given a bundle Id.
     function getTokenCount(uint256 _bundleId) public view returns (uint256) {
         return bundle[_bundleId].count;
     }
 
+    /// @dev Returns a token contained in a bundle, given a bundle Id and index of token.
     function getToken(uint256 _bundleId, uint256 index) public view returns (Token memory) {
         return bundle[_bundleId].tokens[index];
     }
 
+    /// @dev Returns the uri of bundle for a particular bundle Id.
     function getUri(uint256 _bundleId) public view returns (string memory) {
         return bundle[_bundleId].uri;
     }
 
+    /// @dev Lets the calling contract create/update a bundle, by passing in a list of tokens and a unique id.
     function _setBundle(Token[] calldata _tokensToBind, uint256 _bundleId) internal {
         // uint256 _bundleId = _getNextBundleId();
         require(_tokensToBind.length > 0, "no tokens to bind");
@@ -29,6 +32,7 @@ abstract contract TokenBundle is ITokenBundle {
         bundle[_bundleId].count = _tokensToBind.length;
     }
 
+    /// @dev Lets the calling contract set/update a token in a bundle for a unique bundle id and index.
     function _setBundleToken(
         Token memory _tokenToBind,
         uint256 _bundleId,
@@ -39,10 +43,12 @@ abstract contract TokenBundle is ITokenBundle {
         bundle[_bundleId].count += isUpdate ? 0 : 1;
     }
 
+    /// @dev Lets the calling contract set/update the bundle uri for a particular bundle id.
     function _setUri(string calldata _uri, uint256 _bundleId) internal {
         bundle[_bundleId].uri = _uri;
     }
 
+    /// @dev Lets the calling contract delete a bundle with a given id.
     function _deleteBundle(uint256 _bundleId) internal {
         delete bundle[_bundleId];
     }
--- contracts/feature/interface/ITokenBundle.sol
+++ contracts/feature/interface/ITokenBundle.sol
@@ -1,13 +1,29 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
+/**
+ *  Group together arbitrary ERC20, ERC721 and ERC1155 tokens into a single bundle.
+ *
+ *  This bundle of tokens is a generic list of tokens that can have multiple use cases,
+ *  such as mapping to an NFT, or put in an escrow, etc.
+ */
+
 interface ITokenBundle {
+    /// @notice The type of assets that can be wrapped.
     enum TokenType {
         ERC20,
         ERC721,
         ERC1155
     }
 
+    /**
+     *  @notice A generic interface to describe a token to be put in a bundle.
+     *
+     *  @param assetContract The contract address of the asset to bind.
+     *  @param tokenType     The token type (ERC20 / ERC721 / ERC1155) of the asset to bind.
+     *  @param tokenId       The token Id of the asset to bind, if the asset is an ERC721 / ERC1155 NFT.
+     *  @param totalAmount   The amount of the asset to bind, if the asset is an ERC20 / ERC1155 fungible token.
+     */
     struct Token {
         address assetContract;
         TokenType tokenType;
@@ -15,6 +31,13 @@ interface ITokenBundle {
         uint256 totalAmount;
     }
 
+    /**
+     *  @notice An internal data structure to track the contents of a bundle.
+     *
+     *  @param count    The total kinds of assets i.e. `Token` inside a bundle.
+     *  @param uri      The (metadata) URI assigned to the bundle created
+     *  @param tokens   Mapping from a UID -> to the asset i.e. `Token` at that UID.
+     */
     struct BundleInfo {
         uint256 count;
         string uri;
