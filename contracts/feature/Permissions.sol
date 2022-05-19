f17329f8c3f56a9aadb794ce4ddbb2e6b86e8c02
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-05-19 15:11:13 -0400

    add _checkRoleWithSwitch to Permissions

--- contracts/feature/Permissions.sol
+++ contracts/feature/Permissions.sol
@@ -76,4 +76,19 @@ contract Permissions is IPermissions {
             );
         }
     }
+
+    function _checkRoleWithSwitch(bytes32 role, address account) internal view virtual {
+        if (!hasRoleWithSwitch(role, account)) {
+            revert(
+                string(
+                    abi.encodePacked(
+                        "AccessControl: account ",
+                        Strings.toHexString(uint160(account), 20),
+                        " is missing role ",
+                        Strings.toHexString(uint256(role), 32)
+                    )
+                )
+            );
+        }
+    }
 }
