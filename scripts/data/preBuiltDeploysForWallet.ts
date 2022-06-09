a932b7ced3a0b18f82417385b3d5f284e3fb0049
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-06-09 12:21:20 -0400

    update data scripts

--- scripts/data/preBuiltDeploysForWallet.ts
+++ scripts/data/preBuiltDeploysForWallet.ts
@@ -8,14 +8,17 @@ import { AddedEvent } from "typechain/contracts/TWRegistry";
 const startBlock: number = 0;
 const endBlock: number = 0;
 
+// REPLACE
+const deployer: string = "0x38abaC1B42ebC9429CB3c9E242dee5eA1104be5d";
+
 async function main() {
 
     const chainId: number = hre.network.config.chainId as number;
-    console.log(`\nGetting the number of pre-built contracts deployed:\nChain: ${chainId}\nStart block: ${startBlock}  End block: ${endBlock}`)
+    console.log(`\nGetting the number of pre-built contracts deployed:\nChain: ${chainId}\nStart block: ${startBlock}  End block: ${endBlock}\nDeployer: ${deployer}`)
     
     const twRegistry: TWRegistry = await ethers.getContractAt("TWRegistry", "0x7c487845f98938Bb955B1D5AD069d9a30e4131fd");
 
-    const filter = twRegistry.filters.Added();
+    const filter = twRegistry.filters.Added(deployer);
     let events: AddedEvent[];
 
     if(!startBlock && !endBlock) {
@@ -26,7 +29,8 @@ async function main() {
       events = await twRegistry.queryFilter(filter, startBlock, endBlock);
     }
 
-    console.log("Total number of pre-built contracts deployed: ", events.length);
+    console.log("Total deploys by deployer: ", events.length);
+    console.log("Deployment addresses: ", events.map(x => x.args.deployment));
 }
 
 main()
