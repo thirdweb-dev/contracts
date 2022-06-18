507ce33ba00ec643ddef6811e051aadda572e69f
yash <kumaryashcse@gmail.com>
2022-06-19 02:36:05 +0530

    cleanup

--- contracts/drop/DropERC721.sol
+++ contracts/drop/DropERC721.sol
@@ -353,8 +353,6 @@ contract DropERC721 is
             _proofMaxQuantityPerTransaction
         );
 
-        // ClaimCondition memory currentClaimPhase = claimCondition.phases[activeConditionId];
-
         // Verify claim validity. If not valid, revert.
         // when there's allowlist present --> verifyClaimMerkleProof will verify the _proofMaxQuantityPerTransaction value with hashed leaf in the allowlist
         // when there's no allowlist, this check is true --> verifyClaim will check for _quantity being less/equal than the limit
