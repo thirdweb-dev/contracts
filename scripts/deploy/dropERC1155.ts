23b57e633ae33f515468e110cf686e91f707f91d
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-06-18 22:47:55 +0000

    fix deploy scripts

--- scripts/deploy/dropERC1155.ts
+++ scripts/deploy/dropERC1155.ts
@@ -7,8 +7,7 @@ import { TWFactory, DropERC1155 } from "typechain";
 async function main() {
   const [caller]: SignerWithAddress[] = await ethers.getSigners();
 
-  // const twFeeAddress: string = "0x8c4b615040ebd2618e8fc3b20cefe9abafdeb0ea"; // replace
-  const twFactoryAddress: string = "0x5DBC7B840baa9daBcBe9D2492E45D7244B54A2A0"; // replace
+  const twFactoryAddress: string = ethers.constants.AddressZero; // replace
 
   const twFactory: TWFactory = await ethers.getContractAt("TWFactory", twFactoryAddress);
 
