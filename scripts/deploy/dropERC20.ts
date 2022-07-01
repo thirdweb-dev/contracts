c186823cf95c67a823b1a3bddda1d8dc27cc6e1a
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-07-01 19:00:45 -0400

    update deploy scripts for drop contracts

--- scripts/deploy/dropERC20.ts
+++ scripts/deploy/dropERC20.ts
@@ -1,23 +1,9 @@
 import hre, { ethers } from "hardhat";
 
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-
-import { DropERC20, TWFactory } from "typechain";
+import { DropERC20 } from "typechain";
 
 async function main() {
-  const [caller]: SignerWithAddress[] = await ethers.getSigners();
 
-  const twFactoryAddress: string = ethers.constants.AddressZero; // replace
-
-  const twFactory: TWFactory = await ethers.getContractAt("TWFactory", twFactoryAddress);
-
-  const hasFactoryRole = await twFactory.hasRole(
-    ethers.utils.solidityKeccak256(["string"], ["FACTORY_ROLE"]),
-    caller.address,
-  );
-  if (!hasFactoryRole) {
-    throw new Error("Caller does not have FACTORY_ROLE on factory");
-  }
   const dropERC20: DropERC20 = await ethers.getContractFactory("DropERC20").then(f => f.deploy());
 
   console.log(
@@ -26,12 +12,6 @@ async function main() {
     "\naddress: ",
     dropERC20.address,
   );
-
-  console.log("\n");
-
-  const addImplementationTx = await twFactory.addImplementation(dropERC20.address);
-  console.log("Adding DropERC20 implementation to TWFactory: ", addImplementationTx.hash);
-  await addImplementationTx.wait();
 
   console.log("\n");
 
