f6a216d1c1096ad8ee5484240e65eb35721af7e1
Yash <kumaryashcse@gmail.com>
2022-07-06 17:29:53 +0530

    add ERC20 and ERC20Base

---
+++ contracts/base/ERC20/ERC20Base.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "./ERC20.sol";
+
+import "../../feature/ContractMetadata.sol";
+import "../../feature/Multicall.sol";
+import "../../feature/Ownable.sol";
+
+contract ERC20Base is 
+    ERC20,
+    ContractMetadata,
+    Multicall,
+    Ownable
+{
+    /*//////////////////////////////////////////////////////////////
+                            Constructor
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(
+        string memory _name, 
+        string memory _symbol,
+        string memory _contractURI
+    ) ERC20(_name, _symbol) 
+    {
+        _setupContractURI(_contractURI);
+        _setupOwner(msg.sender);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            Minting logic
+    //////////////////////////////////////////////////////////////*/
+
+    function mint(address _to, uint256 _amount) public virtual {
+        require(_canMint(), "Not authorized to mint.");
+
+        _mint(_to, _amount);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        Internal (overrideable) functions
+    //////////////////////////////////////////////////////////////*/
+    
+    /// @dev Returns whether contract metadata can be set in the given execution context.
+    function _canSetContractURI() internal virtual view override returns (bool) {
+        return msg.sender == owner();
+    }
+
+    /// @dev Returns whether tokens can be minted in the given execution context.
+    function _canMint() internal virtual view returns (bool) {
+        return msg.sender == owner();
+    }
+
+    /// @dev Returns whether owner can be set in the given execution context.
+    function _canSetOwner() internal virtual view override returns (bool) {
+        return msg.sender == owner();
+    }
+}
