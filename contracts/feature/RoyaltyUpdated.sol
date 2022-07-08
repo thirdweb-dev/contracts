d85978b64cb85ce121b46789319b795a89d4c2f8
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-07-08 10:44:53 -0400

    Use separate Royalty contrac

--- contracts/feature/RoyaltyUpdated.sol
+++ contracts/feature/RoyaltyUpdated.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "./interface/IRoyalty.sol";
+import "./interface/IRoyaltyUpdated.sol";
 
 /**
  *  Thirdweb's `Royalty` is a contract extension to be used with any base contract. It exposes functions for setting and reading
