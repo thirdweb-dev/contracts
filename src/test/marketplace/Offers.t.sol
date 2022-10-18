f04d47a3886ac3b22a250966b2e3ffc4be8cf53e
Yash <kumaryashcse@gmail.com>
2022-10-18 17:49:22 +0530

    Offers: tests for makeOffer

--- src/test/marketplace/Offers.t.sol
+++ src/test/marketplace/Offers.t.sol
@@ -11,6 +11,8 @@ import { MarketplaceEntrypoint } from "contracts/marketplace/alt/MarketplaceEntr
 import { Offers } from "contracts/marketplace/offers/OffersLogic.sol";
 import { TWProxy } from "contracts/TWProxy.sol";
 
+import { IOffers } from "contracts/marketplace/IMarketplace.sol";
+
 contract MarketplaceOffersTest is BaseTest {
     // Target contract
     address public marketplace;
@@ -67,6 +69,21 @@ contract MarketplaceOffersTest is BaseTest {
                 )
             )
         );
+
+        // [6] Setup roles for seller and assets
+        vm.startPrank(marketplaceDeployer);
+        Permissions(marketplace).grantRole(keccak256("ASSET_ROLE"), address(erc721));
+        Permissions(marketplace).grantRole(keccak256("ASSET_ROLE"), address(erc1155));
+
+        vm.stopPrank();
+
+        vm.label(address(entrypoint), "Entrypoint_Impl");
+        vm.label(marketplace, "Marketplace");
+        vm.label(offers, "Offers_Extension");
+        vm.label(seller, "Seller");
+        vm.label(buyer, "Buyer");
+        vm.label(address(erc721), "ERC721_Token");
+        vm.label(address(erc1155), "ERC1155_Token");
     }
 
     function test_state_initial() public {
@@ -78,13 +95,275 @@ contract MarketplaceOffersTest is BaseTest {
                             Make Offer
     //////////////////////////////////////////////////////////////*/
 
-    function test_state_makeOffer() public {}
+    function test_state_makeOffer() public {
+        // Sample offer parameters.
+        address assetContract = address(erc721);
+        uint256 tokenId = 0;
+        uint256 quantity = 1;
+        address currency = address(erc20);
+        uint256 totalPrice = 1 ether;
+        uint256 expirationTimestamp = 200;
 
-    function test_revert_makeOffer_notOwnerOfOfferedTokens() public {}
+        // mint total-price to buyer
+        erc20.mint(buyer, totalPrice);
 
-    function test_revert_makeOffer_notApprovedMarketplaceToTransferTokens() public {}
+        // Approve Marketplace to transfer currency tokens.
+        vm.prank(buyer);
+        erc20.approve(marketplace, totalPrice);
 
-    function test_revert_makeOffer_wantedZeroTokens() public {}
+        // Make offer.
+        IOffers.OfferParams memory offerParams = IOffers.OfferParams(
+            assetContract,
+            tokenId,
+            quantity,
+            currency,
+            totalPrice,
+            expirationTimestamp
+        );
+
+        vm.prank(buyer);
+        uint256 offerId = Offers(marketplace).makeOffer(offerParams);
+
+        // Test consequent state of the contract.
+
+        // Total offers incremented
+        assertEq(Offers(marketplace).totalOffers(), 1);
+
+        // Fetch listing and verify state.
+        IOffers.Offer memory offer = Offers(marketplace).getOffer(offerId);
+
+        assertEq(offer.offerId, offerId);
+        assertEq(offer.offeror, buyer);
+        assertEq(offer.assetContract, assetContract);
+        assertEq(offer.tokenId, tokenId);
+        assertEq(offer.quantity, quantity);
+        assertEq(offer.currency, currency);
+        assertEq(offer.totalPrice, totalPrice);
+        assertEq(offer.expirationTimestamp, expirationTimestamp);
+        assertEq(uint256(offer.tokenType), uint256(IOffers.TokenType.ERC721));
+    }
+
+    function test_revert_makeOffer_notOwnerOfOfferedTokens() public {
+        // Sample offer parameters.
+        address assetContract = address(erc721);
+        uint256 tokenId = 0;
+        uint256 quantity = 1;
+        address currency = address(erc20);
+        uint256 totalPrice = 1 ether;
+        uint256 expirationTimestamp = 200;
+
+        // Approve Marketplace to transfer currency tokens. (without owning)
+        vm.prank(buyer);
+        erc20.approve(marketplace, totalPrice);
+
+        // Make offer.
+        IOffers.OfferParams memory offerParams = IOffers.OfferParams(
+            assetContract,
+            tokenId,
+            quantity,
+            currency,
+            totalPrice,
+            expirationTimestamp
+        );
+
+        vm.prank(buyer);
+        vm.expectRevert("!BAL20");
+        Offers(marketplace).makeOffer(offerParams);
+    }
+
+    function test_revert_makeOffer_notApprovedMarketplaceToTransferTokens() public {
+        // Sample offer parameters.
+        address assetContract = address(erc721);
+        uint256 tokenId = 0;
+        uint256 quantity = 1;
+        address currency = address(erc20);
+        uint256 totalPrice = 1 ether;
+        uint256 expirationTimestamp = 200;
+
+        // mint total-price to buyer, but not approved to marketplace
+        erc20.mint(buyer, totalPrice);
+
+        // Make offer.
+        IOffers.OfferParams memory offerParams = IOffers.OfferParams(
+            assetContract,
+            tokenId,
+            quantity,
+            currency,
+            totalPrice,
+            expirationTimestamp
+        );
+
+        vm.prank(buyer);
+        vm.expectRevert("!BAL20");
+        Offers(marketplace).makeOffer(offerParams);
+    }
+
+    function test_revert_makeOffer_wantedZeroTokens() public {
+        // Sample offer parameters.
+        address assetContract = address(erc721);
+        uint256 tokenId = 0;
+        uint256 quantity = 0;
+        address currency = address(erc20);
+        uint256 totalPrice = 1 ether;
+        uint256 expirationTimestamp = 200;
+
+        // mint total-price to buyer
+        erc20.mint(buyer, totalPrice);
+
+        // Approve Marketplace to transfer currency tokens.
+        vm.prank(buyer);
+        erc20.approve(marketplace, totalPrice);
+
+        // Make offer.
+        IOffers.OfferParams memory offerParams = IOffers.OfferParams(
+            assetContract,
+            tokenId,
+            quantity,
+            currency,
+            totalPrice,
+            expirationTimestamp
+        );
+
+        vm.prank(buyer);
+        vm.expectRevert("zero quantity.");
+        Offers(marketplace).makeOffer(offerParams);
+    }
+
+    function test_revert_makeOffer_invalidQuantity() public {
+        // Sample offer parameters.
+        address assetContract = address(erc721);
+        uint256 tokenId = 0;
+        uint256 quantity = 2; // Asking for more than `1` quantity of erc721 tokenId
+        address currency = address(erc20);
+        uint256 totalPrice = 1 ether;
+        uint256 expirationTimestamp = 200;
+
+        // mint total-price to buyer
+        erc20.mint(buyer, totalPrice);
+
+        // Approve Marketplace to transfer currency tokens.
+        vm.prank(buyer);
+        erc20.approve(marketplace, totalPrice);
+
+        // Make offer.
+        IOffers.OfferParams memory offerParams = IOffers.OfferParams(
+            assetContract,
+            tokenId,
+            quantity,
+            currency,
+            totalPrice,
+            expirationTimestamp
+        );
+
+        vm.prank(buyer);
+        vm.expectRevert("invalid quantity.");
+        Offers(marketplace).makeOffer(offerParams);
+    }
+
+    function test_revert_makeOffer_invalidExpirationTimestamp() public {
+        // Sample offer parameters.
+        address assetContract = address(erc721);
+        uint256 tokenId = 0;
+        uint256 quantity = 1;
+        address currency = address(erc20);
+        uint256 totalPrice = 1 ether;
+        uint256 expirationTimestamp = block.timestamp;
+
+        // mint total-price to buyer
+        erc20.mint(buyer, totalPrice);
+
+        // Approve Marketplace to transfer currency tokens.
+        vm.prank(buyer);
+        erc20.approve(marketplace, totalPrice);
+
+        // Make offer.
+        IOffers.OfferParams memory offerParams = IOffers.OfferParams(
+            assetContract,
+            tokenId,
+            quantity,
+            currency,
+            totalPrice,
+            expirationTimestamp
+        );
+
+        vm.prank(buyer);
+        vm.expectRevert("invalid expiration.");
+        Offers(marketplace).makeOffer(offerParams);
+    }
+
+    function test_revert_makeOffer_invalidAssetContract() public {
+        // Sample offer parameters.
+        address assetContract = address(erc20);
+        uint256 tokenId = 0;
+        uint256 quantity = 1;
+        address currency = address(erc20);
+        uint256 totalPrice = 1 ether;
+        uint256 expirationTimestamp = block.timestamp;
+
+        // mint total-price to buyer
+        erc20.mint(buyer, totalPrice);
+
+        // Approve Marketplace to transfer currency tokens.
+        vm.prank(buyer);
+        erc20.approve(marketplace, totalPrice);
+
+        // Make offer.
+        IOffers.OfferParams memory offerParams = IOffers.OfferParams(
+            assetContract,
+            tokenId,
+            quantity,
+            currency,
+            totalPrice,
+            expirationTimestamp
+        );
+
+        // Grant ERC20 token asset role.
+        vm.prank(marketplaceDeployer);
+        Permissions(marketplace).grantRole(keccak256("ASSET_ROLE"), address(erc20));
+
+        vm.prank(buyer);
+        vm.expectRevert("token must be ERC1155 or ERC721.");
+        Offers(marketplace).makeOffer(offerParams);
+    }
+
+    function test_revert_createListing_noAssetRoleWhenRestrictionsActive() public {
+        // Sample offer parameters.
+        address assetContract = address(erc721);
+        uint256 tokenId = 0;
+        uint256 quantity = 1;
+        address currency = address(erc20);
+        uint256 totalPrice = 1 ether;
+        uint256 expirationTimestamp = block.timestamp;
+
+        // mint total-price to buyer
+        erc20.mint(buyer, totalPrice);
+
+        // Approve Marketplace to transfer currency tokens.
+        vm.prank(buyer);
+        erc20.approve(marketplace, totalPrice);
+
+        // Make offer.
+        IOffers.OfferParams memory offerParams = IOffers.OfferParams(
+            assetContract,
+            tokenId,
+            quantity,
+            currency,
+            totalPrice,
+            expirationTimestamp
+        );
+
+        // Revoke ASSET_ROLE from token to list.
+        vm.startPrank(marketplaceDeployer);
+        assertEq(Permissions(marketplace).hasRole(keccak256("ASSET_ROLE"), address(0)), false);
+        Permissions(marketplace).revokeRole(keccak256("ASSET_ROLE"), address(erc721));
+        assertEq(Permissions(marketplace).hasRole(keccak256("ASSET_ROLE"), address(erc721)), false);
+
+        vm.stopPrank();
+
+        vm.prank(buyer);
+        vm.expectRevert("!ASSET_ROLE");
+        Offers(marketplace).makeOffer(offerParams);
+    }
 
     /*///////////////////////////////////////////////////////////////
                             Cancel Offer
