977128825cf8e168f24f63e2f2aa8ce5d6dbe990
Krishang Nadgauda <nkrishang@Krishangs-MacBook-Pro.local>
2022-10-19 11:54:10 -0400

    Add rest of the code comments to IMarketplace

--- contracts/marketplace/IMarketplace.sol
+++ contracts/marketplace/IMarketplace.sol
@@ -187,12 +187,33 @@ interface IDirectListings {
     function getListing(uint256 _listingId) external view returns (Listing memory listing);
 }
 
+/**
+ *  The `EnglishAuctions` extension smart contract lets you sell NFTs (ERC-721 or ERC-1155) in an english auction.
+ */
+
 interface IEnglishAuctions {
     enum TokenType {
         ERC721,
         ERC1155
     }
 
+    /**
+     *  @notice The parameters a seller sets when creating an auction listing.
+     *
+     *  @param assetContract The address of the smart contract of the NFTs being auctioned.
+     *  @param tokenId The tokenId of the NFTs being auctioned.
+     *  @param quantity The quantity of NFTs being auctioned. This must be non-zero, and is expected to
+     *                  be `1` for ERC-721 NFTs.
+     *  @param currency The currency in which the bid must be made when bidding for the auctioned NFTs.
+     *  @param minimumBidAmount The minimum bid amount for the auction.
+     *  @param buyoutBidAmount The total bid amount for which the bidder can directly purchase the auctioned items and close the auction as a result.
+     *  @param timeBufferInSeconds This is a buffer e.g. x seconds. If a new winning bid is made less than x seconds before expirationTimestamp, the
+     *                             expirationTimestamp is increased by x seconds.
+     *  @param bidBufferBps This is a buffer in basis points e.g. x%. To be considered as a new winning bid, a bid must be at least x% greater than
+     *                      the current winning bid.
+     *  @param startTimestamp The timestamp at and after which bids can be made to the auction
+     *  @param endTimestamp The timestamp at and after which bids cannot be made to the auction.
+     */
     struct AuctionParameters {
         address assetContract;
         uint256 tokenId;
@@ -206,6 +227,26 @@ interface IEnglishAuctions {
         uint64 endTimestamp;
     }
 
+    /**
+     *  @notice The information stored for an auction.
+     *
+     *  @param auctionId The unique ID of the auction.
+     *  @param auctionCreator The creator of the auction.
+     *  @param assetContract The address of the smart contract of the NFTs being auctioned.
+     *  @param tokenId The tokenId of the NFTs being auctioned.
+     *  @param quantity The quantity of NFTs being auctioned. This must be non-zero, and is expected to
+     *                  be `1` for ERC-721 NFTs.
+     *  @param currency The currency in which the bid must be made when bidding for the auctioned NFTs.
+     *  @param minimumBidAmount The minimum bid amount for the auction.
+     *  @param buyoutBidAmount The total bid amount for which the bidder can directly purchase the auctioned items and close the auction as a result.
+     *  @param timeBufferInSeconds This is a buffer e.g. x seconds. If a new winning bid is made less than x seconds before expirationTimestamp, the
+     *                             expirationTimestamp is increased by x seconds.
+     *  @param bidBufferBps This is a buffer in basis points e.g. x%. To be considered as a new winning bid, a bid must be at least x% greater than
+     *                      the current winning bid.
+     *  @param startTimestamp The timestamp at and after which bids can be made to the auction
+     *  @param endTimestamp The timestamp at and after which bids cannot be made to the auction.
+     *  @param tokenType The type of NFTs auctioned (ERC-721 or ERC-1155)
+     */
     struct Auction {
         uint256 auctionId;
         address auctionCreator;
@@ -222,6 +263,13 @@ interface IEnglishAuctions {
         TokenType tokenType;
     }
 
+    /**
+     *  @notice The information stored for a bid made in an auction.
+     *
+     *  @param auctionId The unique ID of the auction.
+     *  @param bidder The address of the bidder.
+     *  @param bidAmount The total bid amount (in the currency specified by the auction).
+     */
     struct Bid {
         uint256 auctionId;
         address bidder;
@@ -243,22 +291,67 @@ interface IEnglishAuctions {
         address winningBidder
     );
 
+    /**
+     *  @notice Put up NFTs (ERC721 or ERC1155) for an english auction.
+     *
+     *  @param _params The parameters of an auction a seller sets when creating an auction.
+     *
+     *  @return auctionId The unique integer ID of the auction.
+     */
     function createAuction(AuctionParameters memory _params) external returns (uint256 auctionId);
 
+    /**
+     *  @notice Update parameters of an auction.
+     *
+     *  @param _auctionId The ID of the auction to update.
+     *  @param _params The parameters of an auction a seller sets when updating an auction.
+     */
+    function updateAuction(uint256 _auctionId, AuctionParameters memory _params) external;
+
+    /**
+     *  @notice Cancel an auction.
+     *
+     *  @param _auctionId The ID of the auction to cancel.
+     */
     function cancelAuction(uint256 _auctionId) external;
 
+    /**
+     *  @notice Distribute the winning bid amount to the auction creator.
+     *
+     *  @param _auctionId The ID of an auction.
+     */
     function collectAuctionPayout(uint256 _auctionId) external;
 
+    /**
+     *  @notice Distribute the auctioned NFTs to the winning bidder.
+     *
+     *  @param _auctionId The ID of an auction.
+     */
     function collectAuctionTokens(uint256 _auctionId) external;
 
+    /**
+     *  @notice Bid in an active auction.
+     *
+     *  @param _auctionId The ID of the auction to bid in.
+     *  @param _bidAmount The bid amount in the currency specified by the auction.
+     */
     function bidInAuction(uint256 _auctionId, uint256 _bidAmount) external payable;
 
+    /**
+     *  @notice Returns whether a given bid amount would make for a winning bid in an auction.
+     *
+     *  @param _auctionId The ID of an auction.
+     *  @param _bidAmount The bid amount to check.
+     */
     function isNewWinningBid(uint256 _auctionId, uint256 _bidAmount) external view returns (bool);
 
+    /// @notice Returns the auction of the provided auction ID.
     function getAuction(uint256 _auctionId) external view returns (Auction memory auction);
 
+    /// @notice Returns all auctions that are active.
     function getAllAuctions() external view returns (Auction[] memory auctions);
 
+    /// @notice Returns the winning bid of an active auction.
     function getWinningBid(uint256 _auctionId)
         external
         view
@@ -268,8 +361,13 @@ interface IEnglishAuctions {
             uint256 bidAmount
         );
 
+    /// @notice Returns whether an auction is active.
     function isAuctionExpired(uint256 _auctionId) external view returns (bool);
 }
+
+/**
+ *  The `Offers` extension smart contract lets you make and accept offers made for NFTs (ERC-721 or ERC-1155).
+ */
 
 interface IOffers {
     enum TokenType {
@@ -278,6 +376,16 @@ interface IOffers {
         ERC20
     }
 
+    /**
+     *  @notice The parameters an offeror sets when making an offer for NFTs.
+     *
+     *  @param assetContract The contract of the NFTs for which the offer is being made.
+     *  @param tokenId The tokenId of the NFT for which the offer is being made.
+     *  @param quantity The quantity of NFTs wanted.
+     *  @param currency The currency offered for the NFTs.
+     *  @param totalPrice The total offer amount for the NFTs.
+     *  @param expirationTimestamp The timestamp at and after which the offer cannot be accepted.
+     */
     struct OfferParams {
         address assetContract;
         uint256 tokenId;
@@ -287,23 +395,38 @@ interface IOffers {
         uint256 expirationTimestamp;
     }
 
+    /**
+     *  @notice The information stored for the offer made.
+     *
+     *  @param offerId The ID of the offer.
+     *  @param offeror The address of the offeror.
+     *  @param assetContract The contract of the NFTs for which the offer is being made.
+     *  @param tokenId The tokenId of the NFT for which the offer is being made.
+     *  @param quantity The quantity of NFTs wanted.
+     *  @param currency The currency offered for the NFTs.
+     *  @param totalPrice The total offer amount for the NFTs.
+     *  @param expirationTimestamp The timestamp at and after which the offer cannot be accepted.
+     *  @param tokenType The type of token (ERC-721 or ERC-1155) the offer is made for.
+     */
     struct Offer {
         uint256 offerId;
         address offeror;
         address assetContract;
         uint256 tokenId;
-        TokenType tokenType;
         uint256 quantity;
         address currency;
         uint256 totalPrice;
         uint256 expirationTimestamp;
+        TokenType tokenType;
     }
 
     /// @dev Emitted when a new offer is created.
     event NewOffer(address indexed offeror, uint256 indexed offerId, Offer offer);
 
+    /// @dev Emitted when an offer is cancelled.
     event CancelledOffer(address indexed offeror, uint256 indexed offerId);
 
+    /// @dev Emitted when an offer is accepted.
     event NewSale(
         uint256 indexed offerId,
         address indexed assetContract,
@@ -313,15 +436,35 @@ interface IOffers {
         uint256 totalPricePaid
     );
 
+    /**
+     *  @notice Make an offer for NFTs (ERC-721 or ERC-1155)
+     *
+     *  @param _params The parameters of an offer.
+     *
+     *  @return offerId The unique integer ID assigned to the offer.
+     */
     function makeOffer(OfferParams memory _params) external returns (uint256 offerId);
 
+    /**
+     *  @notice Cancel an offer.
+     *
+     *  @param _offerId The ID of the offer to cancel.
+     */
     function cancelOffer(uint256 _offerId) external;
 
+    /**
+     *  @notice Accept an offer.
+     *
+     *  @param _offerId The ID of the offer to accept.
+     */
     function acceptOffer(uint256 _offerId) external;
 
+    /// @notice Returns an offer for the given offer ID.
     function getOffer(uint256 _offerId) external view returns (Offer memory offer);
 
+    /// @notice Returns all active (i.e. non-expired or cancelled) offers.
     function getAllOffers(uint256 _startId, uint256 _endId) external view returns (Offer[] memory offers);
 
+    /// @notice Returns all valid offers. An offer is valid if the offeror owns and has approved Marketplace to transfer the offer amount of currency.
     function getAllValidOffers(uint256 _startId, uint256 _endId) external view returns (Offer[] memory offers);
 }
