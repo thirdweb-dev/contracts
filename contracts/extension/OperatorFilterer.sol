b24252e8e9d7bdb12141d61036a8c80a03914936
Krishang <krishang@thirdweb.com>
2022-11-14 20:08:33 +0530

    Fix OpenSea extensions

--- contracts/extension/OperatorFilterer.sol
+++ contracts/extension/OperatorFilterer.sol
@@ -1,36 +1,46 @@
 // SPDX-License-Identifier: Apache 2.0
-// Credits: OpenSea
+// Credit: OpenSea
 pragma solidity ^0.8.0;
 
 import { IOperatorFilterRegistry } from "./interface/IOperatorFilterRegistry.sol";
 
-contract OperatorFilterer {
+abstract contract OperatorFilterer {
     error OperatorNotAllowed(address operator);
 
-    IOperatorFilterRegistry constant OPERATOR_FILTERER_REGISTRY =
+    IOperatorFilterRegistry constant operatorFilterRegistry =
         IOperatorFilterRegistry(0x000000000000AAeB6D7670E522A718067333cd4E);
 
     constructor(address subscriptionOrRegistrantToCopy, bool subscribe) {
         // If an inheriting token contract is deployed to a network without the registry deployed, the modifier
         // will not revert, but the contract will need to be registered with the registry once it is deployed in
         // order for the modifier to filter addresses.
-        if (address(OPERATOR_FILTERER_REGISTRY).code.length > 0) {
+        if (address(operatorFilterRegistry).code.length > 0) {
             if (subscribe) {
-                OPERATOR_FILTERER_REGISTRY.registerAndSubscribe(address(this), subscriptionOrRegistrantToCopy);
+                operatorFilterRegistry.registerAndSubscribe(address(this), subscriptionOrRegistrantToCopy);
             } else {
                 if (subscriptionOrRegistrantToCopy != address(0)) {
-                    OPERATOR_FILTERER_REGISTRY.registerAndCopyEntries(address(this), subscriptionOrRegistrantToCopy);
+                    operatorFilterRegistry.registerAndCopyEntries(address(this), subscriptionOrRegistrantToCopy);
                 } else {
-                    OPERATOR_FILTERER_REGISTRY.register(address(this));
+                    operatorFilterRegistry.register(address(this));
                 }
             }
         }
     }
 
-    modifier onlyAllowedOperator() virtual {
+    modifier onlyAllowedOperator(address from) virtual {
         // Check registry code length to facilitate testing in environments without a deployed registry.
-        if (address(OPERATOR_FILTERER_REGISTRY).code.length > 0) {
-            if (!OPERATOR_FILTERER_REGISTRY.isOperatorAllowed(address(this), msg.sender)) {
+        if (address(operatorFilterRegistry).code.length > 0) {
+            // Allow spending tokens from addresses with balance
+            // Note that this still allows listings and marketplaces with escrow to transfer tokens if transferred
+            // from an EOA.
+            if (from == msg.sender) {
+                _;
+                return;
+            }
+            if (
+                !(operatorFilterRegistry.isOperatorAllowed(address(this), msg.sender) &&
+                    operatorFilterRegistry.isOperatorAllowed(address(this), from))
+            ) {
                 revert OperatorNotAllowed(msg.sender);
             }
         }
