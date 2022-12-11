3014c2c13c13cc1a0a2f6c0b37fa8a7077fbcaa5
Atharva Deosthale <atharva.deosthale17@gmail.com>
2022-12-12 01:23:23 +0530

    fix: add virtual so that function can be overriden (#289)
    * fix: add virtual so that function can be overriden
    
    * fix: virtual keyword for Staking721Base and Staking1155Base

--- contracts/base/Staking20Base.sol
+++ contracts/base/Staking20Base.sol
@@ -62,7 +62,7 @@ contract Staking20Base is ContractMetadata, Multicall, Ownable, Staking20 {
      *  @param _rewards   Amount of tokens to be given out as reward.
      *
      */
-    function _mintRewards(address _staker, uint256 _rewards) internal override {
+    function _mintRewards(address _staker, uint256 _rewards) internal virtual override {
         // Mint or transfer reward-tokens here.
         // e.g.
         //
