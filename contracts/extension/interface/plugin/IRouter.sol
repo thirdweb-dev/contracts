67dc69726e0969e51b7826048609664dd9ca9899
Krishang <krishang@thirdweb.com>
2023-01-31 15:13:51 -0500

    fix IRouter name

--- contracts/extension/interface/plugin/IRouter.sol
+++ contracts/extension/interface/plugin/IRouter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./IPluginMap.sol";
 
-interface IRouterPrev is IPluginMap {
+interface IRouter is IPluginMap {
     /*///////////////////////////////////////////////////////////////
                                 Events
     //////////////////////////////////////////////////////////////*/
