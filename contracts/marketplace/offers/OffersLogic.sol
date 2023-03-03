437a5cb35cf8c9ccc88677d199ef31c21f39b29b
Yash <kumaryashcse@gmail.com>
2023-03-04 04:43:02 +0530

    make MAX_BPS private

--- contracts/marketplace/offers/OffersLogic.sol
+++ contracts/marketplace/offers/OffersLogic.sol
@@ -32,7 +32,7 @@ contract OffersLogic is IOffers, ReentrancyGuard {
     bytes32 private constant ASSET_ROLE = keccak256("ASSET_ROLE");
 
     /// @dev The max bps of the contract. So, 10_000 == 100 %
-    uint64 public constant MAX_BPS = 10_000;
+    uint64 private constant MAX_BPS = 10_000;
 
     /*///////////////////////////////////////////////////////////////
                               Modifiers
