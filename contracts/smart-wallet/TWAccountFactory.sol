5c87a49170233b2f5d846a327e7084c62430aec1
Krishang <krishang@thirdweb.com>
2023-03-28 10:06:41 -0400

    Update TWAccountFactory

--- contracts/smart-wallet/TWAccountFactory.sol
+++ contracts/smart-wallet/TWAccountFactory.sol
@@ -1,59 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/utils/Create2.sol";
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/utils/Multicall.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 
 import "./TWAccount.sol";
 
 /**
- * A sample factory contract for TWAccount
- * A UserOperations "initCode" holds the address of the factory, and a method call (to createAccount, in this sample factory).
- * The factory's createAccount returns the target account address even if it is already installed.
- * This way, the entryPoint.getSenderAddress() can be called either before or after the account is created.
+ *  TWAccountFactory capabilities:
+ *  - deploy a clone pointing to a TWAccount implementation.
  */
-contract TWAccountFactory {
+contract TWAccountFactory is Multicall {
     TWAccount public immutable accountImplementation;
 
     constructor(IEntryPoint _entryPoint) {
         accountImplementation = new TWAccount(_entryPoint);
     }
 
-    /**
-     * create an account, and return its address.
-     * returns the address even if the account is already deployed.
-     * Note that during UserOperation execution, this method is called only if the account is not deployed.
-     * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after account creation
-     */
-    function createAccount(address owner, uint256 salt) public returns (TWAccount ret) {
-        address addr = getAddress(owner, salt);
-        uint256 codeSize = addr.code.length;
-        if (codeSize > 0) {
-            return TWAccount(payable(addr));
+    /// @notice Deploys a new Account with the given salt and initialization data.
+    function createAccount(bytes32 _salt, bytes calldata _initData) external returns (address account) {
+        address impl = address(accountImplementation);
+        account = Clones.cloneDeterministic(impl, _salt);
+
+        // TODO: Emit event
+
+        if (_initData.length > 0) {
+            // slither-disable-next-line unused-return
+            Address.functionCall(account, _initData);
         }
-        ret = TWAccount(
-            payable(
-                new ERC1967Proxy{ salt: bytes32(salt) }(
-                    address(accountImplementation),
-                    abi.encodeCall(TWAccount.initialize, (owner))
-                )
-            )
-        );
     }
 
-    /**
-     * calculate the counterfactual address of this account as it would be returned by createAccount()
-     */
-    function getAddress(address owner, uint256 salt) public view returns (address) {
-        return
-            Create2.computeAddress(
-                bytes32(salt),
-                keccak256(
-                    abi.encodePacked(
-                        type(ERC1967Proxy).creationCode,
-                        abi.encode(address(accountImplementation), abi.encodeCall(TWAccount.initialize, (owner)))
-                    )
-                )
-            );
+    /// @notice Returns the address of an Account that would be deployed with the given salt.
+    function getAddress(bytes32 _salt) external view returns (address) {
+        return Clones.predictDeterministicAddress(address(accountImplementation), _salt);
     }
 }
---
+++ contracts/smart-wallet/interfaces/ITWAccountFactory.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+interface ITWAccountFactory {
+    /// @notice Returns the address of the Account implementation.
+    function accountImplementation() external view returns (address);
+
+    /// @notice Deploys a new Account with the given salt and initialization data.
+    function createAccount(bytes32 _salt, bytes calldata _initData) external returns (address account);
+
+    /// @notice Returns the address of an Account that would be deployed with the given salt.
+    function getAddress(bytes32 _salt) external view returns (address);
+}
