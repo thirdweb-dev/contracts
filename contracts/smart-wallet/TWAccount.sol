49f3bc3f06b9f447f5f24a72db3676bcb7a62300
Krishang <krishang@thirdweb.com>
2023-04-14 12:47:55 -0400

    Remove unused test events

--- contracts/smart-wallet/TWAccount.sol
+++ contracts/smart-wallet/TWAccount.sol
@@ -176,9 +176,6 @@ contract TWAccount is
         data.nonce += 1;
     }
 
-    event TWAccountOpHash(bytes32 opHash);
-    event TWAccountMsgHash(bytes32 opHash);
-
     /// @notice Validates the signature of a user operation.
     function _validateSignature(UserOperation calldata userOp, bytes32 userOpHash)
         internal
@@ -186,9 +183,7 @@ contract TWAccount is
         override
         returns (uint256 validationData)
     {
-        emit TWAccountOpHash(userOpHash);
         bytes32 hash = userOpHash.toEthSignedMessageHash();
-        emit TWAccountMsgHash(hash);
         address signer = hash.recover(userOp.signature);
 
         if (!isValidSigner(signer)) return SIG_VALIDATION_FAILED;
