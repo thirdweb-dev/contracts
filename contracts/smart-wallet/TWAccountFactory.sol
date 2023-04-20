6bcd4ec624525d27b23c674d0846be0801b296a3
Krishang <krishang@thirdweb.com>
2023-04-19 20:05:46 -0400

    Remove state from account factories

--- contracts/smart-wallet/TWAccountFactory.sol
+++ contracts/smart-wallet/TWAccountFactory.sol
@@ -21,26 +21,6 @@ import "./TWAccount.sol";
 //   \$$$$  |$$ |  $$ |$$ |$$ |      \$$$$$$$ |\$$$$$\$$$$  |\$$$$$$$\ $$$$$$$  |
 //    \____/ \__|  \__|\__|\__|       \_______| \_____\____/  \_______|\_______/
 
-/*///////////////////////////////////////////////////////////////
-                            Storage layout
-//////////////////////////////////////////////////////////////*/
-
-library TWAccountFactoryStorage {
-    bytes32 internal constant TWACCOUNT_FACTORY_STORAGE_POSITION = keccak256("twaccount.factory.storage");
-
-    struct Data {
-        TWStringSet.Set allAccounts;
-        mapping(address => TWStringSet.Set) accountsOfSigner;
-    }
-
-    function factoryStorage() internal pure returns (Data storage twaccountFactoryData) {
-        bytes32 position = TWACCOUNT_FACTORY_STORAGE_POSITION;
-        assembly {
-            twaccountFactoryData.slot := position
-        }
-    }
-}
-
 contract TWAccountFactory is ITWAccountFactory, Multicall {
     using TWStringSet for TWStringSet.Set;
 
@@ -76,8 +56,6 @@ contract TWAccountFactory is ITWAccountFactory, Multicall {
 
         TWAccount(payable(account)).initialize(_admin);
 
-        _setupAccount(_admin, _accountId);
-
         emit AccountCreated(account, _admin, _accountId);
 
         return account;
@@ -96,39 +74,5 @@ contract TWAccountFactory is ITWAccountFactory, Multicall {
     function getAddress(string memory _accountId) public view returns (address) {
         bytes32 salt = keccak256(abi.encode(_accountId));
         return Clones.predictDeterministicAddress(address(_accountImplementation), salt);
-    }
-
-    /// @notice Returns the list of accounts created by a signer.
-    function getAccountsOfSigner(address _signer) external view returns (AccountInfo[] memory) {
-        TWAccountFactoryStorage.Data storage data = TWAccountFactoryStorage.factoryStorage();
-        return _formatAccounts(data.accountsOfSigner[_signer].values());
-    }
-
-    /// @notice Returns the list of all accounts.
-    function getAllAccounts() external view returns (AccountInfo[] memory accounts) {
-        TWAccountFactoryStorage.Data storage data = TWAccountFactoryStorage.factoryStorage();
-        return _formatAccounts(data.allAccounts.values());
-    }
-
-    /*///////////////////////////////////////////////////////////////
-                            Internal functions
-    //////////////////////////////////////////////////////////////*/
-
-    /// @dev Formats a list of accountIds to a list of `AccountInfo` (account id + account address).
-    function _formatAccounts(string[] memory _accountIds) internal view returns (AccountInfo[] memory accounts) {
-        uint256 len = _accountIds.length;
-        accounts = new AccountInfo[](len);
-        for (uint256 i = 0; i < len; i += 1) {
-            string memory accountId = _accountIds[i];
-            address account = getAddress(accountId);
-            accounts[i] = AccountInfo(accountId, account);
-        }
-    }
-
-    /// @dev Adds an account to the list of accounts created by a signer.
-    function _setupAccount(address _signer, string memory _accountId) internal {
-        TWAccountFactoryStorage.Data storage data = TWAccountFactoryStorage.factoryStorage();
-        data.allAccounts.add(_accountId);
-        data.accountsOfSigner[_signer].add(_accountId);
     }
 }
