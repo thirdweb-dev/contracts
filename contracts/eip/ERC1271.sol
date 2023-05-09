bc8245e50cd9e122aadfadca704ae0639f1ebc78
Krishang <krishang@thirdweb.com>
2023-05-09 17:21:08 -0400

    Add ERC-1271

---
+++ contracts/eip/ERC1271.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+contract ERC1271 {
+    // bytes4(keccak256("isValidSignature(bytes32,bytes)")
+    bytes4 internal constant MAGICVALUE = 0x1626ba7e;
+
+    /**
+     * @dev Should return whether the signature provided is valid for the provided hash
+     * @param _hash      Hash of the data to be signed
+     * @param _signature Signature byte array associated with _hash
+     *
+     * MUST return the bytes4 magic value 0x1626ba7e when function passes.
+     * MUST NOT modify state (using STATICCALL for solc < 0.5, view modifier for solc > 0.5)
+     * MUST allow external calls
+     */
+    function isValidSignature(bytes32 _hash, bytes memory _signature) public view returns (bytes4 magicValue);
+}
