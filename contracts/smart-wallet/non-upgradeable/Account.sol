4252c370132c9a3c82026331fc1dc559ea9d4465
nkrishang <62195808+nkrishang@users.noreply.github.com>
2023-05-08 20:25:09 -0400

    Remove OpenZeppelin dependency from smart wallets (#382)
    * release package
    
    * Remove OZ dependency from Account contracts
    
    * pkg update

--- contracts/smart-wallet/non-upgradeable/Account.sol
+++ contracts/smart-wallet/non-upgradeable/Account.sol
@@ -13,8 +13,8 @@ import "../../extension/Multicall.sol";
 import "../../dynamic-contracts/extension/Initializable.sol";
 import "../../dynamic-contracts/extension/PermissionsEnumerable.sol";
 import "../../dynamic-contracts/extension/ContractMetadata.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
-import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "../../openzeppelin-presets/token/ERC721/utils/ERC721Holder.sol";
+import "../../openzeppelin-presets/token/ERC1155/utils/ERC1155Holder.sol";
 
 // Utils
 import "../../openzeppelin-presets/utils/cryptography/ECDSA.sol";
