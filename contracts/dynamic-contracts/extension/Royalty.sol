11deccc2ef302b9b0195e7bf54d0eefc6072db51
Yash <kumaryashcse@gmail.com>
2023-06-08 01:07:46 +0530

    burn to claim drop contract

---
+++ contracts/dynamic-contracts/extension/Royalty.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+/// @author thirdweb
+
+import "../../extension/interface/IRoyalty.sol";
+
+library RoyaltyStorage {
+    bytes32 public constant ROYALTY_STORAGE_POSITION = keccak256("royalty.storage");
+
+    struct Data {
+        /// @dev The (default) address that receives all royalty value.
+        address royaltyRecipient;
+        /// @dev The (default) % of a sale to take as royalty (in basis points).
+        uint16 royaltyBps;
+        /// @dev Token ID => royalty recipient and bps for token
+        mapping(uint256 => IRoyalty.RoyaltyInfo) royaltyInfoForToken;
+    }
+
+    function royaltyStorage() internal pure returns (Data storage royaltyData) {
+        bytes32 position = ROYALTY_STORAGE_POSITION;
+        assembly {
+            royaltyData.slot := position
+        }
+    }
+}
+
+/**
+ *  @title   Royalty
+ *  @notice  Thirdweb's `Royalty` is a contract extension to be used with any base contract. It exposes functions for setting and reading
+ *           the recipient of royalty fee and the royalty fee basis points, and lets the inheriting contract perform conditional logic
+ *           that uses information about royalty fees, if desired.
+ *
+ *  @dev     The `Royalty` contract is ERC2981 compliant.
+ */
+
+abstract contract Royalty is IRoyalty {
+    /**
+     *  @notice   View royalty info for a given token and sale price.
+     *  @dev      Returns royalty amount and recipient for `tokenId` and `salePrice`.
+     *  @param tokenId          The tokenID of the NFT for which to query royalty info.
+     *  @param salePrice        Sale price of the token.
+     *
+     *  @return receiver        Address of royalty recipient account.
+     *  @return royaltyAmount   Royalty amount calculated at current royaltyBps value.
+     */
+    function royaltyInfo(uint256 tokenId, uint256 salePrice)
+        external
+        view
+        virtual
+        override
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        (address recipient, uint256 bps) = getRoyaltyInfoForToken(tokenId);
+        receiver = recipient;
+        royaltyAmount = (salePrice * bps) / 10_000;
+    }
+
+    /**
+     *  @notice          View royalty info for a given token.
+     *  @dev             Returns royalty recipient and bps for `_tokenId`.
+     *  @param _tokenId  The tokenID of the NFT for which to query royalty info.
+     */
+    function getRoyaltyInfoForToken(uint256 _tokenId) public view override returns (address, uint16) {
+        RoyaltyStorage.Data storage data = RoyaltyStorage.royaltyStorage();
+
+        RoyaltyInfo memory royaltyForToken = data.royaltyInfoForToken[_tokenId];
+
+        return
+            royaltyForToken.recipient == address(0)
+                ? (data.royaltyRecipient, uint16(data.royaltyBps))
+                : (royaltyForToken.recipient, uint16(royaltyForToken.bps));
+    }
+
+    /**
+     *  @notice Returns the defualt royalty recipient and BPS for this contract's NFTs.
+     */
+    function getDefaultRoyaltyInfo() external view override returns (address, uint16) {
+        RoyaltyStorage.Data storage data = RoyaltyStorage.royaltyStorage();
+        return (data.royaltyRecipient, uint16(data.royaltyBps));
+    }
+
+    /**
+     *  @notice         Updates default royalty recipient and bps.
+     *  @dev            Caller should be authorized to set royalty info.
+     *                  See {_canSetRoyaltyInfo}.
+     *                  Emits {DefaultRoyalty Event}; See {_setupDefaultRoyaltyInfo}.
+     *
+     *  @param _royaltyRecipient   Address to be set as default royalty recipient.
+     *  @param _royaltyBps         Updated royalty bps.
+     */
+    function setDefaultRoyaltyInfo(address _royaltyRecipient, uint256 _royaltyBps) external override {
+        if (!_canSetRoyaltyInfo()) {
+            revert("Not authorized");
+        }
+
+        _setupDefaultRoyaltyInfo(_royaltyRecipient, _royaltyBps);
+    }
+
+    /// @dev Lets a contract admin update the default royalty recipient and bps.
+    function _setupDefaultRoyaltyInfo(address _royaltyRecipient, uint256 _royaltyBps) internal {
+        if (_royaltyBps > 10_000) {
+            revert("Exceeds max bps");
+        }
+
+        RoyaltyStorage.Data storage data = RoyaltyStorage.royaltyStorage();
+
+        data.royaltyRecipient = _royaltyRecipient;
+        data.royaltyBps = uint16(_royaltyBps);
+
+        emit DefaultRoyalty(_royaltyRecipient, _royaltyBps);
+    }
+
+    /**
+     *  @notice         Updates default royalty recipient and bps for a particular token.
+     *  @dev            Sets royalty info for `_tokenId`. Caller should be authorized to set royalty info.
+     *                  See {_canSetRoyaltyInfo}.
+     *                  Emits {RoyaltyForToken Event}; See {_setupRoyaltyInfoForToken}.
+     *
+     *  @param _recipient   Address to be set as royalty recipient for given token Id.
+     *  @param _bps         Updated royalty bps for the token Id.
+     */
+    function setRoyaltyInfoForToken(
+        uint256 _tokenId,
+        address _recipient,
+        uint256 _bps
+    ) external override {
+        if (!_canSetRoyaltyInfo()) {
+            revert("Not authorized");
+        }
+
+        _setupRoyaltyInfoForToken(_tokenId, _recipient, _bps);
+    }
+
+    /// @dev Lets a contract admin set the royalty recipient and bps for a particular token Id.
+    function _setupRoyaltyInfoForToken(
+        uint256 _tokenId,
+        address _recipient,
+        uint256 _bps
+    ) internal {
+        if (_bps > 10_000) {
+            revert("Exceeds max bps");
+        }
+
+        RoyaltyStorage.Data storage data = RoyaltyStorage.royaltyStorage();
+
+        data.royaltyInfoForToken[_tokenId] = RoyaltyInfo({ recipient: _recipient, bps: _bps });
+
+        emit RoyaltyForToken(_tokenId, _recipient, _bps);
+    }
+
+    /// @dev Returns whether royalty info can be set in the given execution context.
+    function _canSetRoyaltyInfo() internal view virtual returns (bool);
+}
