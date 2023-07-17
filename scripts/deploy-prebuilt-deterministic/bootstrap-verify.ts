0373104f5d9656b04ecbbb534fc7843e86f8a1d4
Yash <72552910+kumaryash90@users.noreply.github.com>
2023-07-17 15:01:22 +0530

    Script for deploying prebuilts on a network (#424)
    * script for deploying prebuilts on a network
    
    * update script

--- scripts/deploy-prebuilt-deterministic/bootstrap-verify.ts
+++ scripts/deploy-prebuilt-deterministic/bootstrap-verify.ts
@@ -1,19 +1,19 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
-import { apiMap, chainIdApiKey, chainIdToName } from "./constants";
+import { apiMap, chainIdApiKey, contractsToDeploy } from "./constants";
 
-////// To run this script: `npx ts-node scripts/deploy-prebuilt-deterministic/verify.ts` //////
-const deployedContractName = "VoteERC20";
+////// To run this script: `npx ts-node scripts/deploy-prebuilt-deterministic/bootstrap-verify.ts` //////
+const chainId = "8453"; // update here
 
 async function main() {
   console.log("---------- Verification ---------");
   console.log();
-  for (const [chainId, networkName] of Object.entries(chainIdToName)) {
-    const sdk = new ThirdwebSDK(chainId);
-    console.log("Network: ", networkName);
+
+  const sdk = new ThirdwebSDK(chainId);
+  for (const publishedContractName of contractsToDeploy) {
     try {
       await sdk.verifier.verifyThirdwebContract(
-        deployedContractName,
+        publishedContractName,
         apiMap[parseInt(chainId)],
         chainIdApiKey[parseInt(chainId)] as string,
       );
