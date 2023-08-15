c5033936518a089e209ed825818215b0bf21457b
Yash <kumaryashcse@gmail.com>
2023-08-15 16:42:06 +0530

    disable initializer

--- contracts/unaudited/burn-to-claim-drop/BurnToClaimDropERC721.sol
+++ contracts/unaudited/burn-to-claim-drop/BurnToClaimDropERC721.sol
@@ -48,7 +48,9 @@ contract BurnToClaimDropERC721 is
                     Constructor + initializer logic
     //////////////////////////////////////////////////////////////*/
 
-    constructor(Extension[] memory _extensions) BaseRouter(_extensions) {}
+    constructor(Extension[] memory _extensions) BaseRouter(_extensions) {
+        _disableInitializers();
+    }
 
     /// @dev Initiliazes the contract, like a constructor.
     function initialize(
