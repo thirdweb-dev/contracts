4202532182930911a029800570a36b6b526534f7
0xFirekeeper <0xFirekeeper@gmail.com>
2023-08-31 22:15:50 +0300

    Full game tests

--- src/test/Game.t.sol
+++ src/test/Game.t.sol
@@ -36,22 +36,54 @@ contract GameTest is BaseTest {
     }
 
     function testAdminFunctionality() public {
-        // Test changing the admin
+        address randomAddress = getActor(69);
+
+        // Test setting a new admin
         address newAdmin = getActor(1);
         vm.prank(admin);
         game.setAdmin(newAdmin);
         assertTrue(game.isAdmin(newAdmin), "New admin should be set");
 
-        vm.startPrank(newAdmin);
+        // Test setting the same admin again (should fail)
+        vm.prank(newAdmin);
+        vm.expectRevert("GameRouter: AlreadyAdmin.");
+        game.setAdmin(newAdmin);
+
+        // Test setting an admin as a non-admin (should fail)
+        vm.prank(randomAddress);
+        vm.expectRevert("GameLibrary: Not admin.");
+        game.setAdmin(randomAddress);
 
         // Test adding a manager
         address newManager = getActor(2);
+        vm.prank(newAdmin);
         game.addManager(newManager);
         assertTrue(game.isManager(newManager), "New manager should be added");
 
+        // Test adding the same manager (should fail)
+        vm.prank(newAdmin);
+        vm.expectRevert("GameRouter: Manager already exists.");
+        game.addManager(newManager);
+
+        // Test adding a manager as a non-admin (should fail)
+        vm.prank(randomAddress);
+        vm.expectRevert("GameLibrary: Not admin.");
+        game.addManager(randomAddress);
+
         // Test removing a manager
+        vm.prank(newAdmin);
         game.removeManager(newManager);
         assertFalse(game.isManager(newManager), "Manager should be removed");
+
+        // Test removing a non-existing manager (should fail)
+        vm.prank(newAdmin);
+        vm.expectRevert("GameRouter: Manager does not exist.");
+        game.removeManager(newManager);
+
+        // Test removing a manager as a non-admin (should fail)
+        vm.prank(randomAddress);
+        vm.expectRevert("GameLibrary: Not admin.");
+        game.removeManager(randomAddress);
 
         // Test updating metadata
         IGame.GameMetadata memory metadata = IGame.GameMetadata(
@@ -60,31 +92,58 @@ contract GameTest is BaseTest {
             "https://newtest.com",
             "https://newtest.com/logo.png"
         );
+        vm.prank(newAdmin);
         game.updateMetadata(metadata);
 
-        vm.stopPrank();
+        // Test updating metadata as a non-admin (should fail)
+        vm.prank(randomAddress);
+        vm.expectRevert("GameLibrary: Not admin.");
+        game.updateMetadata(metadata);
     }
 
     function testManagerFunctionality() public {
+        address randomAddress = getActor(69);
+
         address manager = getActor(2);
         vm.prank(admin);
         game.addManager(manager);
 
-        vm.startPrank(manager);
-
         // Test adding a player
         address player = getActor(3);
+        vm.prank(manager);
         game.addPlayer(player);
         assertTrue(game.isPlayer(player), "New player should be added");
 
+        // Test adding the same player again (should fail)
+        vm.expectRevert("GameRouter: Player already exists.");
+        vm.prank(manager);
+        game.addPlayer(player);
+
+        // Test adding a player as a non-manager (should fail)
+        vm.prank(randomAddress);
+        vm.expectRevert("GameLibrary: Not manager.");
+        game.addPlayer(randomAddress);
+
         // Test removing a player
+        vm.prank(manager);
         game.removePlayer(player);
         assertFalse(game.isPlayer(player), "Player should be removed");
 
-        vm.stopPrank();
+        // Test removing a non-existing player (should fail)
+        vm.expectRevert("GameRouter: Player does not exist.");
+        vm.prank(manager);
+        game.removePlayer(player);
+
+        // Test removing a player as a non-manager (should fail)
+        vm.prank(randomAddress);
+        vm.expectRevert("GameLibrary: Not manager.");
+        game.removePlayer(randomAddress);
     }
 
     function testExtensionSetting() public {
+        address randomAddress = getActor(69);
+
+        // Test extension
         TestExtension testExtension = new TestExtension();
         IExtension.ExtensionMetadata memory metadata = IExtension.ExtensionMetadata(
             "Test",
@@ -93,12 +152,34 @@ contract GameTest is BaseTest {
         );
         IExtension.ExtensionFunction[] memory functions = new IExtension.ExtensionFunction[](1);
         functions[0] = IExtension.ExtensionFunction(testExtension.test.selector, "test()");
-
         IExtension.Extension memory extension = IExtension.Extension(metadata, functions);
+
+        // Test adding an extension as admin
         vm.prank(admin);
         game.addExtension(extension);
 
+        // Test adding the same extension again (should fail)
+        vm.prank(admin);
+        vm.expectRevert("ExtensionState: extension already exists.");
+        game.addExtension(extension);
+
+        // Test adding an extension as a non-admin (should fail)
+        vm.prank(randomAddress);
         vm.expectRevert("BaseRouter: not authorized.");
         game.addExtension(extension);
+
+        // Test removing an extension as admin
+        vm.prank(admin);
+        game.removeExtension(extension);
+
+        // Test removing a non-existing extension (should fail)
+        vm.prank(admin);
+        vm.expectRevert("ExtensionState: extension does not exist.");
+        game.removeExtension(extension);
+
+        // Test removing an extension as a non-admin (should fail)
+        vm.prank(randomAddress);
+        vm.expectRevert("BaseRouter: not authorized.");
+        game.removeExtension(extension);
     }
 }
