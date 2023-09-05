d7a411e94a9e61f421506486909e1c5831650ba5
Yash <kumaryashcse@gmail.com>
2023-09-06 01:07:30 +0530

    update ERC2771ContextStorage

--- contracts/extension/upgradeable/ERC2771Context.sol
+++ contracts/extension/upgradeable/ERC2771Context.sol
@@ -8,32 +8,30 @@ import "../interface/IERC2771Context.sol";
  */
 
 library ERC2771ContextStorage {
-    bytes32 public constant ERC2771_CONTEXT_STORAGE_POSITION = keccak256("erc2771.context.storage");
+    bytes32 public constant ERC2771_CONTEXT_STORAGE_POSITION =
+        keccak256(abi.encode(uint256(keccak256("erc2771.context.storage")) - 1));
 
     struct Data {
         mapping(address => bool) trustedForwarder;
     }
 
-    function erc2771ContextStorage() internal pure returns (Data storage erc2771ContextData) {
+    function data() internal pure returns (Data storage data_) {
         bytes32 position = ERC2771_CONTEXT_STORAGE_POSITION;
         assembly {
-            erc2771ContextData.slot := position
+            data_.slot := position
         }
     }
 }
 
 contract ERC2771Context is IERC2771Context {
     constructor(address[] memory trustedForwarder) {
-        ERC2771ContextStorage.Data storage data = ERC2771ContextStorage.erc2771ContextStorage();
-
         for (uint256 i = 0; i < trustedForwarder.length; i++) {
-            data.trustedForwarder[trustedForwarder[i]] = true;
+            _erc2771ContextStorage().trustedForwarder[trustedForwarder[i]] = true;
         }
     }
 
     function isTrustedForwarder(address forwarder) public view virtual returns (bool) {
-        ERC2771ContextStorage.Data storage data = ERC2771ContextStorage.erc2771ContextStorage();
-        return data.trustedForwarder[forwarder];
+        return _erc2771ContextStorage().trustedForwarder[forwarder];
     }
 
     function _msgSender() internal view virtual returns (address sender) {
@@ -53,6 +51,11 @@ contract ERC2771Context is IERC2771Context {
         } else {
             return msg.data;
         }
+    }
+
+    /// @dev Returns the ERC2771ContextStorage storage.
+    function _erc2771ContextStorage() internal pure returns (ERC2771ContextStorage.Data storage data) {
+        data = ERC2771ContextStorage.data();
     }
 
     uint256[49] private __gap;
