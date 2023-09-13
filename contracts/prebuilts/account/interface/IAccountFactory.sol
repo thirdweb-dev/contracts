41bc4743f17cafc1ffae2139d83101c11db70c72
Krishang <krishang@thirdweb.com>
2023-09-13 09:59:23 -0400

    Create IAccountFactoryCore

--- contracts/prebuilts/account/interface/IAccountFactory.sol
+++ contracts/prebuilts/account/interface/IAccountFactory.sol
@@ -1,26 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.12;
 
-interface IAccountFactory {
+import "./IAccountFactoryCore.sol";
+
+interface IAccountFactory is IAccountFactoryCore {
     /*///////////////////////////////////////////////////////////////
-                                Events
+                        Callback Functions
     //////////////////////////////////////////////////////////////*/
-
-    /// @notice Emitted when a new Account is created.
-    event AccountCreated(address indexed account, address indexed accountAdmin);
-
-    /// @notice Emitted when a new signer is added to an Account.
-    event SignerAdded(address indexed account, address indexed signer);
-
-    /// @notice Emitted when a new signer is added to an Account.
-    event SignerRemoved(address indexed account, address indexed signer);
-
-    /*///////////////////////////////////////////////////////////////
-                        Extension Functions
-    //////////////////////////////////////////////////////////////*/
-
-    /// @notice Deploys a new Account for admin.
-    function createAccount(address admin, bytes calldata _data) external returns (address account);
 
     /// @notice Callback function for an Account to register its signers.
     function onSignerAdded(
@@ -35,20 +21,4 @@ interface IAccountFactory {
         address creatorAdmin,
         bytes memory data
     ) external;
-
-    /*///////////////////////////////////////////////////////////////
-                            View Functions
-    //////////////////////////////////////////////////////////////*/
-
-    /// @notice Returns the address of the Account implementation.
-    function accountImplementation() external view returns (address);
-
-    /// @notice Returns all accounts created on the factory.
-    function getAllAccounts() external view returns (address[] memory);
-
-    /// @notice Returns the address of an Account that would be deployed with the given admin signer.
-    function getAddress(address adminSigner, bytes calldata data) external view returns (address);
-
-    /// @notice Returns all accounts on which a signer has (active or inactive) permissions.
-    function getAccountsOfSigner(address signer) external view returns (address[] memory accounts);
 }
