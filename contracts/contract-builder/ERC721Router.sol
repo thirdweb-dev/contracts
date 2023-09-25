93647b0740dca7a849d7f46c8fea2eaf154d0284
Krishang <krishang@thirdweb.com>
2023-09-25 11:21:58 -0400

    Add ERC721Router

---
+++ contracts/contract-builder/ERC721Router.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+/// @author thirdweb
+
+import "./CoreRouter.sol";
+import "./eip/ERC721AExt.sol";
+
+contract ERC721Router is CoreRouter, ERC721AExt {
+    /*///////////////////////////////////////////////////////////////
+                                Constructor
+    //////////////////////////////////////////////////////////////*/
+    constructor(Extension[] memory _defaultExtensions) CoreRouter(_defaultExtensions) {}
+
+    /*///////////////////////////////////////////////////////////////
+                                Initialize
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Initliazes the contract.
+    function initialize(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol,
+        string memory _contractURI,
+        address[] memory _trustedForwarder,
+        Extension[] memory _initExtensions
+    ) external initializer initializerERC721A {
+        // Initialize with extensions
+        __BaseRouter_init_checked(_initExtensions);
+
+        // Initialize ERC2771Context
+        __ERC2771Context_init(_trustedForwarder);
+
+        // Initialize ERC721A
+        __ERC721A_init(_name, _symbol);
+
+        // Initialize contract metadata
+        _setupContractURI(_contractURI);
+
+        // Initialize ownership
+        _setupOwner(_defaultAdmin);
+
+        // Default admin role.
+        _setupRole(0x00, _defaultAdmin);
+
+        // Extension role
+        bytes32 extRole = keccak256("EXTENSION_ROLE");
+        _setupRole(extRole, _defaultAdmin);
+        _setRoleAdmin(extRole, extRole);
+    }
+}
