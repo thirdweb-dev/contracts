c7e57a9897661d02b1484b559d705bfb0eb6bfe7
Krishang <krishang@thirdweb.com>
2023-09-25 19:15:46 -0400

    Move storage to separate file

---
+++ contracts/dynamic-contracts/extension/PermissionsEnumerableStorage.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+/// @author thirdweb
+
+/**
+ *  @title   PermissionsEnumerable
+ *  @dev     This contracts provides extending-contracts with role-based access control mechanisms.
+ *           Also provides interfaces to view all members with a given role, and total count of members.
+ */
+
+library PermissionsEnumerableStorage {
+    bytes32 public constant PERMISSIONS_ENUMERABLE_STORAGE_POSITION = keccak256("permissions.enumerable.storage");
+
+    /**
+     *  @notice A data structure to store data of members for a given role.
+     *
+     *  @param index    Current index in the list of accounts that have a role.
+     *  @param members  map from index => address of account that has a role
+     *  @param indexOf  map from address => index which the account has.
+     */
+    struct RoleMembers {
+        uint256 index;
+        mapping(uint256 => address) members;
+        mapping(address => uint256) indexOf;
+    }
+
+    struct Data {
+        /// @dev map from keccak256 hash of a role to its members' data. See {RoleMembers}.
+        mapping(bytes32 => RoleMembers) roleMembers;
+    }
+
+    function permissionsEnumerableStorage() internal pure returns (Data storage permissionsEnumerableData) {
+        bytes32 position = PERMISSIONS_ENUMERABLE_STORAGE_POSITION;
+        assembly {
+            permissionsEnumerableData.slot := position
+        }
+    }
+}
