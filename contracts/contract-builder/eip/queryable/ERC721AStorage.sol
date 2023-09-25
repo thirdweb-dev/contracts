66e640ff3b8768baffc466d72ffdc0cbe774131e
Krishang <krishang@thirdweb.com>
2023-09-25 09:27:44 -0400

    Add ERC721A contracts

---
+++ contracts/contract-builder/eip/queryable/ERC721AStorage.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+library ERC721AStorage {
+    // Bypass for a `--via-ir` bug (https://github.com/chiru-labs/ERC721A/pull/364).
+    struct TokenApprovalRef {
+        address value;
+    }
+
+    struct Layout {
+        // =============================================================
+        //                            STORAGE
+        // =============================================================
+
+        // The next token ID to be minted.
+        uint256 _currentIndex;
+        // The number of tokens burned.
+        uint256 _burnCounter;
+        // Token name
+        string _name;
+        // Token symbol
+        string _symbol;
+        // Mapping from token ID to ownership details
+        // An empty struct value does not necessarily mean the token is unowned.
+        // See {_packedOwnershipOf} implementation for details.
+        //
+        // Bits Layout:
+        // - [0..159]   `addr`
+        // - [160..223] `startTimestamp`
+        // - [224]      `burned`
+        // - [225]      `nextInitialized`
+        // - [232..255] `extraData`
+        mapping(uint256 => uint256) _packedOwnerships;
+        // Mapping owner address to address data.
+        //
+        // Bits Layout:
+        // - [0..63]    `balance`
+        // - [64..127]  `numberMinted`
+        // - [128..191] `numberBurned`
+        // - [192..255] `aux`
+        mapping(address => uint256) _packedAddressData;
+        // Mapping from token ID to approved address.
+        mapping(uint256 => ERC721AStorage.TokenApprovalRef) _tokenApprovals;
+        // Mapping from owner to operator approvals
+        mapping(address => mapping(address => bool)) _operatorApprovals;
+    }
+
+    bytes32 internal constant STORAGE_SLOT = keccak256("ERC721A.contracts.storage.ERC721A");
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
