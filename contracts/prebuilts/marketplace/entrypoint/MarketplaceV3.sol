6ab04e3228947878f5d4c3c64a6949a7c7ad9a01
Krishang <krishang@thirdweb.com>
2023-09-26 17:47:33 -0400

    Fix [L-3] Cannot remove upgradability without revoking all default admins

--- contracts/prebuilts/marketplace/entrypoint/MarketplaceV3.sol
+++ contracts/prebuilts/marketplace/entrypoint/MarketplaceV3.sol
@@ -98,9 +98,11 @@ contract MarketplaceV3 is
         _setupPlatformFeeInfo(_platformFeeRecipient, _platformFeeBps);
 
         _setupRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
-        _setupRole(EXTENSION_ROLE, _defaultAdmin);
         _setupRole(keccak256("LISTER_ROLE"), address(0));
         _setupRole(keccak256("ASSET_ROLE"), address(0));
+
+        _setupRole(EXTENSION_ROLE, _defaultAdmin);
+        _setRoleAdmin(EXTENSION_ROLE, EXTENSION_ROLE);
     }
 
     /*///////////////////////////////////////////////////////////////
