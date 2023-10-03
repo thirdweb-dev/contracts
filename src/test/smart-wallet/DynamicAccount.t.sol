cbe90e691d6404cf1f74d0f71a2d301c8ceaaea8
Krishang <krishang@thirdweb.com>
2023-10-03 09:48:57 -0400

    Add benchmark test for deployment gas cost

--- src/test/smart-wallet/DynamicAccount.t.sol
+++ src/test/smart-wallet/DynamicAccount.t.sol
@@ -245,6 +245,58 @@ contract DynamicAccountTest is BaseTest {
                         Test: creating an account
     //////////////////////////////////////////////////////////////*/
 
+    /// @dev benchmark test for deployment gas cost
+    function test_deploy_dynamicAccount() public {
+        // Setting up default extension.
+        IExtension.Extension memory defaultExtension;
+
+        defaultExtension.metadata = IExtension.ExtensionMetadata({
+            name: "AccountExtension",
+            metadataURI: "ipfs://AccountExtension",
+            implementation: address(new AccountExtension())
+        });
+
+        defaultExtension.functions = new IExtension.ExtensionFunction[](7);
+
+        defaultExtension.functions[0] = IExtension.ExtensionFunction(
+            AccountExtension.supportsInterface.selector,
+            "supportsInterface(bytes4)"
+        );
+        defaultExtension.functions[1] = IExtension.ExtensionFunction(
+            AccountExtension.execute.selector,
+            "execute(address,uint256,bytes)"
+        );
+        defaultExtension.functions[2] = IExtension.ExtensionFunction(
+            AccountExtension.executeBatch.selector,
+            "executeBatch(address[],uint256[],bytes[])"
+        );
+        defaultExtension.functions[3] = IExtension.ExtensionFunction(
+            ERC721Holder.onERC721Received.selector,
+            "onERC721Received(address,address,uint256,bytes)"
+        );
+        defaultExtension.functions[4] = IExtension.ExtensionFunction(
+            ERC1155Holder.onERC1155Received.selector,
+            "onERC1155Received(address,address,uint256,uint256,bytes)"
+        );
+        defaultExtension.functions[5] = IExtension.ExtensionFunction(
+            bytes4(0), // Selector for `receive()` function.
+            "receive()"
+        );
+        defaultExtension.functions[6] = IExtension.ExtensionFunction(
+            AccountExtension.isValidSignature.selector,
+            "isValidSignature(bytes32,bytes)"
+        );
+
+        IExtension.Extension[] memory extensions = new IExtension.Extension[](1);
+        extensions[0] = defaultExtension;
+
+        // deploy account factory
+        DynamicAccountFactory factory = new DynamicAccountFactory(
+            IEntryPoint(payable(address(entrypoint))),
+            extensions
+        );
+    }
+
     /// @dev Create an account by directly calling the factory.
     function test_state_createAccount_viaFactory() public {
         vm.expectEmit(true, true, false, true);
