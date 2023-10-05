f3494a467b6e07f419bf0db7c288ea94d9f711b7
WhiteOakKong <aadam.debevec@outlook.com>
2023-10-04 23:43:41 -0500

    test: lazyMint

--- src/test/drop/drop-erc721/lazyMint/lazyMint.t.sol
+++ src/test/drop/drop-erc721/lazyMint/lazyMint.t.sol
@@ -72,7 +72,7 @@ contract DropERC721Test_lazyMint is BaseTest {
     }
 
     modifier dataLengthZero() {
-        lazymint_data = "";
+        lazymint_data = abi.encode("");
         _;
     }
 
@@ -91,12 +91,30 @@ contract DropERC721Test_lazyMint is BaseTest {
         _;
     }
 
-    function test_revert_NoMinterRole() public callerWithoutMinterRole {
+    modifier dataValidFormatNoURI() {
+        lazyMint_provenanceHash = bytes32("provenanceHash");
+        lazyMint_encryptedURI = "";
+        lazymint_data = abi.encode(lazyMint_encryptedURI, lazyMint_provenanceHash);
+        console.log(lazymint_data.length);
+        _;
+    }
+
+    modifier dataValidFormatNoHash() {
+        lazyMint_provenanceHash = bytes32("");
+        lazyMint_encryptedURI = "encryptedURI";
+        lazymint_data = abi.encode(lazyMint_encryptedURI, lazyMint_provenanceHash);
+        console.log(lazymint_data.length);
+        _;
+    }
+
+
+
+    function test_revert_NoMinterRole() public callerWithoutMinterRole dataLengthZero {
         vm.expectRevert("Not authorized");
         drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
     }
 
-    function test_revert_AmountEqualZero() public callerWithMinterRole amountEqualZero {
+    function test_revert_AmountEqualZero() public callerWithMinterRole dataLengthZero amountEqualZero {
         vm.expectRevert("0 amt");
         drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
     }
@@ -124,7 +142,86 @@ contract DropERC721Test_lazyMint is BaseTest {
     function test_event_dataLengthZero() public callerWithMinterRole amountNotEqualZero dataLengthZero {
         uint256 nextTokenIdToLazyMintBefore = drop.nextTokenIdToMint();
 
-        // vm.expectEmit(true, false, false, true);
+        vm.expectEmit(true, false, false, true);
         emit TokensLazyMinted(nextTokenIdToLazyMintBefore, nextTokenIdToLazyMintBefore + lazyMint_amount - 1, lazyMint_revealedURI, lazymint_data);
         drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
     }
+
+    function test_state_noEncryptedURI() public callerWithMinterRole amountNotEqualZero dataValidFormatNoURI {
+        uint256 nextTokenIdToLazyMintBefore = drop.nextTokenIdToMint();
+        uint256 expectedBatchId = nextTokenIdToLazyMintBefore + lazyMint_amount;
+        bytes memory expectedEncryptedData;
+
+        uint256 batchIdReturn = drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
+
+        uint256 batchIdState = drop.getBatchIdAtIndex(0);
+        string memory baseURIState = drop.tokenURI(0);
+        bytes memory encryptedDataState = drop.encryptedData(0);
+
+        assertEq(nextTokenIdToLazyMintBefore + lazyMint_amount, drop.nextTokenIdToMint());
+        assertEq(expectedBatchId, batchIdReturn);
+        assertEq(expectedBatchId, batchIdState);
+        assertEq(string(abi.encodePacked(lazyMint_revealedURI, "0")), baseURIState);
+        assertEq(expectedEncryptedData, encryptedDataState);
+    }
+
+    function test_event_noEncryptedURI() public callerWithMinterRole amountNotEqualZero dataValidFormatNoURI {
+        uint256 nextTokenIdToLazyMintBefore = drop.nextTokenIdToMint();
+
+        vm.expectEmit(true, false, false, true);
+        emit TokensLazyMinted(nextTokenIdToLazyMintBefore, nextTokenIdToLazyMintBefore + lazyMint_amount - 1, lazyMint_revealedURI, lazymint_data);
+        drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
+    }
+
+    function test_state_noProvenanceHash() public callerWithMinterRole amountNotEqualZero dataValidFormatNoHash {
+        uint256 nextTokenIdToLazyMintBefore = drop.nextTokenIdToMint();
+        uint256 expectedBatchId = nextTokenIdToLazyMintBefore + lazyMint_amount;
+        bytes memory expectedEncryptedData;
+
+        uint256 batchIdReturn = drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
+
+        uint256 batchIdState = drop.getBatchIdAtIndex(0);
+        string memory baseURIState = drop.tokenURI(0);
+        bytes memory encryptedDataState = drop.encryptedData(0);
+
+        assertEq(nextTokenIdToLazyMintBefore + lazyMint_amount, drop.nextTokenIdToMint());
+        assertEq(expectedBatchId, batchIdReturn);
+        assertEq(expectedBatchId, batchIdState);
+        assertEq(string(abi.encodePacked(lazyMint_revealedURI, "0")), baseURIState);
+        assertEq(expectedEncryptedData, encryptedDataState);
+    }
+
+    function test_event_noProvenanceHash() public callerWithMinterRole amountNotEqualZero dataValidFormatNoHash {
+        uint256 nextTokenIdToLazyMintBefore = drop.nextTokenIdToMint();
+
+        vm.expectEmit(true, false, false, true);
+        emit TokensLazyMinted(nextTokenIdToLazyMintBefore, nextTokenIdToLazyMintBefore + lazyMint_amount - 1, lazyMint_revealedURI, lazymint_data);
+        drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
+    }
+
+    function test_state_encryptedURIAndHash() public callerWithMinterRole amountNotEqualZero dataValidFormat {
+        uint256 nextTokenIdToLazyMintBefore = drop.nextTokenIdToMint();
+        uint256 expectedBatchId = nextTokenIdToLazyMintBefore + lazyMint_amount;
+
+        uint256 batchIdReturn = drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
+
+        uint256 batchIdState = drop.getBatchIdAtIndex(0);
+        string memory baseURIState = drop.tokenURI(0);
+        bytes memory encryptedDataState = drop.encryptedData(batchIdReturn);
+
+        assertEq(nextTokenIdToLazyMintBefore + lazyMint_amount, drop.nextTokenIdToMint());
+        assertEq(expectedBatchId, batchIdReturn);
+        assertEq(expectedBatchId, batchIdState);
+        assertEq(string(abi.encodePacked(lazyMint_revealedURI, "0")), baseURIState);
+        assertEq(lazymint_data, encryptedDataState);
+    }
+
+    function test_event_encryptedURIAndHash() public callerWithMinterRole amountNotEqualZero dataValidFormat {
+        uint256 nextTokenIdToLazyMintBefore = drop.nextTokenIdToMint();
+
+        vm.expectEmit(true, false, false, true);
+        emit TokensLazyMinted(nextTokenIdToLazyMintBefore, nextTokenIdToLazyMintBefore + lazyMint_amount - 1, lazyMint_revealedURI, lazymint_data);
+        drop.lazyMint(lazyMint_amount, lazyMint_revealedURI, lazymint_data);
+    }
+
+}
