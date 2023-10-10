cf4fc16576e22783fafc0bd03debaca110e8a952
WhiteOakKong <aadam.debevec@outlook.com>
2023-10-10 17:16:12 -0500

    remove harness initializer

--- src/test/drop/drop-erc721/_collectPriceOnClaim/_collectPriceOnClaim.t.sol
+++ src/test/drop/drop-erc721/_collectPriceOnClaim/_collectPriceOnClaim.t.sol
@@ -8,6 +8,8 @@ import "contracts/lib/TWStrings.sol";
 import "../../../utils/BaseTest.sol";
 import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
 
+import { TWProxy } from "contracts/infra/TWProxy.sol";
+
 contract HarnessDropERC721 is DropERC721 {
     function collectionPriceOnClaim(
         address _primarySaleRecipient,
@@ -17,46 +19,15 @@ contract HarnessDropERC721 is DropERC721 {
     ) public payable {
         _collectPriceOnClaim(_primarySaleRecipient, _quantityToClaim, _currency, _pricePerToken);
     }
-
-    function initializeHarness(
-        address _defaultAdmin,
-        string memory _contractURI,
-        address _saleRecipient,
-        address _royaltyRecipient,
-        uint128 _royaltyBps,
-        uint128 _platformFeeBps,
-        address _platformFeeRecipient
-    ) external {
-        bytes32 _transferRole = keccak256("TRANSFER_ROLE");
-        bytes32 _minterRole = keccak256("MINTER_ROLE");
-        bytes32 _metadataRole = keccak256("METADATA_ROLE");
-
-        _setupContractURI(_contractURI);
-        _setupOwner(_defaultAdmin);
-
-        _setupRole(DEFAULT_ADMIN_ROLE, _defaultAdmin);
-        _setupRole(_minterRole, _defaultAdmin);
-        _setupRole(_transferRole, _defaultAdmin);
-        _setupRole(_transferRole, address(0));
-        _setupRole(_metadataRole, _defaultAdmin);
-        _setRoleAdmin(_metadataRole, _metadataRole);
-
-        _setupPlatformFeeInfo(_platformFeeRecipient, _platformFeeBps);
-        _setupDefaultRoyaltyInfo(_royaltyRecipient, _royaltyBps);
-        _setupPrimarySaleRecipient(_saleRecipient);
-    }
 }
 
 contract DropERC721Test_collectPrice is BaseTest {
     using StringsUpgradeable for uint256;
 
-    HarnessDropERC721 public dropImp;
+    address public dropImp;
+    HarnessDropERC721 public proxy;
 
     address private collectPrice_saleRecipient = address(0x010);
-    address private collectPrice_royaltyRecipient = address(0x011);
-    uint128 private collectPrice_royaltyBps = 1000;
-    uint128 private collectPrice_platformFeeBps = 1000;
-    address private collectPrice_platformFeeRecipient = address(0x012);
     uint256 private collectPrice_quantityToClaim = 1;
     uint256 private collectPrice_pricePerToken;
     address private collectPrice_currency;
@@ -65,16 +36,24 @@ contract DropERC721Test_collectPrice is BaseTest {
     function setUp() public override {
         super.setUp();
 
-        dropImp = new HarnessDropERC721();
-        dropImp.initializeHarness(
-            deployer,
-            CONTRACT_URI,
-            collectPrice_saleRecipient,
-            collectPrice_royaltyRecipient,
-            collectPrice_royaltyBps,
-            collectPrice_platformFeeBps,
-            collectPrice_platformFeeRecipient
+        bytes memory initializeData = abi.encodeCall(
+            DropERC721.initialize,
+            (
+                deployer,
+                NAME,
+                SYMBOL,
+                CONTRACT_URI,
+                forwarders(),
+                saleRecipient,
+                royaltyRecipient,
+                royaltyBps,
+                platformFeeBps,
+                platformFeeRecipient
+            )
         );
+
+        dropImp = address(new HarnessDropERC721());
+        proxy = HarnessDropERC721(address(new TWProxy(dropImp, initializeData)));
     }
 
     modifier pricePerTokenZero() {
@@ -109,7 +88,7 @@ contract DropERC721Test_collectPrice is BaseTest {
 
     function test_revert_msgValueNotZero() public nativeCurrency msgValueNotZero pricePerTokenZero {
         vm.expectRevert();
-        dropImp.collectionPriceOnClaim{ value: collectPrice_msgValue }(
+        proxy.collectionPriceOnClaim{ value: collectPrice_msgValue }(
             collectPrice_saleRecipient,
             collectPrice_quantityToClaim,
             collectPrice_currency,
@@ -119,7 +98,7 @@ contract DropERC721Test_collectPrice is BaseTest {
 
     function test_revert_priceValueMismatchNativeCurrency() public nativeCurrency pricePerTokenNotZero {
         vm.expectRevert();
-        dropImp.collectionPriceOnClaim{ value: collectPrice_msgValue }(
+        proxy.collectionPriceOnClaim{ value: collectPrice_msgValue }(
             collectPrice_saleRecipient,
             collectPrice_quantityToClaim,
             collectPrice_currency,
@@ -128,18 +107,18 @@ contract DropERC721Test_collectPrice is BaseTest {
     }
 
     function test_transferNativeCurrency() public nativeCurrency pricePerTokenNotZero msgValueNotZero {
-        uint256 balanceSaleRecipientBefore = address(collectPrice_saleRecipient).balance;
-        uint256 platformFeeRecipientBefore = address(collectPrice_platformFeeRecipient).balance;
-        dropImp.collectionPriceOnClaim{ value: collectPrice_msgValue }(
-            collectPrice_saleRecipient,
+        uint256 balanceSaleRecipientBefore = address(saleRecipient).balance;
+        uint256 platformFeeRecipientBefore = address(platformFeeRecipient).balance;
+        proxy.collectionPriceOnClaim{ value: collectPrice_msgValue }(
+            saleRecipient,
             collectPrice_quantityToClaim,
             collectPrice_currency,
             collectPrice_pricePerToken
         );
 
-        uint256 balanceSaleRecipientAfter = address(collectPrice_saleRecipient).balance;
-        uint256 platformFeeRecipientAfter = address(collectPrice_platformFeeRecipient).balance;
-        uint256 expectedPlatformFee = (collectPrice_pricePerToken * collectPrice_platformFeeBps) / MAX_BPS;
+        uint256 balanceSaleRecipientAfter = address(saleRecipient).balance;
+        uint256 platformFeeRecipientAfter = address(platformFeeRecipient).balance;
+        uint256 expectedPlatformFee = (collectPrice_pricePerToken * platformFeeBps) / MAX_BPS;
         uint256 expectedSaleRecipientProceed = collectPrice_msgValue - expectedPlatformFee;
 
         assertEq(balanceSaleRecipientAfter - balanceSaleRecipientBefore, expectedSaleRecipientProceed);
@@ -147,19 +126,19 @@ contract DropERC721Test_collectPrice is BaseTest {
     }
 
     function test_transferERC20() public erc20Currency pricePerTokenNotZero {
-        uint256 balanceSaleRecipientBefore = erc20.balanceOf(collectPrice_saleRecipient);
-        uint256 platformFeeRecipientBefore = erc20.balanceOf(collectPrice_platformFeeRecipient);
-        erc20.approve(address(dropImp), collectPrice_pricePerToken);
-        dropImp.collectionPriceOnClaim(
-            collectPrice_saleRecipient,
+        uint256 balanceSaleRecipientBefore = erc20.balanceOf(saleRecipient);
+        uint256 platformFeeRecipientBefore = erc20.balanceOf(platformFeeRecipient);
+        erc20.approve(address(proxy), collectPrice_pricePerToken);
+        proxy.collectionPriceOnClaim(
+            saleRecipient,
             collectPrice_quantityToClaim,
             collectPrice_currency,
             collectPrice_pricePerToken
         );
 
-        uint256 balanceSaleRecipientAfter = erc20.balanceOf(collectPrice_saleRecipient);
-        uint256 platformFeeRecipientAfter = erc20.balanceOf(collectPrice_platformFeeRecipient);
-        uint256 expectedPlatformFee = (collectPrice_pricePerToken * collectPrice_platformFeeBps) / MAX_BPS;
+        uint256 balanceSaleRecipientAfter = erc20.balanceOf(saleRecipient);
+        uint256 platformFeeRecipientAfter = erc20.balanceOf(platformFeeRecipient);
+        uint256 expectedPlatformFee = (collectPrice_pricePerToken * platformFeeBps) / MAX_BPS;
         uint256 expectedSaleRecipientProceed = collectPrice_pricePerToken - expectedPlatformFee;
 
         assertEq(balanceSaleRecipientAfter - balanceSaleRecipientBefore, expectedSaleRecipientProceed);
