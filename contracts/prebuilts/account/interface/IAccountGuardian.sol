dd378a114430c0fba62c92478d710bd2629cc9f1
Shivendra Singh <shiven@alfheimr.tech>
2023-10-16 19:16:54 +0530

    created interfaces for Account recovery feature

---
+++ contracts/prebuilts/account/interface/IAccountGuardian.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+interface IAccountGuardian {
+    
+     /*///////////////////////////////////////////////////////////////
+                        Events
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * An event emitted when guardian is added by a smart wallet user.
+     * @param guardian - the verified address of a wallet to be
+     * added as a guardian.
+     */
+    event GuardianAdded(address indexed guardian);
+
+    /**
+     * An event emitted when guardian is removed by a smart wallet user.
+     * @param guardian the address of guardian removed
+     */
+    event GuardianRemoved(address indexed guardian);
+
+
+    /*///////////////////////////////////////////////////////////////
+                        Errors
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * An error thrown when guardian being added is not verified by the Thirdweb's
+     * guardian signup dapp.
+     * @param guardian address that was not added as a guardian
+     */
+    error GuardianNotVerified(address guardian);
+
+    /**
+     * An error thrown if the guardian the user is trying to remove is not a part of 
+     * the user's guardian list.
+     * @param guardian address which the user was trying to remove from their 
+     * guardian list but was not the guardian
+     */
+    error NotAGuardian(address guardian);
+
+    /**
+     * An error thrown when the user tries to remove a guardian from the list during
+     * an active account recovery request
+     * @param guardian address of guardian user is trying to remove
+     * @param recoveryHash active recovery request hash
+     */
+    error GuardianNotRemovedDueToActiveRecoveryRequest(
+        address guardian,
+        bytes32 recoveryHash
+        );
+
+
+     /*///////////////////////////////////////////////////////////////
+                        External Functions
+    //////////////////////////////////////////////////////////////*/
+
+
+    /**
+     * @notice Add guardians for your smart-wallet.
+     * @dev The guardian address needs to connect to the thirdweb’s 
+     * guardian signup dapp by accepting the signin request.
+     * @param guardian the verified address of a wallet to be
+     * added as a guardian.
+     * @return bool boolean flag indicating whether the guardian was added.
+     */
+    function addGuardian(address guardian) external returns(bool);
+
+
+    /**
+     * @notice A user will be able to remove allotted guardian(s) from
+     * their smart-wallet guardian list.
+     * @dev The address should be a registered guardian of the account.
+     * @param guardian address of the guardian the user wishes to remove.
+     * @return bool flag indicating whether the guardian was removed from the list or not.
+     */
+    function removeGuardian(address guardian) external returns(bool);
+
+    /**
+     * @notice Returns a list of all added guardians of the sender.
+     * @return List of guardians of the sender smart-wallet.
+     */
+    function getAllGuardians() external returns(address[] memory);
+
+}
---
+++ contracts/prebuilts/account/interface/IAccountLockRequest.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+interface IAccountLockRequest {
+     /*///////////////////////////////////////////////////////////////
+                        Events
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * An event emitted when account lock request is successfully created by a guardian.
+     * @param smartWallet address of the smart wallet for which lock request is created
+     */
+    event AccountLockRequestCreated(address indexed smartWallet);
+
+    /**
+     * An event emitted when account unlock request is successfully created by a guardian.
+     * @param smartWallet address of the smart wallet for which lock request is created
+     */
+    event AccountUnLockRequestCreated(address indexed smartWallet);
+
+    /**
+     * An event emitted when a guardian accepts a lock request.
+     * @param lockRequest type hash of the lock request
+     * @param guardian address of guardian who accepted the request
+     */
+    event AccountLockRequestAccepted(bytes32 indexed lockRequest, address indexed guardian);
+
+    /**
+     * @notice An event emitted when a guardian declines a lock request.
+     * @param lockRequest type hash of the lock request
+     * @param guardian address of guardian who accepted the request
+     */
+    event AccountLockRequestRejected(bytes32 indexed lockRequest, address indexed guardian);
+
+     /*///////////////////////////////////////////////////////////////
+                        Errors
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * This error is thrown when a non-guardian tries to create a recovery
+     * request of a smart wallet account.
+     * @param smartWallet address of the smart wallet being recovered
+     */
+    error NotAGuardian(address smartWallet);
+
+    /**
+     * Error thrown when a unlock request is created for an already unlocked smart-wallet
+     * @param smartWallet address of the smart wallet being unlocked
+     */
+    error AccountAlreadyUnlocked(address smartWallet);
+
+     /*///////////////////////////////////////////////////////////////
+                        External Functions
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @dev Triggered by a guardian to create a lock request.
+     * @param smartWallet address of the smart wallet to be recovered
+     */
+
+    function createLockRequest(address smartWallet) external returns(bytes memory);
+
+    /**
+     * @dev This function is called when a guardian makes his choice of 
+     * signing or not signing the account lock request.
+     * @param lockRequest type hash of the lock request
+     * @return Request signature incase the guardian accepts the request else returns null.
+     */
+
+    function acceptOrRejectLockRequest(bytes32 lockRequest) external returns(bytes memory);
+
+    /**
+     * @dev Triggered by a guardian to create an unlock request.
+     * @param smartWallet address of the smart wallet to be unlocked
+     */
+
+    function createUnLockRequest(address smartWallet) external returns(bytes memory);
+
+    /**
+     * @dev This function is called when a guardian makes his choice of 
+     * signing or not signing the account unlocking request.
+     * @param unlockRequest type hash of the unlock request
+     * @return Request signature incase the guardian accepts the request else returns null.
+     */
+    function acceptOrRejectUnlockRequest(bytes32 unlockRequest) external returns(bytes memory);
+
+}
