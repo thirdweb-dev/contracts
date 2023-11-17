82c92815fb04d40c4893dd870e6db78a96fc2487
techyNonso <williamikeji@gmail.com>
2023-11-17 10:56:03 +0100

    depencies added but errors persist

--- contracts/prebuilts/account/utils/CrossChainTokenTransfer.sol
+++ contracts/prebuilts/account/utils/CrossChainTokenTransfer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.12;
 
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
