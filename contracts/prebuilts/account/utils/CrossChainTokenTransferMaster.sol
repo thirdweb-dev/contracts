c3c5a31486f1e2924693d02721fe6c12ca22dd51
techyNonso <williamikeji@gmail.com>
2023-12-05 11:14:17 +0100

    link transfer initiator done

--- contracts/prebuilts/account/utils/CrossChainTokenTransferMaster.sol
+++ contracts/prebuilts/account/utils/CrossChainTokenTransferMaster.sol
@@ -16,7 +16,7 @@ import "./CrossChainTokenTransfer.sol";
 
 contract CrossChainTokenTransferMaster {
     //flow for native payment
-    function getEstimate(
+    function getEstimateNative(
         CrossChainTokenTransfer _crossChainContract,
         uint64 _destinationChainSelector,
         address _receiver,
@@ -27,6 +27,7 @@ contract CrossChainTokenTransferMaster {
         estimate = _crossChainContract.estimateNative(_destinationChainSelector, _receiver, _token, _amount);
     }
 
+    //initiate token transfer for native payment
     function initiateTokenTransferPayNative(
         CrossChainTokenTransfer _crossChainContract,
         uint64 _destinationChainSelector,
@@ -46,6 +47,51 @@ contract CrossChainTokenTransferMaster {
                 msg.sender,
                 _token,
                 _amount,
+                _amount
+            )
+        );
+
+        require(success, "Cross chain token transfer initiation failed");
+    }
+
+    //flow for Link payment
+    function getEstimateLink(
+        CrossChainTokenTransfer _crossChainContract,
+        uint64 _destinationChainSelector,
+        address _receiver,
+        address _token,
+        uint256 _amount
+    ) external view returns (uint estimate) {
+        //call the estimate function
+        estimate = _crossChainContract.estimateLink(_destinationChainSelector, _receiver, _token, _amount);
+    }
+
+    //initiate token transfer for Link payment
+    function initiateTokenTransferPayLink(
+        CrossChainTokenTransfer _crossChainContract,
+        uint64 _destinationChainSelector,
+        address _receiver,
+        address _token,
+        uint256 _amount,
+        address _link,
+        uint256 _linkAmount
+    ) external {
+        //approve token amount
+        IERC20(_token).approve(address(_crossChainContract), _amount);
+
+        //approve Link amount
+        IERC20(_link).approve(address(_crossChainContract), _linkAmount);
+
+        //call transfer function with link value
+        (bool success, ) = address(_crossChainContract).call{ value: msg.value }(
+            abi.encodeWithSignature(
+                "transferTokensPayLINK(uint64 , address, address ,address , uint256 ,uint256, uint256 )",
+                _destinationChainSelector,
+                _receiver,
+                msg.sender,
+                _token,
+                _amount,
+                _linkAmount,
                 _amount
             )
         );
