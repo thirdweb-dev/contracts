6933b2338afdc32d91142794d9e2d44e7d937eee
Yash <kumaryashcse@gmail.com>
2024-01-11 22:48:25 +0530

    correct methods

--- contracts/prebuilts/unaudited/checkout/PluginCheckout.sol
+++ contracts/prebuilts/unaudited/checkout/PluginCheckout.sol
@@ -16,10 +16,11 @@ import "./TargetCheckout.sol";
 
 contract PluginCheckout is IPRBProxyPlugin, TargetCheckout {
     function getMethods() external pure override returns (bytes4[] memory) {
-        bytes4[] memory methods = new bytes4[](3);
+        bytes4[] memory methods = new bytes4[](4);
         methods[0] = this.withdraw.selector;
         methods[1] = this.execute.selector;
         methods[2] = this.swapAndExecute.selector;
+        methods[3] = this.approveSwapRouter.selector;
         return methods;
     }
 }
