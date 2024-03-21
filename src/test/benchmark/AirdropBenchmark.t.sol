d5eb80a342ea62542e580ddb4037026d9333b5ca
Yash <kumaryashcse@gmail.com>
2024-03-22 04:17:04 +0530

    fix

--- src/test/benchmark/AirdropBenchmark.t.sol
+++ src/test/benchmark/AirdropBenchmark.t.sol
@@ -10,24 +10,25 @@ import "../utils/BaseTest.sol";
 contract AirdropBenchmarkTest is BaseTest {
     Airdrop internal airdrop;
 
-    bytes32 private constant CONTENT_TYPEHASH_ERC20 = keccak256("AirdropContent20(address recipient,uint256 amount)");
+    bytes32 private constant CONTENT_TYPEHASH_ERC20 =
+        keccak256("AirdropContentERC20(address recipient,uint256 amount)");
     bytes32 private constant REQUEST_TYPEHASH_ERC20 =
         keccak256(
-            "AirdropRequest20(bytes32 uid,address tokenAddress,uint256 expirationTimestamp,AirdropContent20[] contents)AirdropContent20(address recipient,uint256 amount)"
+            "AirdropRequestERC20(bytes32 uid,address tokenAddress,uint256 expirationTimestamp,AirdropContentERC20[] contents)AirdropContentERC20(address recipient,uint256 amount)"
         );
 
     bytes32 private constant CONTENT_TYPEHASH_ERC721 =
-        keccak256("AirdropContent721(address recipient,uint256 tokenId)");
+        keccak256("AirdropContentERC721(address recipient,uint256 tokenId)");
     bytes32 private constant REQUEST_TYPEHASH_ERC721 =
         keccak256(
-            "AirdropRequest721(bytes32 uid,address tokenAddress,uint256 expirationTimestamp,AirdropContent721[] contents)AirdropContent721(address recipient,uint256 tokenId)"
+            "AirdropRequestERC721(bytes32 uid,address tokenAddress,uint256 expirationTimestamp,AirdropContentERC721[] contents)AirdropContentERC721(address recipient,uint256 tokenId)"
         );
 
     bytes32 private constant CONTENT_TYPEHASH_ERC1155 =
-        keccak256("AirdropContent1155(address recipient,uint256 tokenId,uint256 amount)");
+        keccak256("AirdropContentERC1155(address recipient,uint256 tokenId,uint256 amount)");
     bytes32 private constant REQUEST_TYPEHASH_ERC1155 =
         keccak256(
-            "AirdropRequest1155(bytes32 uid,address tokenAddress,uint256 expirationTimestamp,AirdropContent1155[] contents)AirdropContent1155(address recipient,uint256 tokenId,uint256 amount)"
+            "AirdropRequestERC1155(bytes32 uid,address tokenAddress,uint256 expirationTimestamp,AirdropContentERC1155[] contents)AirdropContentERC1155(address recipient,uint256 tokenId,uint256 amount)"
         );
 
     bytes32 private constant NAME_HASH = keccak256(bytes("Airdrop"));
@@ -49,24 +50,26 @@ contract AirdropBenchmarkTest is BaseTest {
         );
     }
 
-    function _getContentsERC20(uint256 length) internal pure returns (Airdrop.AirdropContent20[] memory contents) {
-        contents = new Airdrop.AirdropContent20[](length);
+    function _getContentsERC20(uint256 length) internal pure returns (Airdrop.AirdropContentERC20[] memory contents) {
+        contents = new Airdrop.AirdropContentERC20[](length);
         for (uint256 i = 0; i < length; i++) {
             contents[i].recipient = address(uint160(i + 10));
             contents[i].amount = i + 10;
         }
     }
 
-    function _getContentsERC721(uint256 length) internal pure returns (Airdrop.AirdropContent721[] memory contents) {
-        contents = new Airdrop.AirdropContent721[](length);
+    function _getContentsERC721(uint256 length) internal pure returns (Airdrop.AirdropContentERC721[] memory contents) {
+        contents = new Airdrop.AirdropContentERC721[](length);
         for (uint256 i = 0; i < length; i++) {
             contents[i].recipient = address(uint160(i + 10));
             contents[i].tokenId = i;
         }
     }
 
-    function _getContentsERC1155(uint256 length) internal pure returns (Airdrop.AirdropContent1155[] memory contents) {
-        contents = new Airdrop.AirdropContent1155[](length);
+    function _getContentsERC1155(
+        uint256 length
+    ) internal pure returns (Airdrop.AirdropContentERC1155[] memory contents) {
+        contents = new Airdrop.AirdropContentERC1155[](length);
         for (uint256 i = 0; i < length; i++) {
             contents[i].recipient = address(uint160(i + 10));
             contents[i].tokenId = 0;
@@ -75,7 +78,7 @@ contract AirdropBenchmarkTest is BaseTest {
     }
 
     function _signReqERC20(
-        Airdrop.AirdropRequest20 memory req,
+        Airdrop.AirdropRequestERC20 memory req,
         uint256 privateKey
     ) internal view returns (bytes memory signature) {
         bytes32[] memory contentHashes = new bytes32[](req.contents.length);
@@ -107,7 +110,7 @@ contract AirdropBenchmarkTest is BaseTest {
     }
 
     function _signReqERC721(
-        Airdrop.AirdropRequest721 memory req,
+        Airdrop.AirdropRequestERC721 memory req,
         uint256 privateKey
     ) internal view returns (bytes memory signature) {
         bytes32[] memory contentHashes = new bytes32[](req.contents.length);
@@ -139,7 +142,7 @@ contract AirdropBenchmarkTest is BaseTest {
     }
 
     function _signReqERC1155(
-        Airdrop.AirdropRequest1155 memory req,
+        Airdrop.AirdropRequestERC1155 memory req,
         uint256 privateKey
     ) internal view returns (bytes memory signature) {
         bytes32[] memory contentHashes = new bytes32[](req.contents.length);
@@ -186,7 +189,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc20.approve(address(airdrop), 100 ether);
 
-        Airdrop.AirdropContent20[] memory contents = _getContentsERC20(10);
+        Airdrop.AirdropContentERC20[] memory contents = _getContentsERC20(10);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -200,7 +203,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc20.approve(address(airdrop), 100 ether);
 
-        Airdrop.AirdropContent20[] memory contents = _getContentsERC20(100);
+        Airdrop.AirdropContentERC20[] memory contents = _getContentsERC20(100);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -214,7 +217,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc20.approve(address(airdrop), 100 ether);
 
-        Airdrop.AirdropContent20[] memory contents = _getContentsERC20(1000);
+        Airdrop.AirdropContentERC20[] memory contents = _getContentsERC20(1000);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -232,8 +235,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc20.approve(address(airdrop), 100 ether);
 
-        Airdrop.AirdropContent20[] memory contents = _getContentsERC20(10);
-        Airdrop.AirdropRequest20 memory req = Airdrop.AirdropRequest20({
+        Airdrop.AirdropContentERC20[] memory contents = _getContentsERC20(10);
+        Airdrop.AirdropRequestERC20 memory req = Airdrop.AirdropRequestERC20({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc20),
             expirationTimestamp: 1000,
@@ -253,8 +256,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc20.approve(address(airdrop), 100 ether);
 
-        Airdrop.AirdropContent20[] memory contents = _getContentsERC20(100);
-        Airdrop.AirdropRequest20 memory req = Airdrop.AirdropRequest20({
+        Airdrop.AirdropContentERC20[] memory contents = _getContentsERC20(100);
+        Airdrop.AirdropRequestERC20 memory req = Airdrop.AirdropRequestERC20({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc20),
             expirationTimestamp: 1000,
@@ -274,8 +277,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc20.approve(address(airdrop), 100 ether);
 
-        Airdrop.AirdropContent20[] memory contents = _getContentsERC20(1000);
-        Airdrop.AirdropRequest20 memory req = Airdrop.AirdropRequest20({
+        Airdrop.AirdropContentERC20[] memory contents = _getContentsERC20(1000);
+        Airdrop.AirdropRequestERC20 memory req = Airdrop.AirdropRequestERC20({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc20),
             expirationTimestamp: 1000,
@@ -334,7 +337,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc721.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent721[] memory contents = _getContentsERC721(10);
+        Airdrop.AirdropContentERC721[] memory contents = _getContentsERC721(10);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -348,7 +351,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc721.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent721[] memory contents = _getContentsERC721(100);
+        Airdrop.AirdropContentERC721[] memory contents = _getContentsERC721(100);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -362,7 +365,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc721.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent721[] memory contents = _getContentsERC721(1000);
+        Airdrop.AirdropContentERC721[] memory contents = _getContentsERC721(1000);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -380,8 +383,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc721.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent721[] memory contents = _getContentsERC721(10);
-        Airdrop.AirdropRequest721 memory req = Airdrop.AirdropRequest721({
+        Airdrop.AirdropContentERC721[] memory contents = _getContentsERC721(10);
+        Airdrop.AirdropRequestERC721 memory req = Airdrop.AirdropRequestERC721({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc721),
             expirationTimestamp: 1000,
@@ -401,8 +404,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc721.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent721[] memory contents = _getContentsERC721(100);
-        Airdrop.AirdropRequest721 memory req = Airdrop.AirdropRequest721({
+        Airdrop.AirdropContentERC721[] memory contents = _getContentsERC721(100);
+        Airdrop.AirdropRequestERC721 memory req = Airdrop.AirdropRequestERC721({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc721),
             expirationTimestamp: 1000,
@@ -422,8 +425,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc721.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent721[] memory contents = _getContentsERC721(1000);
-        Airdrop.AirdropRequest721 memory req = Airdrop.AirdropRequest721({
+        Airdrop.AirdropContentERC721[] memory contents = _getContentsERC721(1000);
+        Airdrop.AirdropRequestERC721 memory req = Airdrop.AirdropRequestERC721({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc721),
             expirationTimestamp: 1000,
@@ -482,7 +485,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc1155.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent1155[] memory contents = _getContentsERC1155(10);
+        Airdrop.AirdropContentERC1155[] memory contents = _getContentsERC1155(10);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -496,7 +499,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc1155.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent1155[] memory contents = _getContentsERC1155(100);
+        Airdrop.AirdropContentERC1155[] memory contents = _getContentsERC1155(100);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -510,7 +513,7 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc1155.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent1155[] memory contents = _getContentsERC1155(1000);
+        Airdrop.AirdropContentERC1155[] memory contents = _getContentsERC1155(1000);
 
         vm.prank(signer);
         vm.resumeGasMetering();
@@ -528,8 +531,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc1155.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent1155[] memory contents = _getContentsERC1155(10);
-        Airdrop.AirdropRequest1155 memory req = Airdrop.AirdropRequest1155({
+        Airdrop.AirdropContentERC1155[] memory contents = _getContentsERC1155(10);
+        Airdrop.AirdropRequestERC1155 memory req = Airdrop.AirdropRequestERC1155({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc1155),
             expirationTimestamp: 1000,
@@ -549,8 +552,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc1155.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent1155[] memory contents = _getContentsERC1155(100);
-        Airdrop.AirdropRequest1155 memory req = Airdrop.AirdropRequest1155({
+        Airdrop.AirdropContentERC1155[] memory contents = _getContentsERC1155(100);
+        Airdrop.AirdropRequestERC1155 memory req = Airdrop.AirdropRequestERC1155({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc1155),
             expirationTimestamp: 1000,
@@ -570,8 +573,8 @@ contract AirdropBenchmarkTest is BaseTest {
         vm.prank(signer);
         erc1155.setApprovalForAll(address(airdrop), true);
 
-        Airdrop.AirdropContent1155[] memory contents = _getContentsERC1155(1000);
-        Airdrop.AirdropRequest1155 memory req = Airdrop.AirdropRequest1155({
+        Airdrop.AirdropContentERC1155[] memory contents = _getContentsERC1155(1000);
+        Airdrop.AirdropRequestERC1155 memory req = Airdrop.AirdropRequestERC1155({
             uid: bytes32(uint256(1)),
             tokenAddress: address(erc1155),
             expirationTimestamp: 1000,
