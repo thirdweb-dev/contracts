a0029f845a66d110f7000556fdf8be9e5256bba2
Yash <kumaryashcse@gmail.com>
2024-05-08 04:54:30 +0530

    clean

--- contracts/extension/upgradeable/impl/MetaTx.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.11;
-
-import "../ERC2771Context.sol";
-
-contract MetaTx is ERC2771Context {
-    constructor(address[] memory trustedForwarder) ERC2771Context(trustedForwarder) {}
-}
--- contracts/infra/interface/ITWFee.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.11;
-
-interface ITWFee {
-    function getFeeInfo(address _proxy, uint256 _type) external view returns (address recipient, uint256 bps);
-}
