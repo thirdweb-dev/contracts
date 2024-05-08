b35a14c6175d8ce98edf29054cb32007fdcc00f6
Yash <kumaryashcse@gmail.com>
2024-05-08 23:32:13 +0530

    deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,35 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-import "@nomicfoundation/hardhat-toolbox";
+// import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   zksolc: {
     version: "latest", // Uses latest available in https://github.com/matter-labs/zksolc-bin/
     settings: {},
   },
-  defaultNetwork: "zkSyncTestnet",
+  defaultNetwork: "zkSyncSepoliaTestnet",
   networks: {
     hardhat: {
       zksync: false,
     },
-    zkSyncTestnet: {
+    zkSyncSepoliaTestnet: {
       url: "https://sepolia.era.zksync.dev",
       ethNetwork: "sepolia", // or a Sepolia RPC endpoint from Infura/Alchemy/Chainstack etc.
       zksync: true,
+      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
+    },
+    zkSyncMainnet: {
+      url: "https://mainnet.era.zksync.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
     },
   },
   solidity: {
---
+++ scripts/deploy/zksync/plugin/englishAuctionsLogic-zksync.ts
@@ -0,0 +1,54 @@
+import { Wallet, Provider, ContractFactory } from "zksync-ethers";
+import * as hre from "hardhat";
+import dotenv from "dotenv";
+import { ZkSyncArtifact } from "@matterlabs/hardhat-zksync-solc/dist/src/types";
+
+import "@matterlabs/hardhat-zksync-node/dist/type-extensions";
+import "@matterlabs/hardhat-zksync-verify/dist/src/type-extensions";
+
+dotenv.config();
+
+async function main() {
+  const provider = new Provider(hre.network.config.url);
+  const wallet = new Wallet(`${process.env.TEST_PRIVATE_KEY}`, provider);
+  const artifact: ZkSyncArtifact = (await hre.artifacts.readArtifact("EnglishAuctionsLogic")) as ZkSyncArtifact;
+  const englishAuctions = new ContractFactory(artifact.abi, artifact.bytecode, wallet, "create");
+
+  // WETH address as constructor param
+  // Zksync Mainnet: 0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91
+  // Zksync Sepolia: 0x0462C05457Fed440740Ff3696bDd2D0577411e34
+  const contract = await englishAuctions.deploy("0x0462C05457Fed440740Ff3696bDd2D0577411e34");
+  await contract.deployed();
+
+  console.log("Deployed EnglishAuctionsLogic \n: ", contract.address);
+
+  console.log("\n");
+
+  console.log("Verifying contract.");
+  // deployed address zksync mainnet: 0xcd86890BC05dC3118DAC90330722b23c6cc970e2
+  // deployed address zksync sepolia: 0xefE2fF8F3282Fd63898cb0A532099BA7780b459F
+  await verify(
+    contract.address,
+    "contracts/prebuilts/marketplace/english-auctions/EnglishAuctionsLogic.sol:EnglishAuctionsLogic",
+    ["0x0462C05457Fed440740Ff3696bDd2D0577411e34"],
+  );
+}
+
+async function verify(address: string, contract: string, args: any[]) {
+  try {
+    return await hre.run("verify:verify", {
+      address: address,
+      contract: contract,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    console.log(address, args, e);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
